b9e8fc473ec5f3944229fce331f6619c696163ec
Phil Labee <philip@couchbase.com>
2013-04-23 15:03:58 -0700

    CBD-779:  fixup line endings in email files generated by merge-report
    Change-Id: I0cbf55ac5480232952c063b51cdd6a3623697989

--- scripts/jenkins/merge-report.sh
+++ scripts/jenkins/merge-report.sh
@@ -193,16 +193,16 @@ for COMP in ${PROJECTS}
                 OUT_DIR=${UP2DATE}
                 OUTFILE=${COMP}-UP-TO-DATE.txt
                 OUT_ARG='-e'
-                echo ${COMP}                                                                             >>   ${NOTIFY_GOOD}
+                echo ${OUT_ARG} ${COMP}                                                                  >>   ${NOTIFY_GOOD}
                 git log --oneline --graph --no-abbrev-commit --pretty="format:%H  %ci  %s" -1 | head -1  >>   ${NOTIFY_GOOD}
-                echo "\n"                                                                                >>   ${NOTIFY_GOOD}
+                echo ${OUT_ARG} "\n"                                                                     >>   ${NOTIFY_GOOD}
               else
                 OUT_DIR=${REPORTS}
                 OUTFILE=${COMP}-merge_report-${BRANCH_SRC}-${BRANCH_DST}.txt
                 OUT_ARG='-e'
-                echo            ${COMP}                                                                  >>   ${NOTIFY_TODO}
+                echo ${OUT_ARG} ${COMP}                                                                  >>   ${NOTIFY_TODO}
                 echo ${OUT_ARG} ${MSG}                                                                   >>   ${NOTIFY_TODO}
-                echo "\n"                                                                                >>   ${NOTIFY_TODO}
+                echo ${OUT_ARG} "\n"                                                                     >>   ${NOTIFY_TODO}
             fi
         fi
         write_log  ${OUT_DIR}  ${OUTFILE}  "${COMP} merge ${BRANCH_SRC} into ${BRANCH_DST}"                ${OUT_ARG}
