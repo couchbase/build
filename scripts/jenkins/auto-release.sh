e36b5dc1bd0e02328953af8e44428c3df78174d8
Chisheng Hong <chisheng@couchbase.com>
2013-04-24 12:15:13 -0700

    CBQE-1248 upload script for auto removing staging file
        echo "Usage:"
        echo "./auto_release {VERSION}. {VERSION} is something like 2.0.2"
        echo "By default the script will handle packages for all names, platform and package types"
        echo "If you want to specify platform, name or types, usage is:"
        echo "NAME={NAME} PLATFORM={PLATFORM} TYPE={TYPE} ./auto_release {VERSION}"
    
    Change-Id: Ie883469dbd1ce2252e0caa15b4dc47517121d16c

---
+++ scripts/jenkins/auto-release.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+usage(){
+    echo "Usage:"
+    echo "./auto_release {VERSION}. {VERSION} is something like 2.0.2"
+    echo "By default the script will handle packages for all names, platform and package types"
+    echo "If you want to specify platform, name or types, usage is:"
+    echo "NAME={NAME} PLATFORM={PLATFORM} TYPE={TYPE} ./auto_release {VERSION}"
+}
+
+if [ $# -eq 0 ]; then
+    echo "Build number is not provided"
+    exit
+fi
+
+if [ $1 = "--help" ] || [ $1 = "-h" ]; then
+    usage
+    exit
+fi
+
+if [ -z "$TYPE" ]; then
+    echo "Stage packages for all types"
+    types=("rpm" "deb" "setup.exe")
+else
+    echo "Stage packages for $TYPE"
+    types=$TYPE
+fi
+
+if [ -z "$PLATFORM" ]; then
+    echo "Stage packages for both 32 and 64 bits"
+    platforms=(32 64)
+else
+    echo "Stage packages for $PLATFORM"
+    platforms=$PLATFORM
+fi
+
+if [ -z "$NAME" ]; then
+    echo "Stage packages for both enterprise and community editions"
+    names=("enterprise" "community")
+else
+    echo "Stage packages for $NAME"
+    names=$NAME
+fi
+
+for package_type in ${types[@]}; do
+    for platform in ${platforms[@]}; do
+        for name in ${names[@]}; do
+            if [ $platform -eq 32 ]; then
+                staging="couchbase-server-${name}_x86_${1}.${package_type}.staging"
+            else
+                staging="couchbase-server-${name}_x86_${platform}_${1}.${package_type}.staging"
+            fi
+
+            echo "Remove staging file for $staging and ready for release"
+            s3cmd del "s3://packages.couchbase.com/releases/${1}/${staging}"
+        done
+    done
+done
