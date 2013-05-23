786b376c58013345079b37fc955f445b99d15e6e
Phil Labee <philip@couchbase.com>
2013-05-23 14:56:24 -0700

    MB-6972:  64-bit RPMs go in x86_64 directory
    Change-Id: Idca64b81a45697d1a0c92a916918c47ed13c5302

--- scripts/release/linux_repo/import_rpm.sh
+++ scripts/release/linux_repo/import_rpm.sh
@@ -73,8 +73,8 @@ echo "Importing into local ${EDITION} repo at ${REPO}"
 echo ""
 
 fetch_rpm  couchbase-server-${EDITION}_x86_64_${VERSION}-rel.rpm
-cp         couchbase-server-${EDITION}_x86_64_${VERSION}-rel.rpm  ${REPO}/5/i386/couchbase-server-${EDITION}_${BASEVER}.x86_64.rpm
-cp         couchbase-server-${EDITION}_x86_64_${VERSION}-rel.rpm  ${REPO}/6/i386/couchbase-server-${EDITION}_${BASEVER}.x86_64.rpm
+cp         couchbase-server-${EDITION}_x86_64_${VERSION}-rel.rpm  ${REPO}/5/x86_64/couchbase-server-${EDITION}_${BASEVER}.x86_64.rpm
+cp         couchbase-server-${EDITION}_x86_64_${VERSION}-rel.rpm  ${REPO}/6/x86_64/couchbase-server-${EDITION}_${BASEVER}.x86_64.rpm
     
 fetch_rpm  couchbase-server-${EDITION}_x86_${VERSION}-rel.rpm
 cp         couchbase-server-${EDITION}_x86_${VERSION}-rel.rpm     ${REPO}/5/i386/couchbase-server-${EDITION}_${BASEVER}.i386.rpm
