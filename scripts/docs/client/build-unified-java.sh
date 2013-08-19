21f37dcec63839f2e502ec2b7b6db4387f6e2f70
Phil Labee <philip@couchbase.com>
2013-08-19 13:39:02 -0700

    JCBC-303:  also build ruby, and call the builder scripts
    Change-Id: I470c9df35553ca68528a8bfa1b91866eaaef00e0

--- scripts/docs/client/build-unified-java.sh
+++ scripts/docs/client/build-unified-java.sh
@@ -3,7 +3,7 @@
 
 GITHUB=git://10.1.1.210
 
-for PROJ in couchbase-java-client spymemcached
+for PROJ in couchbase-java-client couchbase-ruby-client spymemcached
     do
     PROJ_DIR=./${PROJ}
     if [[ -d ${PROJ_DIR} ]] ; then rm -rf ${PROJ_DIR} ; mkdir ${PROJ_DIR} ; fi
@@ -14,6 +14,10 @@ done
 DOC_DIR=./unified-docs
 if [[ -d ${DOC_DIR} ]] ; then rm -rf ${DOC_DIR} ; mkdir ${DOC_DIR} ; fi
 
+./build-java-unified-docs.sh
+./build-ruby-docs.sh
+
 javadoc -d ${DOC_DIR} \
     `find couchbase-java-client/src/main/java/com/couchbase/client -name "*.java"` \
+    `find couchbase-ruby-client/src/main/java/com/couchbase/client -name "*.java"` \
     `find spymemcached/src/main/java/net/spy/memcached             -name "*.java"`
