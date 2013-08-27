b1367d1bbed0e73314ae112d643e171004d2e5c6
Phil Labee <philip@couchbase.com>
2013-08-27 09:39:49 -0700

    CBLT-44: to set up GO compiler and cross-compilers
    Change-Id: I131f3543bfb8b1edf94d8574d2deb348627544fb

---
+++ scripts/setup/go/setup_cross_compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+pushd ${GOROOT}/src  2>&1 > /dev/null
+echo ......... now in  ${GOROOT}/src
+
+echo -------- step 1
+./make.bash
+
+echo -------- step 2
+for arch in 8 6
+    do
+    for cmd in a c g l
+        do
+        go tool dist install -v cmd/$arch$cmd
+    done
+done
+
+echo -------- step 3
+export CGO_ENABLED=0 
+
+ echo ..............windows........amd64
+ CGO_ENABLED=0 GOOS=windows GOARCH=amd64   go tool dist install -v pkg/runtime
+ CGO_ENABLED=0 GOOS=windows GOARCH=amd64   go install -v -a std
+
+ echo ..............windows........386
+ CGO_ENABLED=0 GOOS=windows GOARCH=386     go tool dist install -v pkg/runtime
+ CGO_ENABLED=0 GOOS=windows GOARCH=386     go install -v -a std
+
+echo ......... now back
+popd                 2>&1 > /dev/null
+
---
+++ scripts/setup/go/test.h
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go run hello.go
