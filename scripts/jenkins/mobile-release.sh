ded61bf8a5f5161e4e9a7934d090c783a60a1712
abhinavdangeti <abhinav@couchbase.com>
2013-09-06 15:05:31 -0700

    Couchbase-lite and sync-gateway staging amends

--- scripts/jenkins/mobile-release.sh
+++ scripts/jenkins/mobile-release.sh
@@ -29,18 +29,41 @@ else
     platforms=$MODEL
 fi
 
-for platform_type in ${platforms[@]}; do
-    if [ $platform_type == "android" ]; then
-        staging="cblite_android_{1}.zip.staging"
-        path="couchbase-lite/android/1.0-beta"
-    elif [ $platform_type == "ios" ]; then
-        staging="cblite_ios_{1}.zip.staging"
-        path="couchbase-lite/ios/1.0-beta"
-    elif [ $platform_type == "sync_gateway" ]; then
-        staging="sync_gateway_{1}.zip.staging"
-        path="couchbase-sync-gateway/1.0-beta"
-    fi
+sync_types=("rpm" "deb" "zip")
+sync_platforms=("x86" "x86_64")
+android_check=0
+ios_check=0
 
-    echo "Removing staging file for $staging and ready fo release"
-    s3cmdl del "s3://packages.couchbase.com/releases/${path}/${staging}"
+for platform_type in ${platforms[@]}; do
+    for s_type in ${sync_types[@]}; do
+        for s_pl in ${sync_platforms[@]}; do
+            if [ $platform_type == "android" ]; then
+                if [ $android_check -eq 0 ]; then
+                    staging="couchbase-lite-community-android_`echo {1} | cut -d '-' -f1`-beta.zip.staging"
+                    path="couchbase-lite/android/1.0-beta"
+                    android_check=1
+                else
+                    continue
+                fi
+            elif [ $platform_type == "ios" ]; then
+                if [ $ios_check -eq 0 ]; then
+                    staging="couchbase-lite-community-ios_`echo {1} | cut -d '-' -f1`-beta.zip.staging"
+                    path="couchbase-lite/ios/1.0-beta"
+                    ios_check=1
+                else
+                    continue
+                fi
+            elif [ $platform_type == "couchbase-sync-gateway" ]; then
+                if [ $s_pl == "x86_64" ] && [ $s_type == "zip" ]; then
+                    echo "Do nothing for .zip with x86_64"
+                    continue
+                else
+                    staging="couchbase-sync-gateway-community_`echo {1} | cut -d '-' -f1`-beta_${s_pl}.${s_type}.staging"
+                    path="couchbase-sync-gateway/1.0-beta"
+                fi
+            fi
+            echo "Removing staging file for $staging and ready fo release"
+            s3cmdl del "s3://packages.couchbase.com/releases/${path}/${staging}"
+        done
+    done
 done
--- scripts/jenkins/mobile-undo.sh
+++ scripts/jenkins/mobile-undo.sh
@@ -37,19 +37,43 @@ else
     echo "Stage packages for $MODEL"
     platforms=$MODEL
 
+sync_types=("rpm" "deb" "zip")
+sync_platforms=("x86" "x86_64")
+android_check=0
+ios_check=0
+
 echo "==============================================="
 for platform_type in ${platforms[@]};do
-    if [ $platform_type == "android" ]; then
-        base_name="cblite_android_${VERSION}.zip"
-        path="couchbase-lite/android/1.0-beta"
-    elif [ $platform_type == "ios" ]; then
-        base_name="cblite_ios_${VERSION}.zip"
-        path="couchbase-lite/ios/1.0-beta"
-    elif [ $platform_type == "couchbase-sync-gateway" ]; then
-        base_name="sync_gateway_${VERSION}.zip"
-        path="couchbase-sync-gateway/1.0-beta"
-    fi
-    echo "Removing the specific packages from S3"
-    s3cmd del "s3://packages.couchbase.com/releases/${path}/${base_name}"
-    s3cmd del "s3://packages.couchbase.com/releases/${path}/${base_name}.staging"
+    for s_type in ${sync_types[@]};do
+        for s_pl in ${sync_platforms[@]}; do
+            if [ $platform_type == "android" ]; then
+                if [ $android_check -eq 0 ]; then
+                    base_name="couchbase-lite-community-android_`echo ${VERSION} | cut -d '-' -f1`-beta.zip"
+                    path="couchbase-lite/android/1.0-beta"
+                    android_check=1
+                else
+                    continue
+                fi
+            elif [ $platform_type == "ios" ]; then
+                if [ $ios_check -eq 0 ]; then
+                    base_name="couchbase-lite-community-ios_`echo ${VERSION} | cut -d '-' -f1`-beta.zip"
+                    path="couchbase-lite/ios/1.0-beta"
+                    ios_check=1
+                else
+                    continue
+                fi
+            elif [ $platform_type == "couchbase-sync-gateway" ]; then
+                 if [ $s_pl == "x86_64" ] && [ $s_type == "zip" ]; then
+                    echo "Do nothing for .zip with x86_64"
+                    continue
+                else
+                    base_name="couchbase-sync-gateway-community_`echo ${VERSION} | cut -d '-' -f1`-beta_${s_pl}.${s_type}"
+                    path="couchbase-sync-gateway/1.0-beta"
+                fi
+            fi
+            echo "Removing the specific packages from S3"
+            s3cmd del "s3://packages.couchbase.com/releases/${path}/${base_name}"
+            s3cmd del "s3://packages.couchbase.com/releases/${path}/${base_name}.staging"
+        done
+    done
 done
