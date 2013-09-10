4827244507afb0438f1a2ee6ab7f0a784a5d67ca
Phil Labee <philip@couchbase.com>
2013-09-10 10:46:12 -0700

    CBIT-12:  add some debugging output
    Change-Id: Ib3f5eac399e9462d177bdd1afa99f503c8988c81

--- scripts/jenkins/build_trigger/server_buildbot_trigger.sh
+++ scripts/jenkins/build_trigger/server_buildbot_trigger.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+#          
 #          jenkins monitors changes in files pointed to by server manifest
 #          and triggers buildbot repo-* job to build on all platforms
 
 DATA_DIR=/var/lib/jenkins/data/buildbot_trigger
 
 URL_ROOT=http://builds.hq.northscale.net:8010
-USERPASS='username=couchbase.build&passwd=couchbase.build.password'
+USERPASS='username=couchbase.build\&passwd=couchbase.build.password'
 
 BRANCH=master
 
@@ -19,6 +19,8 @@ BUILDER=repo-couchbase-${BRANCH}-builder
 
 if [[ ! -f ${MFST_PRE} ]]
     then
+    echo ......launching FIRST build ${BUILDER}
+    echo ......curl ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
     curl ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
     repo manifest -r  > ${MFST_PRE}
     exit 0
@@ -28,9 +30,12 @@ repo manifest -r  > ${MFST_NOW}
 
 manifest_diff=`diff ${MFST_NOW} ${MFST_PRE} | grep "project name" | grep -v testrunner`
 
-if [ "x$manifest_diff" != "x" ]; then
+if [[ "x$manifest_diff" != "x" ]]
+  then
+    echo ......launching NEW build ${BUILDER}
+    echo ......curl ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
     curl ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
     repo manifest -r  > ${MFST_PRE}
-else
+  else
     echo ">>> No relevant changes"
 fi
