bd66df4e310c4ddf1a5536ed80f422b52b9f375c
Phil Labee <philip@couchbase.com>
2013-10-16 15:36:24 -0700

    merge d15ae96dfb4b4a023e021841322dd9be0ec57b4c: "call /usr/bin/curl"
    Change-Id: I45b9339ff393bcf3f8b75bc5c6e3e7e8da98c210

--- scripts/jenkins/build_trigger/server_buildbot_trigger.sh
+++ scripts/jenkins/build_trigger/server_buildbot_trigger.sh
@@ -3,6 +3,7 @@
 #          jenkins monitors changes in files pointed to by server manifest
 #          and triggers buildbot repo-* job to build on all platforms
 
+CURL=/usr/bin/curl
 DATA_DIR=/var/lib/jenkins/data/buildbot_trigger
 
 URL_ROOT=http://builds.hq.northscale.net:8010
@@ -20,8 +21,8 @@ BUILDER=repo-couchbase-${BRANCH}-builder
 if [[ ! -f ${MFST_PRE} ]]
     then
     echo ......launching FIRST build ${BUILDER}
-    echo ......curl ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
-    curl ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
+    echo ......${CURL} ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
+    ${CURL} ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
     repo manifest -r  > ${MFST_PRE}
     exit 0
 fi
@@ -33,8 +34,8 @@ manifest_diff=`diff ${MFST_NOW} ${MFST_PRE} | grep "project name" | grep -v test
 if [[ "x$manifest_diff" != "x" ]]
   then
     echo ......launching NEW build ${BUILDER}
-    echo ......curl ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
-    curl ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
+    echo ......${CURL} ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
+    ${CURL} ${URL_ROOT}/builders/${BUILDER}/force?forcescheduler=all_repo_builders\&${USERPASS}
     repo manifest -r  > ${MFST_PRE}
   else
     echo ">>> No relevant changes"
