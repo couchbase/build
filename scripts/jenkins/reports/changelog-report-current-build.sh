a6b674110cbd3e03cc977a1958be9af03cda4eeb
Phil Labee <philip@couchbase.com>
2013-11-06 13:20:14 -0800

    CBLT-82:  import build script from legacy jenkins
    Change-Id: Ib769d045d1780f809f6e2d5fb34be36c92c8f299

---
+++ scripts/jenkins/reports/changelog-report-current-build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#          
+#          run by jenkins job 'changelog-report-current-build'
+#          
+#          with paramters:  
+#               
+#               BUILDER      -- e.g. ubuntu-1204-x86-master-builder
+#               BRANCH       -- 
+#               CURRENT_BLD  -- 
+#               
+set -e
+env | grep -v password | grep -v passwd | sort
+echo =========================================
+echo ____________starting___`date`
+
+REPORT_DIR=${WORKSPACE}/${BUILDER}
+if [[ ! -d ${REPORT_DIR} ]] ; then mkdir ${REPORT_DIR} ; fi
+
+PROPFILE=${REPORT_DIR}/last_good.properties
+echo see ${PROPFILE}
+
+${WORKSPACE}/scripts/cgi/get_latest_good_build.pl -b ${BUILDER} -r ${BRANCH} | tee ${PROPFILE}
+sudo chmod 666 ${PROPFILE}
+
+export FIRST_BLD=`grep BUILD_NUMBER ${PROPFILE} | awk -F- '{print $2}'`
+
+export LAST_BLD=${REPORT_DIR}/current.xml
+
+if [[ ! -f ${LAST_BLD} ]]
+    then
+    echo ""
+    echo "ERROR: Failed to locate current manifest:"
+    echo ""
+    echo "    ${LAST_BLD}"
+    echo ""
+    echo "Verify that buildbot uploaded this file."
+    echo ""
+    exit 99
+fi
+
+DO_SENDIT=0
+PFORM_ARG=""
+PFORM_REX='([a-z]*-[a-z0-9]*)-[a-z0-9]*-builder'
+PFORM_REX='(ubuntu-x64)-[a-z0-9]*-builder'
+
+if [[ ${BUILDER} =~ ${PFORM_REX} ]]
+    then
+    PLATFORM=${BASH_REMATCH[1]}
+    PFORM_ARG="-P ${PLATFORM}"
+    echo "----------------- builder ${BUILDER} is on platform ${PFORM_ARG} -----------------"
+    DO_SENDIT=1
+fi
+
+BLDNUM_ARG=""
+if [[ ${CURRENT_BLD} ]] ; then BLDNUM_ARG="-b ${CURRENT_BLD}" ; fi
+
+${WORKSPACE}/scripts/jenkins/changelog-report.sh -d ${REPORT_DIR} -e ${DO_SENDIT}  ${BLDNUM_ARG}  ${PFORM_ARG}
+
