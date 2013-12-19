1c8220698d47623754705a5b63f273e7cc6d9066
Phil Labee <philip@couchbase.com>
2013-12-19 15:01:07 -0800

    CBLT-166:  call make.bash for each platform, add linux-386 and darwin-amd64
    Change-Id: Id7d5ec16cf7dc7b9e0165ce16e5280ed8d67509d

--- scripts/setup/go/setup_cross_compile.sh
+++ scripts/setup/go/setup_cross_compile.sh
@@ -19,10 +19,12 @@ echo -------- step 3
 export CGO_ENABLED=0 
 
  echo ..............windows........amd64
+ CGO_ENABLED=0 GOOS=windows GOARCH=amd64   ./make.bash --no-clean
  CGO_ENABLED=0 GOOS=windows GOARCH=amd64   go tool dist install -v pkg/runtime
  CGO_ENABLED=0 GOOS=windows GOARCH=amd64   go install -v -a std
 
  echo ..............windows........386
+ CGO_ENABLED=0 GOOS=windows GOARCH=386     ./make.bash --no-clean
  CGO_ENABLED=0 GOOS=windows GOARCH=386     go tool dist install -v pkg/runtime
  CGO_ENABLED=0 GOOS=windows GOARCH=386     go install -v -a std
 
@@ -30,17 +32,34 @@ echo -------- step 4
 export CGO_ENABLED=0 
 
  echo ..............linux.............ARM
+ CGO_ENABLED=0 GOOS=linux  GOARCH=arm          ./make.bash --no-clean
  CGO_ENABLED=0 GOOS=linux  GOARCH=arm          go tool dist install -v pkg/runtime
  CGO_ENABLED=0 GOOS=linux  GOARCH=arm          go install -v -a std
 
+echo -------- step 5
+export CGO_ENABLED=0 
+
+ echo ..............linux.............386
+ CGO_ENABLED=0 GOOS=linux  GOARCH=386          ./make.bash --no-clean
+ CGO_ENABLED=0 GOOS=linux  GOARCH=386          go tool dist install -v pkg/runtime
+ CGO_ENABLED=0 GOOS=linux  GOARCH=386          go install -v -a std
+
  echo ..............linux.............ARM5
+ CGO_ENABLED=0 GOOS=linux  GOARCH=arm GOARM=5  ./make.bash --no-clean
  CGO_ENABLED=0 GOOS=linux  GOARCH=arm GOARM=5  go tool dist install -v pkg/runtime
  CGO_ENABLED=0 GOOS=linux  GOARCH=arm GOARM=5  go install -v -a std
 
  echo ..............freebsd...........amd64
+ CGO_ENABLED=0 GOOS=freebsd    GOARCH=amd64    ./make.bash --no-clean
  CGO_ENABLED=0 GOOS=freebsd    GOARCH=amd64    go tool dist install -v pkg/runtime
  CGO_ENABLED=0 GOOS=freebsd    GOARCH=amd64    go install -v -a std
 
+echo -------- step 6
+export CGO_ENABLED=0 
+ echo ..............darwin............amd64
+ CGO_ENABLED=0 GOOS=darwin     GOARCH=amd64    ./make.bash --no-clean
+ CGO_ENABLED=0 GOOS=darwin     GOARCH=amd64    go tool dist install -v pkg/runtime
+ CGO_ENABLED=0 GOOS=darwin     GOARCH=amd64    go install -v -a std
 
 echo ......... now back
 popd                 2>&1 > /dev/null
