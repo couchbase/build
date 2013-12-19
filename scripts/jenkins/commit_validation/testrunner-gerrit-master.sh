8f31ea8219a22845a5e334ef1e6e74dccacd15c3
Phil Labee <philip@couchbase.com>
2013-12-19 10:11:36 -0800

    MB-9767:  add some markers to output
    Change-Id: Ib370fc9692ec6924dabe5938d765d2cf82b7e523

--- scripts/jenkins/commit_validation/testrunner-gerrit-master.sh
+++ scripts/jenkins/commit_validation/testrunner-gerrit-master.sh
@@ -8,19 +8,25 @@
 
 source ~jenkins/.bash_profile
 set -e
+ulimit -a
 
 echo ============================================ `date`
 env | grep -iv password | grep -iv passwd | sort
 
-ulimit -a
+echo ============================================ clean
 rm -rf testrunner/cluster_run.log
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
+
+echo ============================================ update testrunner
 cd testrunner
 git reset --hard HEAD
 git fetch ssh://review.couchbase.org:29418/testrunner $GERRIT_REFSPEC && git checkout FETCH_HEAD
+
+echo ============================================ make
 cd ..
 make -j8
+echo ============================================ make simple-test
 cd testrunner
 make simple-test
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
