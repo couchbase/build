b2e581275ec2d3b1b1297869db05a37016eb2bc4
Phil Labee <philip@couchbase.com>
2014-01-10 11:00:43 -0800

    CBLT-176:  move packaging steps in from: package_sync_gateway.sh
    Change-Id: I3fb817294996c9dc54edf6bf71889f98e963de92

--- scripts/jenkins/mobile/build_sync_gateway.sh
+++ scripts/jenkins/mobile/build_sync_gateway.sh
@@ -54,6 +54,27 @@ if [[ $GOOS =~ linux   ]] ; then EXEC=sync_gateway     ;                       f
 if [[ $GOOS =~ darwin  ]] ; then EXEC=sync_gateway     ; PKGR=package-mac.rb ; fi
 if [[ $GOOS =~ windows ]] ; then EXEC=sync_gateway.exe ; PKGR=package-win.rb ; fi
 
+if [[ $DISTRO =~ centos ]] ; then PKGR=package-rpm.rb ; PKGTYPE=rpm
+    if [[ $ARCH =~ i686 ]] ; then ARCH=i386  ; fi
+fi
+
+ARCHP=${ARCH}
+if [[ $DISTRO =~ ubuntu ]] ; then PKGR=package-deb.rb ; PKGTYPE=deb
+    if [[ $ARCHP =~ 64  ]] ; then ARCHP=amd64
+                             else ARCHP=i386  ; fi
+fi
+if [[ ! $PKGR ]] 
+    then
+    echo -e "\nunsupported platform:  $DISTRO\n"
+    exit 666
+fi
+
+GOPLAT=${GOOS}-${GOARCH}
+PLATFORM=${OS}-${ARCH}
+                                  PKG_NAME=couchbase-sync-gateway_${REVISION}_${ARCHP}.${PKGTYPE}
+if [[ $DISTRO =~ macosx ]] ; then PKG_NAME=couchbase-sync-gateway_${REVISION}_${DISTRO}-${ARCH}.tar.gz
+                                                                     PLATFORM=${DISTRO}-${ARCH}         ; fi
+
 export GOOS ; export EXEC
 
 GO_RELEASE=1.2
@@ -69,6 +90,19 @@ CBFS_URL=http://cbfs.hq.couchbase.com:8484/builds
 
 BLD_DIR=${WORKSPACE}/build
 SGW_DIR=${WORKSPACE}/sync_gateway
+BLD_DIR=${SGW_DIR}/build
+
+PREFIXD=${BLD_DIR}/opt/couchbase-sync-gateway
+if [[ -e ${PREFIXD} ]] ; then rm -rf ${PREFIXD} ; fi
+mkdir -p ${PREFIXD}/bin/
+
+PREFIX=/opt/couchbase-sync-gateway
+PREFIXP=./opt/couchbase-sync-gateway
+                                                #  needed by ~/.rpmmacros 
+                                                #  called by package-rpm.rb
+                                                #
+RPM_ROOT_DIR=${BLD_DIR}/build/rpm/couchbase-sync-gateway_${REVISION}/rpmbuild/
+export RPM_ROOT_DIR
 
 cd ${WORKSPACE}
 echo ======== sync sync_gateway ===================
@@ -79,6 +113,7 @@ git pull  origin  ${GITSPEC}
 git submodule init
 git submodule update
 git show --stat
+REPO_SHA=`git log --oneline --pretty="format:%H" -1`
 
 cd ${SGW_DIR}
 echo ======== build ===============================
@@ -104,17 +139,24 @@ echo ======== test ================================
 echo .................... running test.sh
                                 ./test.sh
 
+cp ${DEST_DIR}/${EXEC}     ${PREFIXD}/bin/
+cp ${BLD_DIR}/LICENSE.txt  ${PREFIXD}
+cp ${BLD_DIR}/README.txt   ${PREFIXD}
+echo ${REVISION}         > ${PREFIXD}/VERSION.txt
+
 echo ======== package =============================
+echo ${BLD_DIR}' => ' ./${PKGR} ${PREFIX} ${PREFIXP} ${REVISION} ${REPO_SHA} ${PLATFORM} ${ARCHP}
+cd   ${BLD_DIR}
+./${PKGR} ${PREFIX} ${PREFIXP} ${REVISION} ${REPO_SHA} ${PLATFORM} ${ARCHP}
 
-pushd  ${SGW_DIR}/bin  2>&1 >/dev/null
-zip -r ${ZIP_FILE} *
 echo  ======= upload ==============================
-echo ................... uploading to ${CBFS_URL}/${ZIP_FILE}
-curl -XPUT --data-binary @${ZIP_FILE} ${CBFS_URL}/${ZIP_FILE}
-
-popd                   2>&1 >/dev/null
+cp ${PREFIXD}/${PKG_NAME} ${SGW_DIR}
+cd                        ${SGW_DIR}
+echo ................... uploading to ${CBFS_URL}/${PKG_NAME}
+curl -XPUT --data-binary @${PKG_NAME} ${CBFS_URL}/${PKG_NAME}
 
 echo  ============================================== update default value of test jobs
-${WORKSPACE}/build/scripts/cgi/set_jenkins_default_param.pl  -j mobile_functional_tests_ios_${GITSPEC}      -p SYNCGATE_VERSION  -v ${BUILD_NUMBER}
-${WORKSPACE}/build/scripts/cgi/set_jenkins_default_param.pl  -j mobile_functional_tests_android_${GITSPEC}  -p SYNCGATE_VERSION  -v ${BUILD_NUMBER}
+#${WORKSPACE}/build/scripts/cgi/set_jenkins_default_param.pl  -j mobile_functional_tests_ios_${GITSPEC}      -p SYNCGATE_VERSION  -v ${BUILD_NUMBER}
+#${WORKSPACE}/build/scripts/cgi/set_jenkins_default_param.pl  -j mobile_functional_tests_android_${GITSPEC}  -p SYNCGATE_VERSION  -v ${BUILD_NUMBER}
+echo DISABLED
 
