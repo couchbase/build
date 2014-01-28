c2a4b72efb78366a6b8977f550468b8c2bc0874c
Chris Hillery <ceej@couchbase.com>
2014-01-27 23:35:23 -0800

    More sensible initialization of last-build-attempted from last-known-good manifest
    Change-Id: I41ae6eb6824e1b6ee0985008fd8015de2030290e

--- scripts/jenkins/build_trigger/single-repo-change.py
+++ scripts/jenkins/build_trigger/single-repo-change.py
@@ -6,12 +6,19 @@ import sys
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import tostring
 
-def create_stub_manifest(manifest_file, curr_manifest):
-    new_manifest = copy.deepcopy(curr_manifest)
-    new_root = new_manifest.getroot()
-    for project in new_root.findall('project'):
-        new_root.remove(project)
-    return new_manifest
+def init_last_manifest(last_manifest_file, good_manifest):
+    """
+    If last-build-attempt manifest exists, open it; otherwise, initialize
+    if from last-known-good manifest.
+    """
+    if not os.path.isfile(last_manifest_file):
+        last_manifest = copy.deepcopy(good_manifest)
+        last_manifest.getroot().insert(0, \
+            ET.Comment("NOTE: this manifest is never built directly. It comprises a series of 'bookmarks' identifying the last tested revision of each project."))
+    else:
+        last_manifest = ET.ElementTree(file=last_manifest_file)
+
+    return last_manifest
 
 def main():
     if len(sys.argv) <= 1:
@@ -22,37 +29,36 @@ def main():
     curr_manifest = ET.ElementTree(file='.repo/manifest.xml')
     good_manifest = ET.ElementTree(\
         file='.repo/manifests/last-known-good-{}.xml'.format(sys.argv[1]))
-
-    # If last-build-attempt manifest exists, open it; otherwise, create it
-    # from current, minus all projects
-    prev_manifest_file = '.repo/manifests/last-build-attempt-{}.xml'.\
+    last_manifest_file = '.repo/manifests/last-build-attempt-{}.xml'.\
                          format(sys.argv[1])
-    if not os.path.isfile(prev_manifest_file):
-        prev_manifest = create_stub_manifest(prev_manifest_file, curr_manifest)
-    else:
-        prev_manifest = ET.ElementTree(file=prev_manifest_file)
+    last_manifest = init_last_manifest(last_manifest_file, good_manifest)
 
     # Search for a change between last-build-attempt and current
     changed_proj = None
+    last_proj = None
     for proj in curr_manifest.findall("project"):
         proj_name = proj.attrib["name"]
         proj_rev = proj.attrib["revision"]
-        prev_proj = prev_manifest.find('project[@name="{}"]'.format(proj_name))
-        if prev_proj == None or (proj_rev != prev_proj.attrib["revision"]):
-            # Found change! Copy project element into prev_manifest, and
-            # remember the changed project name
-            prev_root = prev_manifest.getroot()
-            if prev_proj != None:
-                prev_root.remove(prev_proj)
-            prev_root.append(proj)
+        last_proj = last_manifest.find('project[@name="{}"]'.format(proj_name))
+        if last_proj == None or (proj_rev != last_proj.attrib["revision"]):
+            # Found a change! Remember project name and stop iterating
             changed_proj = proj_name
             break
     else:
         # Didn't find a change - all done for this pass
         print "Did not find a changed project in manifest!"
         return 2
+
+    # QQQ handle project deletion as well
  
+    # Copy changed project element into last_manifest, overwriting if
+    # necessary, and save back to disk
     print "Found changed project {}!".format(changed_proj)
+    last_root = last_manifest.getroot()
+    if last_proj != None:
+        last_root.remove(last_proj)
+    last_root.append(proj)
+    last_manifest.write(last_manifest_file)
  
     # Construct a new manifest from the known-good manifest, with the only
     # change being the current version of changed_proj
@@ -61,10 +67,6 @@ def main():
         # Insert new project
         good_manifest.getroot().append(proj)
     else:
-        # Ensure the last-known-good revision is also different than the current
-        # (it might not be if a stub last-build-attempt manifest was made)
-        if good_proj.attrib["revision"] == proj_rev:
-            pass # QQQ should iterate back to next changed project
         good_proj.attrib["revision"] = proj_rev
 
     print tostring(good_manifest.getroot())
