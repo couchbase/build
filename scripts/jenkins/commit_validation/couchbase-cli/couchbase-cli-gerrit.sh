0dbd66780d1352cd28eca531d9293f724bb66707
elimare <eric.limare@couchbase.com>
2014-01-30 13:20:21 -0800

    Add make simple test to couchbase-cli
    Change-Id: Idadcfcb7a4c25a218e3843b52cefd25525eed134
    Reviewed-on: http://review.couchbase.org/33039
    Reviewed-by: Maria McDuff <maria.mcduff@gmail.com>
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/commit_validation/couchbase-cli/couchbase-cli-gerrit.sh
+++ scripts/jenkins/commit_validation/couchbase-cli/couchbase-cli-gerrit.sh
@@ -15,10 +15,22 @@ ulimit -a
 echo ============================================ `date`
 env | grep -iv password | grep -iv passwd | sort
   
+echo ============================================ clean
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
+make clean-xfd-hard
+
+
 echo ============================================ update cli
+cd couchbase-cli
 git fetch ssh://review.couchbase.org:29418/couchbase-cli $GERRIT_REFSPEC && git checkout FETCH_HEAD
 
-sudo easy_install --upgrade nose
-nosetests -v
+echo ============================================ make
+cd ..
+make -j4
+
+echo ============================================ make simple-test
+cd testrunner
+make simple-test
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 
 echo ============================================ `date`
