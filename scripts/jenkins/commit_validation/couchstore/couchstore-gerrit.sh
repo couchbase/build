4a2f96c8b91cd142ce1976d60a521ae2b4efceef
elimare <eric.limare@couchbase.com>
2014-01-31 15:01:49 -0800

    Add make simple-test script to couchstore project
    Change-Id: Ica6410c03b994e51d87d48aed2982d687884d0c8
    Reviewed-on: http://review.couchbase.org/33093
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/commit_validation/couchstore/couchstore-gerrit.sh
+++ scripts/jenkins/commit_validation/couchstore/couchstore-gerrit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #          
-#          run by jenkins job 'couchstore-gerrit-master'
+#          run by jenkins job:  couchstore-gerrit-master
+#                               
+#                               
 #          
 #          with no paramters
 #          
-#          triggered on Patchset Creation of repo: testrunner branch: master
+#          triggered on Patchset Creation of repo: couchstore
 
 source ~jenkins/.bash_profile
 set -e
@@ -12,17 +14,23 @@ ulimit -a
 
 echo ============================================ `date`
 env | grep -iv password | grep -iv passwd | sort
-
+  
 echo ============================================ clean
-
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
-cd cmake/couchstore
+
+
+echo ============================================ update couchstore
+cd couchstore
 git fetch ssh://review.couchbase.org:29418/couchstore $GERRIT_REFSPEC && git checkout FETCH_HEAD
 
-echo =========================================== make
-cd ../..
-make
-cd cmake/couchstore
-make test
+echo ============================================ make
+cd ..
+make -j4
+
+echo ============================================ make simple-test
+cd testrunner
+make simple-test
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 
 echo ============================================ `date`
