cf50aa91d8d3e4b86a1f81c44e666a6ef1321f76
elimare <eric.limare@couchbase.com>
2014-02-03 10:35:43 -0800

    Add make simple-test script to couchdb project
    Change-Id: Ibfb9add1566a906f0a1a1c5e8a3c5e77ff8a6fb5
    Reviewed-on: http://review.couchbase.org/33092
    Reviewed-by: Volker Mische <volker.mische@gmail.com>
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/commit_validation/couchdb/couchdb-gerrit.sh
+++ scripts/jenkins/commit_validation/couchdb/couchdb-gerrit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #          
-#          run by jenkins job 'couchbase-gerrit-master'
+#          run by jenkins job:  couchdb-gerrit-master
+#                               
+#                               
 #          
 #          with no paramters
 #          
-#          triggered on Patchset Creation of repo: testrunner branch: master
+#          triggered on Patchset Creation of repo: couchdb
 
 source ~jenkins/.bash_profile
 set -e
@@ -12,26 +14,23 @@ ulimit -a
 
 echo ============================================ `date`
 env | grep -iv password | grep -iv passwd | sort
-
+  
 echo ============================================ clean
-sudo killal -9 beam.smp epmd memcached python >/dev/null || true
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
-repo forall -c "git clean -xfd"
+
 
 echo ============================================ update couchdb
 cd couchdb
 git fetch ssh://review.couchbase.org:29418/couchdb $GERRIT_REFSPEC && git checkout FETCH_HEAD
-git log --pretty=oneline -n 10
 
 echo ============================================ make
 cd ..
 make -j4 || (make -j1 && false)
-cd couchdb
 
-cpulimit -e 'beam.smp' -l 50 &
-CPULIMIT_PID=$!
-
-PATH=$PATH:${WORKSPACE}/install/bin   make check
-kill $CPULIMIT_PID || true
+echo ============================================ make simple-test
+cd testrunner
+make simple-test
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 
 echo ============================================ `date`
