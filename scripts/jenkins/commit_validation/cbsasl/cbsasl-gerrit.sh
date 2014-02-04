0ffa906d7953216e09a125b996bc3b8192594acc
elimare <eric.limare@couchbase.com>
2014-02-04 11:51:58 -0800

    Update cbasl for make simple-test
    Change-Id: Ie4f82e61b35939e38cc2650be9397daf3f334d75
    Reviewed-on: http://review.couchbase.org/33187
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/commit_validation/cbsasl/cbsasl-gerrit.sh
+++ scripts/jenkins/commit_validation/cbsasl/cbsasl-gerrit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #          
 #          run by jenkins job 'cbsasl-gerrit-master'
+#                              cbsasl-gerrit-300
+#                              cbsasl-gerrit-250
 #          
 #          with no paramters
 #          
@@ -18,17 +20,16 @@ sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
 
 echo ============================================ update cbsasl
-cd cbsasl
+pushd cmake/cbsal 2>&1 > /dev/null
 git fetch ssh://review.couchbase.org:29418/cbsasl $GERRIT_REFSPEC && git checkout FETCH_HEAD
 
 echo ============================================ make
-cd ..
-make -j4
+popd 		2>&1 > /dev/null
+make -j4 || (make -j1 && false)
+
 echo ============================================ make simple-test
 cd testrunner
 make simple-test
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
-sleep 30
-scripts/start_cluster_and_run_tests.sh b/resources/dev-4-nodes.ini conf/py-viewmerge.conf 
 
 echo ============================================ `date`
