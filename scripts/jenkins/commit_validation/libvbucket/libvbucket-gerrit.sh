f3834e4d859888ab17982b959aa723c6f5b0d9ab
elimare <eric.limare@couchbase.com>
2014-02-04 15:32:13 -0800

    Update libvbucket to call make simple test
    Change-Id: Ibd82ffdc1702d0c3abd0d60708deb09dd186d880
    Reviewed-on: http://review.couchbase.org/33215
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Reviewed-by: Eric Limare <eric.limare@couchbase.com>
    Tested-by: Eric Limare <eric.limare@couchbase.com>

--- scripts/jenkins/commit_validation/libvbucket/libvbucket-gerrit.sh
+++ scripts/jenkins/commit_validation/libvbucket/libvbucket-gerrit.sh
@@ -18,17 +18,15 @@ sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
 
 echo ============================================ update libvbucket
-cd libvbucket
+pushd cmake/libvbucket  2>&1 > /dev/null
 git fetch ssh://review.couchbase.org:29418/libvbucket $GERRIT_REFSPEC && git checkout FETCH_HEAD
 
 echo ============================================ make
-cd ..
-make -j4
+popd                    2>&1 > /dev/null
+make -j4 || (make -j1 && false)
 echo ============================================ make simple-test
 cd testrunner
 make simple-test
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
-sleep 30
-scripts/start_cluster_and_run_tests.sh b/resources/dev-4-nodes.ini conf/py-viewmerge.conf 
 
 echo ============================================ `date`
