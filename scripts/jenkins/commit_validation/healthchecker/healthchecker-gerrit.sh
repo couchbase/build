c36a4f358191cdeec294d7a88f9c418f1676f121
elimare <eric.limare@couchbase.com>
2014-02-04 15:08:25 -0800

    update healthchecker for make simple test
    Change-Id: I7d26d3481f3a66a964526081c8a0b6ff8681dd42
    Reviewed-on: http://review.couchbase.org/33208
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Reviewed-by: Eric Limare <eric.limare@couchbase.com>
    Tested-by: Eric Limare <eric.limare@couchbase.com>

--- scripts/jenkins/commit_validation/healthchecker/healthchecker-gerrit.sh
+++ scripts/jenkins/commit_validation/healthchecker/healthchecker-gerrit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #          
 #          run by jenkins job 'healthchecker-gerrit-master'
-#          
+#                             healthchecker-gerrit-300
+#                             healthchecker-gerrit-250
 #          with no paramters
 #          
 #          triggered on Patchset Creation of repo: healthchecker branch: master
@@ -18,17 +19,16 @@ sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
 
 echo ============================================ update healthchecker
-cd healthchecker
+pushd cmake/healthchecker 2>&1 > /dev/null
 git fetch ssh://review.couchbase.org:29418/healthchecker $GERRIT_REFSPEC && git checkout FETCH_HEAD
 
 echo ============================================ make
-cd ..
-make -j4
+popd 2>&1 > /dev/null
+make -j4 || (make -j1 && false)
+
 echo ============================================ make simple-test
 cd testrunner
 make simple-test
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
-sleep 30
-scripts/start_cluster_and_run_tests.sh b/resources/dev-4-nodes.ini conf/py-viewmerge.conf 
 
 echo ============================================ `date`
