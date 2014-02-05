8da33fb14c0c88022433308af4df5590e8f8b0e7
elimare <eric.limare@couchbase.com>
2014-02-04 16:13:36 -0800

    Update moxi and portsigar for make simple test
    Change-Id: Ic35949be7d632b57ebeb823e46d3d18c92477339
    Reviewed-on: http://review.couchbase.org/33220
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Reviewed-by: Eric Limare <eric.limare@couchbase.com>
    Tested-by: Eric Limare <eric.limare@couchbase.com>

--- scripts/jenkins/commit_validation/moxi/moxi-gerrit.sh
+++ scripts/jenkins/commit_validation/moxi/moxi-gerrit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #          
 #          run by jenkins job 'moxi-gerrit-master'
+#                              moxi-gerrit-300
+#                              moxi-gerrit-250
 #          
 #          with no paramters
 #          
@@ -18,17 +20,16 @@ sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
 
 echo ============================================ update moxi
-cd moxi
+pushd cmake/moxi    2>&1 > /dev/null
 git fetch ssh://review.couchbase.org:29418/moxi $GERRIT_REFSPEC && git checkout FETCH_HEAD
 
 echo ============================================ make
-cd ..
-make -j4
+popd                2>&1 > /dev/null
+make -j4 || (make -j1 && false)
+
 echo ============================================ make simple-test
 cd testrunner
 make simple-test
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
-sleep 30
-scripts/start_cluster_and_run_tests.sh b/resources/dev-4-nodes.ini conf/py-viewmerge.conf 
 
 echo ============================================ `date`
