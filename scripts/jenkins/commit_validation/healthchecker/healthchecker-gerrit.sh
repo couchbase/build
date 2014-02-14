fe603724051191c9f197983b2d575ba73fd9f1ed
elimare <eric.limare@couchbase.com>
2014-02-13 18:01:36 -0800

    [CBD-1991]: Update healthchecker script for Jenkins job.
    Change-Id: I28ad0b8f93e3b414192ce3a7a539124850eef8be
    Reviewed-on: http://review.couchbase.org/33525
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Reviewed-by: Eric Limare <eric.limare@couchbase.com>
    Tested-by: Eric Limare <eric.limare@couchbase.com>

--- scripts/jenkins/commit_validation/healthchecker/healthchecker-gerrit.sh
+++ scripts/jenkins/commit_validation/healthchecker/healthchecker-gerrit.sh
@@ -17,17 +17,20 @@ env | grep -iv password | grep -iv passwd | sort
 echo ============================================ clean
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 
-git clean -xfd
+make clean-xfd-hard
+repo forall -c "git clean -xfd"
 
 echo ============================================ update healthchecker, cli, testrunner
-if [[ ! -d couchbase-cli ]] ; then git fetch ssh://review.couchbase.org:29418/couchbase-cli $GERRIT_REFSPEC && git checkout FETCH_HEAD ; fi
 
 if [[ ! -d healthchecker ]] ; then git fetch ssh://review.couchbase.org:29418/healthchecker $GERRIT_REFSPEC && git checkout FETCH_HEAD ; fi
 
-if [[ ! -d testrunner ]] ; then git fetch ssh://review.couchbase.org:29418/testrunner $GERRIT_REFSPEC && git checkout FETCH_HEAD ; fi
-
 echo ============================================ make simple-test
+cd ..
+make 
 cd testrunner
+make simple-test
+cd ..
+
 Line1="clitest.healthcheckertest.HealthcheckerTests:"
 Line2="     healthchecker_test,sasl_buckets=1,doc_ops=update,GROUP=P0"
 Line3="     healthchecker_test,standard_buckets=1,doc_ops=delete,GROUP=P0"
@@ -36,6 +39,36 @@ cat > ./cbhealthchecker.conf << EOL
 ${Line1}
 ${Line2}
 ${Line3}
+EOL
+
+IP=`/sbin/ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`;
+
+echo "IP is  $IP";
+
+iniLine1=“[global]"
+iniLine2=“username:root"
+iniLine3=password:couchbase"
+iniLine4=“port:8091"
+iniLine5=“”
+iniLine6=“[servers]"
+iniLine7=“1:$IP"
+iniLine8=“"
+iniLine9=“[membase]"
+iniLine10=“rest_username:Administrator"
+iniLine11=“rest_password:password”
+
+cat > ./ini_file.ini << EOL
+${iniLine1}
+${iniLine2}
+${iniLine3}
+${iniLine4}
+${iniLine5}
+${iniLine6}
+${iniLine7}
+${iniLine8}
+${iniLine9}
+${iniLine10}
+${iniLine11}
 EOL
 
 python ./testrunner -i ini_file.ini -c cbhealthchecker.conf -p get_collectinfo=true
