bede60eafa684775f5c27bb95b24cae82e4aaf4c
Phil Labee <philip@couchbase.com>
2014-02-18 13:10:32 -0800

    CBLT-224:  64-bit framwork is in "Release 64bit-ios-universal"
    Change-Id: Ie2ffdec46223527e2f7a07d75c9ccc94fff6d823
    Reviewed-on: http://review.couchbase.org/33713
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/mobile/build_cblite_ios.sh
+++ scripts/jenkins/mobile/build_cblite_ios.sh
@@ -42,6 +42,8 @@ LIC_DEST=${ZIP_SRCD}
 
 RIO_SRCD=${BUILDDIR}/Release-ios-universal
 RIO_DEST=${ZIP_SRCD}
+R64_SRCD=${BUILDDIR}/Release\ 64bit-ios-universal
+R64_DEST=${ZIP_SRCD}
 
 REL_SRCD=${BUILDDIR}/Release
 REL_DEST=${ZIP_SRCD}
@@ -139,9 +141,11 @@ for TARGET in "CBL iOS"
     echo ============================================  64-bit target: ${TARGET} | tee -a ${LOG_FILE}
     xcodebuild -target "${TARGET}" -configuration 'Release 64bit'               | tee -a ${LOG_FILE}
 done
-cp  -r   ${RIO_SRCD}/CouchbaseLite.framework         ${RIO_DEST}/CouchbaseLite.64bit.framework
-cp  -r   ${RIO_SRCD}/CouchbaseLiteListener.framework ${RIO_DEST}/CouchbaseLiteListener.64bit.framework
-rm -rf   ${RIO_DEST}/CouchbaseLite.64bit.framework/PrivateHeaders
+cp  -r   ${R64_SRCD}/CouchbaseLite.framework         ${R64_DEST}/CouchbaseLite.64bit.framework
+cp  -r   ${R64_SRCD}/CouchbaseLiteListener.framework ${R64_DEST}/CouchbaseLiteListener.64bit.framework
+rm -rf   ${R64_DEST}/CouchbaseLite.64bit.framework/PrivateHeaders
+
+cd       ${R64_DEST}/CouchbaseLite.64bit.framework  && file CouchbaseLite
 
 echo  ============================================== package ${ZIP_FILE}
 cd       ${ZIP_SRCD}
