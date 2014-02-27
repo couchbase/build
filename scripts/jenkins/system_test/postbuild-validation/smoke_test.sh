a537771b133c1af8221cf46408b37c17b42e1c1b
elimare <eric.limare@couchbase.com>
2014-02-27 15:06:19 -0800

    [CBD-1249]: Fixed syntax error.
    Change-Id: I1ec6cdccf45f45e5cc0d50ec1fabb3517b1c3390
    Reviewed-on: http://review.couchbase.org/34002
    Reviewed-by: Eric Limare <eric.limare@couchbase.com>
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/system_test/postbuild-validation/smoke_test.sh
+++ scripts/jenkins/system_test/postbuild-validation/smoke_test.sh
@@ -14,7 +14,7 @@ env | grep -iv password | grep -iv passwd | sort
 
 echo ============================================ sync up testrunner, build
 
-if [[ ! -d build ]] ; then git clone https://github.com/couchbaselabs/build.git ; fi
+if [[ ! -d build ]] ; then git clone https://github.com/couchbase/build.git ; fi
 
 echo "The current working directory $(pwd)"
 
@@ -24,17 +24,17 @@ echo "The current working directory $(pwd)."
 
 if [ "$1" = "--centos64" ]
     then
-     config.ini="centos-6-x86.ini"
+       configini="centos-6-x86.ini"
 fi
 
-cp ./build/scripts/jenkins/system_test/postbuild-validation/${config.ini} ./testrunner/b/resources
+cp ./build/scripts/jenkins/system_test/postbuild-validation/${configini} ./testrunner/b/resources
 
 pushd testrunner  2>&1 > /dev/null
 
 
-python ./scripts/install.py -i ./b/resources/${config.ini} -p version=3.0.0-395,product=cb,parallel=True,vbuckets=1024
+python ./scripts/install.py -i ./b/resources/${configini} -p version=3.0.0-398,product=cb,parallel=True,vbuckets=1024
 
 echo "The current working directory $(pwd) "
-python testrunner -i b/resources/${config.ini} -c conf/simple.conf
+python testrunner -i b/resources/${configini} -c conf/simple.conf
 
 
