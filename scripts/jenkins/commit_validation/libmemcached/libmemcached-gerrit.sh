ea0cf13f8e6628f87ef99fe40014951d157b0c80
elimare <eric.limare@couchbase.com>
2014-03-10 11:15:24 -0700

    [CBD-1191]: Initial checkins for libmemcached and memcached commit validation. Change-Id: Ia04182516ab7ff1c1178f4a6bf864d02fde6ea4f Reviewed-on: http://review.couchbase.org/34343 Reviewed-by: Eric Limare <eric.limare@couchbase.com> Tested-by: Eric Limare <eric.limare@couchbase.com>

---
+++ scripts/jenkins/commit_validation/libmemcached/libmemcached-gerrit.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# run by jenkins job 'libmemcached-gerrit'
+#
+#
+# triggered on Patchset Creation of repo: libmemcached
+
+source ~jenkins/.bash_profile
+set -e
+ulimit -a
+
+echo ============================================ `date`
+env | grep -iv password | grep -iv passwd | sort
+
+echo ============================================ clean
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
+make clean-xfd-hard
+
+echo ============================================ update memcached
+pushd  cmake/libmemcached 2>&1 > /dev/null
+git fetch ssh://review.couchbase.org:29418/libmemcached $GERRIT_REFSPEC && git checkout FETCH_HEAD
+
+echo ============================================ make
+popd 2>&1 > /dev/null
+make -j4
+echo ============================================ make simple-test
+cd testrunner
+make simple-test
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
+
+echo ============================================ `date`
+
+
+
+
+
+
+
