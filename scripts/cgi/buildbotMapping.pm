ccd2ace81c504d98edf51b4f2d8624bb96c0e0d1
Phil Labee <philip@couchbase.com>
2014-03-11 19:37:29 -0700

    CBD-1239: use win2008-x86-30-builder-01 for master win-32 builds
    Change-Id: If8ff9bdd6fd04a648906c2f12fa262d1c90822e3
    Reviewed-on: http://review.couchbase.org/34407
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/cgi/buildbotMapping.pm
+++ scripts/cgi/buildbotMapping.pm
@@ -133,7 +133,7 @@ my %buildbots = ( "production" => { "centos-5"   => { 32 => { "1.8.1"  =>  "cent
                                                             "2.5.0"  =>  "cs-win2008-x86-20-builder-250",
                                                             "2.5.1"  =>  "cs-win2008-x86-20-builder-251",
                                                             "3.0.0"  =>  "cs-win2008-x86-20-builder-300",
-                                                            "master" =>  "cs-win2008-x86-20-builder-master",
+                                                            "master" =>  "cs-win2008-x86-30-builder-01-master",
                                                           },
                                                     64 => { "1.8.1"  =>  "win-x64-181-builder",
                                                             "2.0.0"  =>  "cs-win2008-x64-20-builder",
