34dc4cb1f06ecd4d81c22372676b8887c0c44237
Phil Labee <philip@couchbase.com>
2014-03-19 13:41:32 -0700

    CBLT-202:  set default value of BLD_TO_RELEASE in upload_cblite_android_artifacts jobs
    Change-Id: I75753b0a2f2527c231871b8d4e3126227515e012
    Reviewed-on: http://review.couchbase.org/34696
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/mobile/build_cblite_android.sh
+++ scripts/jenkins/mobile/build_cblite_android.sh
@@ -185,5 +185,10 @@ curl -XPUT --data-binary @${WORKSPACE}/${DOCS_ZIP} ${CBFS_URL}/${DOCS_ZIP}
 
 echo ============================================ removing couchbase-sync-gateway
 sudo dpkg --remove   couchbase-sync-gateway     || true
+
+echo ============================================ setting default value of BLD_TO_RELEASE in upload_cblite_android_artifacts_${GITSPEC}
+
+${WORKSPACE}/build/scripts/cgi/set_jenkins_default_param.pl -j upload_cblite_android_artifacts_${GITSPEC} -p BLD_TO_RELEASE -v ${REVISION}
+
 echo ============================================ `date`
 
