14d7cc0e340fb39d9d58dae27417354cf801ed25
Trond Norbye <trond.norbye@gmail.com>
2014-04-08 10:58:57 +0200

    Make the debug printouts easier to parse
    Change-Id: I97baa39799ae87c47c707ced30d44e61ce73b5e3
    Reviewed-on: http://review.couchbase.org/35445
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- scripts/jenkins/commit_validation/memcached/memcached-gerrit.sh
+++ scripts/jenkins/commit_validation/memcached/memcached-gerrit.sh
@@ -9,23 +9,46 @@ source ~jenkins/.bash_profile
 set -e
 ulimit -a
 
-echo ============================================ `date`
+cat <<EOF
+============================================
+===                `date "+%H:%M:%S"`              ===
+============================================
+EOF
 env | grep -iv password | grep -iv passwd | sort
 
-echo ============================================ clean
+cat <<EOF
+============================================
+===               clean                  ===
+============================================
+EOF
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
 
-echo ============================================ update memcached
+cat <<EOF
+============================================
+===          update memcached            ===
+============================================
+EOF
 pushd  memcached 2>&1 > /dev/null
 git fetch ssh://review.couchbase.org:29418/memcached $GERRIT_REFSPEC && git checkout FETCH_HEAD
 
-echo ============================================ make
+cat <<EOF
+============================================
+===               Build                  ===
+============================================
+EOF
 popd 2>&1 > /dev/null
-make all install
+make all
+if [ -d build ]
+then
+   make install
+fi
 
-echo ============================================ run unit tests
-
+cat <<EOF
+============================================
+===          Run unit tests              ===
+============================================
+EOF
 if [ -d build/memcached ]
 then
    pushd build/memcached 2>&1 > /dev/null
@@ -36,9 +59,17 @@ fi
 make test
 popd 2>&1 > /dev/null
 
-echo ============================================ make simple-test
+cat <<EOF
+============================================
+===         Run end to end tests         ===
+============================================
+EOF
 cd testrunner
 make simple-test
-sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 
-echo ============================================ `date`
+cat <<EOF
+============================================
+===                `date "+%H:%M:%S"`              ===
+============================================
+EOF
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
