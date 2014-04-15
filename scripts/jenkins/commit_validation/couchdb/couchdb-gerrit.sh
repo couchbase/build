323734c3cc3e203e3d5b7c22ec26a5989b6d1b5d
elimare <eric.limare@couchbase.com>
2014-04-15 13:54:20 -0700

    CBD-1346: clean up .repo (for 1 file)
    Change-Id: I1e050dc1219aa72beb53d9c8b027750de3f37fb5
    Reviewed-on: http://review.couchbase.org/35739
    Reviewed-by: Eric Limare <eric.limare@couchbase.com>
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/commit_validation/couchdb/couchdb-gerrit.sh
+++ scripts/jenkins/commit_validation/couchdb/couchdb-gerrit.sh
@@ -91,3 +91,11 @@ cat <<EOF
 ============================================
 EOF
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
+
+## Cleanup .repo directory
+
+if [ -d ${WORKSPACE}/.repo ]
+then
+  rm -rf ${WORKSPACE}/.repo
+fi
+
