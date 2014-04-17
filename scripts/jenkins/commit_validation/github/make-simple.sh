ccdc67a367d84e1bae94dcab40b5e8fc25c0f73f
Chris Hillery <ceej@couchbase.com>
2014-04-17 12:04:16 -0700

    Adding plain make-simple job for github validation
    Change-Id: I831014dbdedc87353d37237f9ad8488ffa24ce5c
    Reviewed-on: http://review.couchbase.org/35976
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ scripts/jenkins/commit_validation/github/make-simple.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#          run by jenkins job 'make-simple-github'
+#
+#          with no paramters
+#
+#          triggered every three hours
+#
+
+source ~jenkins/.bash_profile
+set -e
+ulimit -a
+
+echo ============================================ `date`
+env | grep -iv password | grep -iv passwd | sort
+
+echo ============================================ clean
+rm -rf testrunner/cluster_run.log
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
+make clean-xfd-hard
+repo forall -c "git clean -xfd"
+
+echo ============================================ make
+make all install
+echo ============================================ make simple-test
+cd testrunner
+make simple-test
+sudo killall -9 beam.smp epmd memcached python >/dev/null || true
+zip cluster_run_log cluster_run.log
+echo ============================================ `date`
