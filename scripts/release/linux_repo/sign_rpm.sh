21b0446efcf66a4f98910c1f7feda0b35ff8923f
elimare <eric.limare@couchbase.com>
2014-05-07 15:46:17 -0700

    MB-6972: Fix make yum work by using createrepo after signing
    Change-Id: I9583bed68fd68a985a921363116861bae7a022be
    Reviewed-on: http://review.couchbase.org/36806
    Reviewed-by: Eric Limare <eric.limare@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Eric Limare <eric.limare@couchbase.com>

--- scripts/release/linux_repo/sign_rpm.sh
+++ scripts/release/linux_repo/sign_rpm.sh
@@ -71,6 +71,9 @@ rpm --resign -D "_signature gpg" -D "_gpg_name ${RPM_GPG_KEY_V3}" ${REPO}/5/i386
 rpm --resign -D "_signature gpg" -D "_gpg_name ${RPM_GPG_KEY_V4}" ${REPO}/6/x86_64/couchbase-server-${EDITION}_${BASEVER}.x86_64.rpm 
 rpm --resign -D "_signature gpg" -D "_gpg_name ${RPM_GPG_KEY_V4}" ${REPO}/6/i386/couchbase-server-${EDITION}_${BASEVER}.i386.rpm 
 
+createrepo --update  ${REPO}/5/x86_64
+createrepo --update  ${REPO}/6/x86_64
+
 gpg --batch --yes -u ${RPM_GPG_KEY_V3} --detach-sign --armor ${REPO}/5/x86_64/repodata/repomd.xml
 gpg --batch --yes -u ${RPM_GPG_KEY_V3} --detach-sign --armor ${REPO}/5/i386/repodata/repomd.xml
 gpg --batch --yes -u ${RPM_GPG_KEY_V4} --detach-sign --armor ${REPO}/6/x86_64/repodata/repomd.xml
