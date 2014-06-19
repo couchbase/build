4c928c86117699401ef46c4566e3b38e0608b62b
Phil Labee <philip@couchbase.com>
2014-06-19 11:38:45 -0700

    CBLT-319:  fix package name for macosx sgw prepare-release stage
    Change-Id: I93ed47d24371d96b2358f8e019b3aea3be2d7019
    Reviewed-on: http://review.couchbase.org/38499
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/mobile/prepare_release_sgw.sh
+++ scripts/jenkins/mobile/prepare_release_sgw.sh
@@ -44,24 +44,24 @@ EDITION=${3}
 
 if [[ ${EDITION} =~ 'community' ]]
   then
-    SGW_SRC_LIST="                                                                \
-                 couchbase-sync-gateway_${BLD_NUM}_i386-community.rpm       \
-                 couchbase-sync-gateway_${BLD_NUM}_x86_64-community.rpm      \
-                 couchbase-sync-gateway_${BLD_NUM}_i386-community.deb         \
-                 couchbase-sync-gateway_${BLD_NUM}_amd64-community.deb         \
-                 couchbase-sync-gateway_${BLD_NUM}_macosx_x86_64-community.rpm  \
-                 setup_couchbase-sync-gateway_${BLD_NUM}_x86-community.rpm       \
-                 setup_couchbase-sync-gateway_${BLD_NUM}_amd64-community.rpm      \
+    SGW_SRC_LIST="                                                           \
+                 couchbase-sync-gateway_${BLD_NUM}_i386-community.rpm         \
+                 couchbase-sync-gateway_${BLD_NUM}_x86_64-community.rpm        \
+                 couchbase-sync-gateway_${BLD_NUM}_i386-community.deb           \
+                 couchbase-sync-gateway_${BLD_NUM}_amd64-community.deb           \
+                 couchbase-sync-gateway_${BLD_NUM}_macosx-x86_64-community.tar.gz \
+                 setup_couchbase-sync-gateway_${BLD_NUM}_x86-community.rpm         \
+                 setup_couchbase-sync-gateway_${BLD_NUM}_amd64-community.rpm        \
                  "
 else
-    SGW_SRC_LIST="                                                      \
-                 couchbase-sync-gateway_${BLD_NUM}_i386.rpm       \
-                 couchbase-sync-gateway_${BLD_NUM}_x86_64.rpm      \
-                 couchbase-sync-gateway_${BLD_NUM}_i386.deb         \
-                 couchbase-sync-gateway_${BLD_NUM}_amd64.deb         \
-                 couchbase-sync-gateway_${BLD_NUM}_macosx_x86_64.rpm  \
-                 setup_couchbase-sync-gateway_${BLD_NUM}_x86.rpm       \
-                 setup_couchbase-sync-gateway_${BLD_NUM}_amd64.rpm      \
+    SGW_SRC_LIST="                                                 \
+                 couchbase-sync-gateway_${BLD_NUM}_i386.rpm         \
+                 couchbase-sync-gateway_${BLD_NUM}_x86_64.rpm        \
+                 couchbase-sync-gateway_${BLD_NUM}_i386.deb           \
+                 couchbase-sync-gateway_${BLD_NUM}_amd64.deb           \
+                 couchbase-sync-gateway_${BLD_NUM}_macosx-x86_64.tar.gz \
+                 setup_couchbase-sync-gateway_${BLD_NUM}_x86.rpm         \
+                 setup_couchbase-sync-gateway_${BLD_NUM}_amd64.rpm        \
                  "
 fi
 
