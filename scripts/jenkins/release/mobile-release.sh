e2e5fb92028ad0ce109e11e31b3482c62f108669
Phil Labee <philip@couchbase.com>
2014-06-20 16:04:02 -0700

    CBLT-319:  pkg prefix:  couchbase-sync-gateway-{edition}
    Change-Id: I770aef26ae0e82cd6ba9878854c323431f8044b1
    Reviewed-on: http://review.couchbase.org/38623
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/release/mobile-release.sh
+++ scripts/jenkins/release/mobile-release.sh
@@ -80,7 +80,7 @@ if  [[ ${product} == 'sync_gateway' ]]
     PREFIX="couchbase-sync-gateway"
     pkg_ends="x86_64.rpm  x86.rpm  x86_64.tar.gz  x86_64.deb  x86.deb  x86_64.exe  x86.exe"
     
-    for end in ${pkg_ends[@]} ; do pkgs="$pkgs ${PREFIX}_${version}_${end}" ; done
+    for end in ${pkg_ends[@]} ; do pkgs="$pkgs ${PREFIX}-${edition}_${version}_${end}" ; done
     
     s3_relbucket="s3://packages.couchbase.com/releases/couchbase-sync-gateway/${rel_dir}"
 fi
