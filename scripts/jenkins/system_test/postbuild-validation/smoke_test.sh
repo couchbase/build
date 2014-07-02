58db7a5f76a058ee2af1a634798cc6ea7cc0a072
Phil Labee <philip@couchbase.com>
2014-07-02 13:56:23 -0700

    CBD-1249:  add support for centos-6-x64
    Change-Id: I8a4649083ff99fce22fad22e6d7d7f2229af3490
    Reviewed-on: http://review.couchbase.org/39058
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/system_test/postbuild-validation/smoke_test.sh
+++ scripts/jenkins/system_test/postbuild-validation/smoke_test.sh
@@ -1,40 +1,55 @@
 #!/bin/bash
 #
-# run by jenkins job 'postbuild-validation'
-# 
+# run by jenkins job:  'smoke-test-4nodes-centos64'
+#                      'smoke-test-4nodes-ubuntu64'
+#                       etc.
 # 
 #
-
-source ~jenkins/.bash_profile
+source ~/.bash_profile
 set -e
 ulimit -a
+
+function usage
+    {
+    echo -e "\nuse:  ${0}   platform  build_number\n\nwhere\n"
+    echo -e "   platform      is one of:       centos-6-64, ubuntu-6-64, ...\n"
+    echo -e "   build_number  is of the form:  3.0.0-920\n\n"
+    }
+
+if [[ ! ${1} ]] ; then usage ; exit 99 ; fi
+OS_ARCH=${1}
+
+if [[ ! ${2} ]] ; then usage ; exit 88 ; fi
+VERSION=${2}
+
+CONFIG_SRC=$(pwd)
+
+AUT_DIR=${WORKSPACE}/testrunner
+CONFIG_DEST=${AUT_DIR}/b/resources/
+
+if [[ $OS_ARCH =~ centos-6-64 ]] ; then CONFIG=${OS_ARCH}.ini ; fi
+if [[ $OS_ARCH =~ centos-6-86 ]] ; then CONFIG=${OS_ARCH}.ini ; fi
+if [[ $OS_ARCH =~ ubuntu-6-64 ]] ; then CONFIG=${OS_ARCH}.ini ; fi
+if [[ $OS_ARCH =~ ubuntu-6-86 ]] ; then CONFIG=${OS_ARCH}.ini ; fi
+
+if [[ !                  ${CONFIG} ]] ; then echo -e "\n\nUnsupported platform: ${OS_ARCH}\n"               ; usage ; exit 1 ; fi
+if [[ ! -e ${CONFIG_SRC}/${CONFIG} ]] ; then echo -e "\n\nMissing config file:  ${CONFIG_SRC}/${CONFIG}\n"  ; usage ; exit 2 ; fi
+
 
 echo ============================================ `date`
 env | grep -iv password | grep -iv passwd | sort
 
-echo ============================================ sync up testrunner, build
+echo ============================================ sync up testrunner
+if [[ ! -d ${AUT_DIR} ]] ; then git clone https://github.com/couchbase/testrunner.git ${AUT_DIR}; fi
+cd         ${AUT_DIR}
+git checkout      master
+git pull  origin  master
+git show --stat
 
-if [[ ! -d build ]] ; then git clone https://github.com/couchbase/build.git ; fi
+cp ${CONFIG_SRC}/${CONFIG}  ${CONFIG_DEST}
 
-echo "The current working directory $(pwd)"
+python ${AUT_DIR}/scripts/install.py -i ${CONFIG_DEST}/${CONFIG} -p version=${VERSION},product=cb,parallel=True,vbuckets=16
 
-if [[ ! -d testrunner ]] ; then git clone https://github.com/couchbase/testrunner.git ; fi
-
-echo "The current working directory $(pwd)."
-
-if [ "$1" = "--centos64" ]
-    then
-       configini="centos-6-x86.ini"
-fi
-
-cp ./build/scripts/jenkins/system_test/postbuild-validation/${configini} ./testrunner/b/resources
-
-pushd testrunner  2>&1 > /dev/null
-
-
-python ./scripts/install.py -i ./b/resources/${configini} -p version=${LATEST_BUILD},product=cb,parallel=True,vbuckets=16
-
-echo "The current working directory $(pwd) "
-python testrunner -i b/resources/${configini} -c conf/simple.conf
+python ${AUT_DIR}/testrunner         -i ${CONFIG_DEST}/${CONFIG} -c conf/simple.conf
 
 
