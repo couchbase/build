5d4527de65481b6ab1a382ed924fd6dedc0a6c98
Chris Hillery <ceej@couchbase.com>
2014-08-12 03:19:23 -0700

    Add restart_builddocker.py script
    Change-Id: I9a911c75f8ad565229d6a32f7c818d753ef24e9b
    Reviewed-on: http://review.couchbase.org/40529
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ docker/scripts/restart_builddocker.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import sys
+import urllib2
+import json
+import time
+import os
+from subprocess import call, check_output
+
+if len(sys.argv) < 3:
+    print "Usage: {0} <image name> <buildslave name> [<port>]".format(sys.argv[0])
+    sys.exit(2)
+
+image = sys.argv[1]
+slave = sys.argv[2]
+if len(sys.argv) > 3:
+    port = sys.argv[3]
+else:
+    port = "2201"
+
+# First ensure that volume-container is created
+print "Checking for volume container..."
+volumect = "buildslave-volume-container"
+devnull = open(os.devnull, "w")
+result = call(["docker", "inspect", volumect], stdout=devnull, stderr=devnull)
+if result != 0:
+    print "Creating volume container..."
+    output = check_output(
+        ["docker", "run", "--name={0}".format(volumect),
+         "--volume=/home/couchbase/grommit:/home/buildbot/grommit",
+         "ceejatec/naked-ubuntu:10.04"])
+
+# See if buildbot thinks the slave is connect
+print "Seeing if {0} is connected to buildbot master...".format(slave)
+slaveurl = 'http://builds.hq.northscale.net:8010/json/slaves/{0}'
+while True:
+    response = urllib2.urlopen(slaveurl.format(slave))
+    slavedata = json.load(response)
+
+    if (not slavedata["connected"]) or (len(slavedata["runningBuilds"]) == 0):
+        break
+
+    print "Slave {0} is currently busy, waiting 60 seconds...".format(slave)
+    time.sleep(60)
+
+# See if slave is running locally
+print "Checking if {0} is running locally...".format(slave)
+result = call(["docker", "inspect", slave], stdout=devnull, stderr=devnull)
+if result == 0:
+    print "Killing {0}".format(slave)
+
+    output = check_output(["docker", "rm", "-f", slave])
+    if output.strip() != slave:
+        print "Stopped slave had wrong name, but continuing to start new..."
+
+# Finally, create new slave container
+print "Creating new {0} container...".format(slave)
+output = check_output(
+    ["docker", "run", "--name={0}".format(slave), "--detach=true",
+     "--publish={0}:22".format(port), "--volumes-from={0}".format(volumect),
+     image])
+print "Result: {0}".format(output)
+
+
