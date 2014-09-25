692ec0565f2b323bee1a6a247d7854164bae761e
Chris Hillery <ceej@couchbase.com>
2014-09-25 13:26:11 -0700

    MB-11060: Parameterize build for x86/amd64 architectures CBD-1366: Report errors and abort
    Change-Id: I889908d53a23949971416f4b6129856426608c89
    Reviewed-on: http://review.couchbase.org/41661
    Reviewed-by: Philip Labee <philip@couchbase.com>
    Tested-by: Philip Labee <philip@couchbase.com>

--- scripts/jenkins/couchbase_server/server-windows-build.bat
+++ scripts/jenkins/couchbase_server/server-windows-build.bat
@@ -9,6 +9,7 @@ set BUILD_NUMBER=%RELEASE%-%BLD_NUM%
 set VOLTRON_BRANCH=%3
 set MANIFEST=%4
 set LICENSE=%5
+set ARCHITECTURE=%6
 
 if "%MANIFEST:~0,3%" == "toy" (
     rem                       # strip off "toy-" from beginning and ".xml" from end:
@@ -24,25 +25,15 @@ rem #### set PUT_CMD=s3cmd --config=c:\Users\Administrator\s3cmd.ini put --no-pr
 rem #### set CHK_CMD=s3cmd --config=c:\Users\Administrator\s3cmd.ini ls
 rem #### set PKGSTORE="s3://packages.northscale.com/latestbuilds/%RELEASE%/"
 
-rem Detect 32-bit or 64-bit OS
-set RegQry=HKLM\Hardware\Description\System\CentralProcessor\0
-REG.exe Query %RegQry% > checkOS.txt
-find /i "x86" < CheckOS.txt > StringCheck.txt
-if %ERRORLEVEL% == 0 (
-    set target_platform=x86
-) else (
-    set target_platform=amd64
-)
-
 set
 echo ============================================== %DATE%
 
-if "%target_platform%" == "amd64" goto setup_amd64
-if "%target_platform%" == "x86"   goto setup_x86
+if "%ARCHITECTURE%" == "amd64" goto setup_amd64
+if "%ARCHITECTURE%" == "x86"   goto setup_x86
 
 :setup_x86
 echo Setting up Visual Studio environment for x86
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"       x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 goto repo_download
 
 :setup_amd64
@@ -54,8 +45,8 @@ goto repo_download
 @echo on
 if not exist couchbase mkdir couchbase
 cd couchbase
-repo init -u git://github.com/couchbase/manifest -m %MANIFEST%
-repo sync --jobs=4
+repo init -u git://github.com/couchbase/manifest -m %MANIFEST% || goto error
+repo sync --jobs=4 || goto error
 set SOURCE_ROOT=%CD%
 
 rem Unfortunately we need to have all of the directories
@@ -75,7 +66,7 @@ set PATH=%MODULEPATH%;%PATH%;%SOURCE_ROOT%\install\bin
 set OBJDIR=
 SET MODULEPATH=
 cd %SOURCE_ROOT%
-if "%target_platform%" == "amd64" set PATH=%PATH%;%SOURCE_ROOT%\install\x86\bin
+if "%ARCHITECTURE%" == "amd64" set PATH=%PATH%;%SOURCE_ROOT%\install\x86\bin
 
 echo ==============================================
 set
@@ -87,41 +78,14 @@ if "%LICENSE%" == "enterprise" (
    set BUILD_ENTERPRISE=False
 )
 
-nmake BUILD_ENTERPRISE=%BUILD_ENTERPRISE% EXTRA_CMAKE_OPTIONS="-D PRODUCT_VERSION=%BUILD_NUMBER%-%REL% -D CMAKE_ERL_LIB_INSTALL_PREFIX=lib -D CB_DOWNLOAD_DEPS=1 -D CMAKE_BUILD_TYPE=Release"
+nmake BUILD_ENTERPRISE=%BUILD_ENTERPRISE% EXTRA_CMAKE_OPTIONS="-D PRODUCT_VERSION=%BUILD_NUMBER%-%REL% -D CMAKE_ERL_LIB_INSTALL_PREFIX=lib -D CB_DOWNLOAD_DEPS=1 -D CB_DOWNLOAD_DEPS_ARCH=%ARCHITECTURE% -D CB_DOWNLOAD_DEPS_CACHE=\cbdepscache-%ARCHITECTURE% -D CMAKE_BUILD_TYPE=Release" || goto error
 
 cd ..
-if exist voltron goto voltron_exists
-    git clone --branch %VOLTRON_BRANCH% git://10.1.1.210/voltron.git
-:voltron_exists
-
-cd voltron
-git reset --hard
-git clean -dfx
-git fetch
-git checkout    %VOLTRON_BRANCH%
-git pull origin %VOLTRON_BRANCH%
-
-:package_win
-echo ======== package =============================
-ruby server-win.rb %SOURCE_ROOT%\install 5.10.4 couchbase_server %BUILD_NUMBER% %LICENSE% %target_platform%
-
-set PKG_SRC_DIR=%WORKSPACE%\voltron\couchbase_server\%RELEASE%\%BLD_NUM%
-set PKG_SRC_NAME=couchbase_server-%LICENSE%-windows-%target_platform%-%BUILD_NUMBER%.exe
-
-if "%MANIFEST:~0,3%" == "toy" (
-    set PKG_DEST_NAME=couchbase_server-%OWNER%-%LICENSE%-windows-%target_platform%-%BUILD_NUMBER%.exe
-) else (
-    set PKG_DEST_NAME=%PKG_SRC_NAME%
-)
-copy %PKG_SRC_DIR%\%PKG_SRC_NAME% %WORKSPACE%\%PKG_DEST_NAME%
-
-rem #### echo ======== upload ==============================
-rem #### :upload_to_s3
-rem #### echo %PUT_CMD% %WORKSPACE%\%PKG_DEST_NAME% %PKGSTORE%/%PKG_DEST_NAME%
-rem ####      %PUT_CMD% %WORKSPACE%\%PKG_DEST_NAME% %PKGSTORE%/%PKG_DEST_NAME%
-rem #### 
-rem #### echo %CHK_CMD% %PKGSTORE%/%PKG_DEST_NAME%
-rem ####      %CHK_CMD% %PKGSTORE%/%PKG_DEST_NAME%
-
 echo ============================================== %DATE%
+goto eof
+
+:error
+echo Failed with error %ERRORLEVEL%.
+exit /b %ERRORLEVEL%
+
 :eof
