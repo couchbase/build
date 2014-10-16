b0491c452acb6e08b6a7ddd675deef5e3977717a
Volker Mische <volker.mische@gmail.com>
2014-10-16 22:30:59 +0200

    Add commit validation for geocouch
    On every commit dialyzer, the geocouch unit tests and `make simple-test`
    will be run.
    
    Change-Id: I8af4a338a2c134ab640a835a5198e72db959b5ed
    Reviewed-on: http://review.couchbase.org/42296
    Tested-by: Volker Mische <volker.mische@gmail.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- scripts/jenkins/commit_validation/geocouch/geocouch-gerrit.sh
+++ scripts/jenkins/commit_validation/geocouch/geocouch-gerrit.sh
@@ -1,35 +1,62 @@
 #!/bin/bash
-#
-#          run by jenkins job 'geocouch-gerrit-master'
-#
-#          with no paramters
-#
-#          triggered on Patchset Creation of repo: geocouch branch: master
 
+# run by jenkins job 'geocouch-gerrit-master'
+# with no paramters
+# triggered on Patchset Creation of repo: geocouch branch: master
 
 source ~jenkins/.bash_profile
 set -e
 ulimit -a
 
-echo ============================================ `date`
+echo ============================================ `date --iso-8601=seconds`
 env | grep -iv password | grep -iv passwd | sort
 
-echo ============================================ clean
+echo ============================================ clean \
+`date --iso-8601=seconds`
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
 
-echo ============================================ update geocouch
+echo ============================================ update geocouch \
+`date --iso-8601=seconds`
 cd geocouch
-git fetch ssh://review.couchbase.org:29418/geocouch $GERRIT_REFSPEC && git checkout FETCH_HEAD
-
-echo ============================================ make
+git fetch ssh://review.couchbase.org:29418/geocouch $GERRIT_REFSPEC && \
+git checkout FETCH_HEAD
 cd ..
-make -j4 all install
-echo ============================================ make simple-test
+
+echo ============================================ make \
+`date --iso-8601=seconds`
+make -j4
+
+echo ============================================ run dialyzer \
+`date --iso-8601=seconds`
+# Copy geocouch.plt from /tmp to ${WORKSPACE}/build/geocouch to gain build time
+if [ -f /tmp/geocouch.plt ]
+then
+    cp /tmp/geocouch.plt ${WORKSPACE}/build/geocouch/
+fi
+
+cd build
+make geocouch-dialyzer
+cd ..
+
+# Copy geocouch.plt from ${WORKSPACE}/build/geocouch back to /tmp so it
+# can be restored
+if [ -f ${WORKSPACE}/build/geocouch/geocouch.plt ]
+then
+    cp ${WORKSPACE}/build/geocouch/geocouch.plt /tmp/
+fi
+
+echo ============================================ run geocouch unit tests \
+`date --iso-8601=seconds`
+make geocouch-build-for-testing -j4
+cd build/geocouch-for-tests
+make test
+cd ../../
+
+echo ============================================ make simple-test \
+`date --iso-8601=seconds`
 cd testrunner
 make simple-test
-sudo killall -9 beam.smp epmd memcached python >/dev/null || true
-sleep 30
-scripts/start_cluster_and_run_tests.sh b/resources/dev-4-nodes.ini conf/py-viewmerge.conf
+cd ..
 
-echo ============================================ `date`
+echo ============================================ `date --iso-8601=seconds`
