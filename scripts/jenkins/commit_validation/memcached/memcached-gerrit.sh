44b9151c8cec356667ae78a59d2048794a6fe39d
Dave Rigby <daver@couchbase.com>
2014-10-30 11:08:31 +0000

    [Commit validation] memcached-gerrit.sh: Update to master branch, multple improvements
    Cleanup and enhance the memcached commit validation build, making it
    fit to run on every memcached checkin. Makes a number of improvements:
    
    - remove parallel-unfriendly functionality.
    - Run tests in parallel
    - Improve output formatting
    - Echo main commands to console
    - Remove cleanup of .repo - just slows down subsequent syncs
    - Show test output on failure
    - Generate xUnit results file
    - Use ccache if available
    
    This script (checkout proposed patchm, build whole tree & run
    memcached unit tests) now takes approx 2m40s.
    
    Change-Id: Id3923f94c6dbc867150041ed6dfcdf3a86803e16
    Reviewed-on: http://review.couchbase.org/42766
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Dave Rigby <daver@couchbase.com>

--- scripts/jenkins/commit_validation/memcached/memcached-gerrit.sh
+++ scripts/jenkins/commit_validation/memcached/memcached-gerrit.sh
@@ -5,78 +5,73 @@
 #
 # triggered on Patchset Creation of repo: memcached
 
+# How many jobs to run in parallel by default?
+PARALLELISM="${PARALLELISM:-8}"
+
 source ~jenkins/.bash_profile
 set -e
-ulimit -a
+
+# CCACHE is good - use it if available.
+export PATH=/usr/lib/ccache:$PATH
+
+function echo_cmd {
+    echo \# "$@"
+    "$@"
+}
 
 cat <<EOF
+
 ============================================
-===                `date "+%H:%M:%S"`              ===
+===            environment               ===
 ============================================
 EOF
+ulimit -a
+echo ""
 env | grep -iv password | grep -iv passwd | sort
 
 cat <<EOF
+
 ============================================
 ===               clean                  ===
 ============================================
 EOF
-sudo killall -9 beam.smp epmd memcached python >/dev/null || true
-make clean-xfd-hard
+echo_cmd make clean-xfd-hard
 
 cat <<EOF
+
 ============================================
 ===          update memcached            ===
 ============================================
 EOF
-pushd  memcached 2>&1 > /dev/null
-git fetch ssh://review.couchbase.org:29418/memcached $GERRIT_REFSPEC && git checkout FETCH_HEAD
+pushd memcached 2>&1 > /dev/null
+echo_cmd git fetch ssh://review.couchbase.org:29418/memcached $GERRIT_REFSPEC
+echo_cmd git checkout FETCH_HEAD
+popd 2>&1 > /dev/null
 
 cat <<EOF
+
 ============================================
 ===               Build                  ===
 ============================================
 EOF
-popd 2>&1 > /dev/null
-make all
-if [ -d build ]
-then
-   make install
-fi
+echo_cmd make -j${PARALLELISM}
 
 cat <<EOF
+
 ============================================
 ===          Run unit tests              ===
 ============================================
 EOF
 if [ -d build/memcached ]
 then
-   pushd build/memcached 2>&1 > /dev/null
+   TEST_DIR=build/memcached
 else
-   pushd memcached 2>&1 > /dev/null
+   TEST_DIR=memcached
 fi
-
-make test
+pushd ${TEST_DIR} 2>&1 > /dev/null
+# -j${PARALLELISM} : Run tests in parallel.
+# -T Test   : Generate XML output file of test results.
+# "|| true" : Needed to ensure that xUnit scanner is run even if one or more
+#             tests fail.
+echo_cmd make test ARGS="-j${PARALLELISM} --output-on-failure --no-compress-output -T Test" || true
 popd 2>&1 > /dev/null
-
-cat <<EOF
-============================================
-===         Run end to end tests         ===
-============================================
-EOF
-cd testrunner
-make simple-test
-
-cat <<EOF
-============================================
-===                `date "+%H:%M:%S"`              ===
-============================================
-EOF
-sudo killall -9 beam.smp epmd memcached python >/dev/null || true
-
-## Cleanup .repo directory
-
-if [ -d ${WORKSPACE}/.repo ]
-then
-  rm -rf ${WORKSPACE}/.repo
-fi
