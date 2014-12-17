a047f5a8a81854d99a03d9cbf79137e13032c6f0
Chris Hillery <ceej@couchbase.com>
2014-12-16 16:46:04 -0800

    Sherlock trigger script
    Change-Id: I728d67d864d64a5b9f4e176675eba99ca3fdf11c
    Reviewed-on: http://review.couchbase.org/44407
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ scripts/jenkins/couchbase_server/sherlock-trigger.sh
@@ -0,0 +1,21 @@
+# Launched by the job "repo-sherlock" when a change to the sherlock.xml
+# manifest is detected.
+
+# Checkout out the build-team-manifests project to save the build manifest
+# for downstream builds.
+if [ ! -d build-team-manifests ]
+then
+    git clone ssh://git@github.com/couchbase/build-team-manifests
+fi
+
+# Save the build manifest.
+repo manifest -r > build-team-manifests/sherlock.xml
+cd build-team-manifests
+git add sherlock.xml
+msg="Sherlock build ${BUILD_NUMBER} at "`date`
+git commit -m $msg
+git push
+
+# If this script is executing, a change was detected. Time to update the mirror!
+cd /home/buildbot/reporef
+repo sync --jobs=6
