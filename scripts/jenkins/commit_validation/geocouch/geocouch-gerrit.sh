f3f362ceb1bb6e137803d77edab9d4711ef9a0e0
Volker Mische <volker.mische@gmail.com>
2015-01-27 13:47:11 +0100

    Cleanup geocouch commit validation script
    Cleanup the geocouch commit validation script in several ways
    
     - output all the script commands `set -x`
     - the output of the date is no longer needed as Jenkins has the
       timestamp plugin installed
    
    Change-Id: I88927e75f63b63f44e280502565928e8dc3a42f4
    Reviewed-on: http://review.couchbase.org/45922
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Volker Mische <volker.mische@gmail.com>

--- scripts/jenkins/commit_validation/geocouch/geocouch-gerrit.sh
+++ scripts/jenkins/commit_validation/geocouch/geocouch-gerrit.sh
@@ -6,29 +6,50 @@
 
 source ~jenkins/.bash_profile
 set -e
-ulimit -a
+set -x
 
-echo ============================================ `date --iso-8601=seconds`
+# CCACHE is good - use it if available.
+export PATH=/usr/lib/ccache:$PATH
+
+
+cat <<EOF
+============================================
+===              environment             ===
+============================================
+EOF
+ulimit -a
 env | grep -iv password | grep -iv passwd | sort
 
-echo ============================================ clean \
-`date --iso-8601=seconds`
+cat <<EOF
+============================================
+===                 clean                ===
+============================================
+EOF
 sudo killall -9 beam.smp epmd memcached python >/dev/null || true
 make clean-xfd-hard
 
-echo ============================================ update geocouch \
-`date --iso-8601=seconds`
+cat <<EOF
+============================================
+===            update geocouch           ===
+============================================
+EOF
 cd geocouch
 git fetch ssh://review.couchbase.org:29418/geocouch $GERRIT_REFSPEC && \
 git checkout FETCH_HEAD
 cd ..
 
-echo ============================================ make \
-`date --iso-8601=seconds`
+cat <<EOF
+============================================
+===                 build                ===
+============================================
+EOF
 make -j4
 
-echo ============================================ run dialyzer \
-`date --iso-8601=seconds`
+cat <<EOF
+============================================
+===             run dialyzer             ===
+============================================
+EOF
 # Copy geocouch.plt from /tmp to ${WORKSPACE}/build/geocouch to gain build time
 if [ -f /tmp/geocouch.plt ]
 then
@@ -46,17 +67,21 @@ then
     cp ${WORKSPACE}/build/geocouch/geocouch.plt /tmp/
 fi
 
-echo ============================================ run geocouch unit tests \
-`date --iso-8601=seconds`
+cat <<EOF
+============================================
+===        run geocouch unit tests       ===
+============================================
+EOF
 make geocouch-build-for-testing -j4
 cd build/geocouch-for-tests
 make test
 cd ../../
 
-echo ============================================ make simple-test \
-`date --iso-8601=seconds`
+cat <<EOF
+============================================
+===           make simple-test           ===
+============================================
+EOF
 cd testrunner
 make simple-test
 cd ..
-
-echo ============================================ `date --iso-8601=seconds`
