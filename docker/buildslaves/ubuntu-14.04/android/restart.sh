a3e6e5dd5bfca2b5be4bbc794d6d161b2f86126a
Chris Hillery <ceej@couchbase.com>
2015-02-06 18:26:03 -0800

    Test new tagging convention for Docker containers (from Michael)
    Change-Id: I6330f891b23b7d306a3407e0838437dd3779978c
    Reviewed-on: http://review.couchbase.org/46556
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/buildslaves/ubuntu-14.04/android/restart.sh
+++ docker/buildslaves/ubuntu-14.04/android/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker rm -f mobile-android
-docker run --name="mobile-android" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2422:22 -d ceejatec/mobile-android-jenkinsdocker
+docker run --name="mobile-android" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2422:22 -d ceejatec/ubuntu1404-mobile-android-docker:3a1d2.feb04
 
