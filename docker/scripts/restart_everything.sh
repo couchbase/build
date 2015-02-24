ed5d651b681f0065cb44cfde0f9e59d578b39e28
Chris Hillery <ceej@couchbase.com>
2015-02-24 01:49:29 -0800

    Restart Ubuntu 14.04 SGW buildslave
    Change-Id: I97a0badcf28d90fbcf04617f4f6779fafcfee1a8
    Reviewed-on: http://review.couchbase.org/47264
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_everything.sh
+++ docker/scripts/restart_everything.sh
@@ -12,5 +12,6 @@ echo @@@@@@@@@@@@@@@@@@@@@@
 ./restart_all_builddockers.sh
 ./restart_cv_builders.sh
 ../buildslaves/centos-65/sgw_jenkins/restart.sh
+../buildslaves/ubuntu-14.04/sgw_jenkins/restart.sh
 ../buildslaves/ubuntu-14.04/android/restart.sh
 
