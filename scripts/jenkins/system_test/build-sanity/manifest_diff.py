28847e6628fd4de31114503bab56ce9bb3fa6759
hkodungallur <hari.kodungallur@couchbase.com>
2015-03-11 22:28:13 -0700

    - use optionparser - option to create email list of committers
    Change-Id: I454df293ec10d9848487dd69346e29ab9a444bd1
    Reviewed-on: http://review.couchbase.org/48143
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- scripts/jenkins/system_test/build-sanity/manifest_diff.py
+++ scripts/jenkins/system_test/build-sanity/manifest_diff.py
@@ -3,10 +3,14 @@ import os
 import sys
 import subprocess
 import xml.etree.ElementTree as ET
+from optparse import OptionParser
+
 
 #
 # Given two manifests, find the changelog for each project that has changed
 # from the first manifest to the second
+# Optionally also create a file called committers_email that has email ids
+# of all the committers
 #
 # ASSUMPTIONS & LIMITATIONS:
 # * Assumes that repo has synced the code for all the projects relative to
@@ -17,17 +21,6 @@ import xml.etree.ElementTree as ET
 # * Assumes branch/upstream doesn't change between the two manifests
 #
 
-
-if len(sys.argv) < 3:
-    print 'Usage: manifest_diff.py <manifest1> <manifest2>'
-    print
-    sys.exit(1)
-
-manifest1=sys.argv[1]
-manifest2=sys.argv[2]
-
-manifest1_dict = {}
-manifest2_dict = {}
 
 def parse_manifest(manifest, dict_values):
     tree = ET.parse(manifest)
@@ -40,17 +33,49 @@ def parse_manifest(manifest, dict_values):
             path = p.attrib['path']
         dict_values[name] = (path, rev)
 
-parse_manifest(manifest1, manifest1_dict)
-parse_manifest(manifest2, manifest2_dict)
+def main(manifest1, manifest2, create_email_file):
+    manifest1_dict = {}
+    manifest2_dict = {}
+    email_list = []
 
-for key in manifest1_dict.keys():
-    if manifest2_dict.has_key(key):
-        man1 = manifest1_dict[key]
-        man2 = manifest2_dict[key]
-        if man1[1] != man2[1]:
-            print 'CHANGELOG FOR: %s\n' %key
-            command='cd {0}; git log --name-only {1}..{2}'.format(man1[0], man1[1], man2[1])
-            p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
-            (child_stdout, child_stdin) = (p.stdout, p.stdin)
-            print child_stdout.read()
-            print 
+    parse_manifest(manifest1, manifest1_dict)
+    parse_manifest(manifest2, manifest2_dict)
+
+    for key in manifest1_dict.keys():
+        if manifest2_dict.has_key(key):
+            man1 = manifest1_dict[key]
+            man2 = manifest2_dict[key]
+            if man1[1] != man2[1]:
+                print 'CHANGELOG FOR: %s\n' %key
+                command='cd {0}; git log --name-only {1}..{2}'.format(man1[0], man1[1], man2[1])
+                p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+                (child_stdout, child_stdin) = (p.stdout, p.stdin)
+                print child_stdout.read()
+                print 
+                if create_email_file:
+                    command="cd {0}; git log --pretty=format:'%ae' {1}..{2}".format(man1[0], man1[1], man2[1])
+                    p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+                    (child_stdout, child_stdin) = (p.stdout, p.stdin)
+                    output = child_stdout.read()
+                    for line in output.split():
+                        if line not in email_list:
+                            email_list.append(line)
+
+        if create_email_file and email_list:
+            with open('committers_email', 'w') as F:
+                for email in email_list:
+                    F.write('%s ' %email)
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-o", "--old-manifest", dest="old_manifest",
+                      help="manifest file for the previous build number", metavar="MANIFEST FILE")
+    parser.add_option("-n", "--new-manifest", dest="new_manifest",
+                      help="manifest file for the current build number", metavar="MANIFEST FILE")
+    parser.add_option("-e", "--email-file",
+                      action="store_true", dest="email_file", default=False,
+                      help="create a committers_email file with emails of the committers from the generated changelog")
+
+    (options, args) = parser.parse_args()
+    if options.old_manifest and options.new_manifest:
+        main(options.old_manifest, options.new_manifest, options.email_file)
