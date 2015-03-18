63b372afb6c2ff90dd1388d75b07735a7d160919
Chris Hillery <ceej@couchbase.com>
2015-03-17 17:13:57 -0700

    Don't care if there's nothing to delete
    Change-Id: I73ae9790e4c4f36a7d006652fc49060305a6a610
    Reviewed-on: http://review.couchbase.org/48424
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- scripts/jenkins/couchbase_server/sherlock-trigger.sh
+++ scripts/jenkins/couchbase_server/sherlock-trigger.sh
@@ -22,7 +22,7 @@ git commit --allow-empty -m "$msg"
 git push origin HEAD:master
 
 # Also save it with the right name for uploading to latestbuilds.
-rm ${WORKSPACE}/*.xml
+rm -f ${WORKSPACE}/*.xml
 cp sherlock.xml ${WORKSPACE}/couchbase-server-${VERSION}-${BUILD_NUMBER}-manifest.xml
 
 # Save the new commit SHA and the build number for downstream jobs in
