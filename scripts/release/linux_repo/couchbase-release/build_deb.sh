bf5bba066c4ea40f87618830737d829137bfcf68
hkodungallur <hari.kodungallur@couchbase.com>
2015-07-20 00:16:30 -0700

    wrong gpg file name fixed
    Change-Id: I6de8aed0e4c9309a56ae443436d9bc29595cd4b8
    Reviewed-on: http://review.couchbase.org/53409
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- scripts/release/linux_repo/couchbase-release/build_deb.sh
+++ scripts/release/linux_repo/couchbase-release/build_deb.sh
@@ -3,7 +3,7 @@ thisdir=`dirname $0`
 pushd $thisdir
 
 mkdir -p deb/couchbase-release-1.0-0/etc/apt/trusted.gpg.d/
-cp -p GPG-KEY-COUCHBASE-1.0-0 deb/couchbase-release-1.0-0/etc/apt/trusted.gpg.d/
+cp -p GPG-KEY-COUCHBASE-1.0 deb/couchbase-release-1.0-0/etc/apt/trusted.gpg.d/
 sudo chown -R root.root deb/couchbase-release-1.0-0
 dpkg-deb --build deb/couchbase-release-1.0-0/
 sudo rm -rf deb/couchbase-release-1.0-0
