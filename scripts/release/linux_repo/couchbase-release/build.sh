aeab3e674697ffd3cd5bdf6b35771dfc75e501f0
hkodungallur <hari.kodungallur@couchbase.com>
2015-07-20 00:09:54 -0700

    remove hardcode
    Change-Id: Ie5030ed011382a4d68bac47d65119da1447cdc13
    Reviewed-on: http://review.couchbase.org/53408
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- scripts/release/linux_repo/couchbase-release/build.sh
+++ scripts/release/linux_repo/couchbase-release/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+thisdir=`dirname $0`
+
 python -mplatform | grep -i ubuntu > /dev/null 2>&1
 ret=$?
 if [ $ret -eq 0 ];then
     echo Building on Ubuntu
-    build/scripts/release/linux_repo/couchbase-release/build_deb.sh
+    ${thisdir}/build_deb.sh
     exit 0
 fi
 
@@ -12,6 +14,6 @@ python -mplatform | grep -i centos > /dev/null 2>&1
 ret=$?
 if [ $ret -eq 0 ];then
     echo Building on Centos
-    build/scripts/release/linux_repo/couchbase-release/build_rpm.sh
+    ${thisdir}/build_rpm.sh
     exit 0
 fi
