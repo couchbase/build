6a08c325365fc0c6d85d31fe71e8ca54cbf3059b
hkodungallur <hari.kodungallur@couchbase.com>
2015-07-23 16:02:26 -0700

    after build, rechecout files that had to be chown-ed
    Change-Id: I23f41ef2d18779f95bbf803d2bf25c1f7957f01c
    Reviewed-on: http://review.couchbase.org/53631
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- scripts/release/linux_repo/couchbase-release/build_deb.sh
+++ scripts/release/linux_repo/couchbase-release/build_deb.sh
@@ -6,6 +6,8 @@ mkdir -p deb/couchbase-release-1.0-0/etc/apt/trusted.gpg.d/
 cp -p GPG-KEY-COUCHBASE-1.0 deb/couchbase-release-1.0-0/etc/apt/trusted.gpg.d/
 sudo chown -R root.root deb/couchbase-release-1.0-0
 dpkg-deb --build deb/couchbase-release-1.0-0/
+sudo rm -rf deb/couchbase-release-1.0-0
+git checkout deb/couchbase-release-1.0-0
 
 popd
 
