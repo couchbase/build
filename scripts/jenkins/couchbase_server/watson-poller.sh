828043351937992b49d945b08d64c6110e12f234
Chris Hillery <ceej@couchbase.com>
2015-08-08 01:15:27 -0700

    Adding watson-trigger.sh and watson-poller.sh
    Change-Id: I8eb656227da3c36558be28ead3f3ccca2bfc288c
    Reviewed-on: http://review.couchbase.org/54262
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ scripts/jenkins/couchbase_server/watson-poller.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ex
+# Launched by "repo-watson" to see if a new build needs to be triggered.
+#
+# Expects "PRODUCT_BRANCH" to be specified in the environment.
+
+# Clean previous artifacts.
+rm -f current.xml trigger.properties
+
+# Check out the build-team-manifests project to compare with the previous build.
+git config --global user.name "Couchbase Build Team"
+git config --global user.email "build-team@couchbase.com"
+git config --global color.ui false
+if [ ! -d build-team-manifests ]
+then
+    git clone ssh://git@github.com/couchbase/build-team-manifests
+else
+    (cd build-team-manifests; git pull)
+fi
+(cd build-team-manifests; git checkout ${PRODUCT_BRANCH})
+
+# Write out the current build manifest.
+repo manifest -r > current.xml
+
+# See if it is different than the tip of build-team-manifests.
+diffchars=`repo diffmanifests ${WORKSPACE}/current.xml ${WORKSPACE}/build-team-manifests/watson.xml --raw | wc -c`
+if [ "${diffchars}" = "0" ]
+then
+    echo "No differences since last build - not triggering downstream build"
+else
+    # Create trigger.properties with dummy content, to make Jenkins trigger
+    # the downstream build
+    echo "BUILD=yes" > trigger.properties
+    echo "Manifest differences found - triggering downstream build!"
+fi
---
+++ scripts/jenkins/couchbase_server/watson-trigger.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -ex
+# Launched by the job "watson-build" to start a new build.
+#
+# Expects "PRODUCT_BRANCH" and "VERSION" to be specified in the environment,
+# in addition to "WORKSPACE".
+
+# Checkout out the build-team-manifests project to save the build manifest
+# for downstream builds.
+git config --global user.name "Couchbase Build Team"
+git config --global user.email "build-team@couchbase.com"
+git config --global color.ui false
+if [ ! -d build-team-manifests ]
+then
+    git clone ssh://git@github.com/couchbase/build-team-manifests
+fi
+(cd build-team-manifests && git checkout ${PRODUCT_BRANCH} && git pull)
+
+# Save a copy of the previous build manifest before overwriting it.
+cp build-team-manifests/watson.xml .repo/manifests/last-build.xml
+
+# Write out the new build manifest.
+repo manifest -r > build-team-manifests/watson.xml
+
+# Play fun repo games to figure out the last build's build number, and add 1.
+repo init -m last-build.xml -g all
+bldnum=$(( `repo forall build -c 'echo $REPO__BLD_NUM'` + 1))
+
+# Update and commit the new build manifest
+cd build-team-manifests
+sed -i "s/@BLD_NUM@/${bldnum}/g" watson.xml
+git add watson.xml
+msg="Watson '${PRODUCT_BRANCH}' build ${VERSION}-${bldnum} at "`date`
+git commit --allow-empty -m "$msg"
+git push origin HEAD:${PRODUCT_BRANCH}
+
+# Also save it with the right name for uploading to latestbuilds.
+rm -f ${WORKSPACE}/*.xml
+cp watson.xml ${WORKSPACE}/couchbase-server-${VERSION}-${bldnum}-manifest.xml
+
+# Save the new commit SHA and the build number for downstream jobs in
+# "trigger.properties".
+echo -n "MANIFEST_SHA=" > ${WORKSPACE}/trigger.properties
+git rev-parse HEAD >> ${WORKSPACE}/trigger.properties
+echo "BLD_NUM=${bldnum}" >> ${WORKSPACE}/trigger.properties
+
+# Update the reporef mirror for downstream jobs that can share it.
+mkdir -p ~/reporef
+cd ~/reporef
+if [ ! -d .repo ]
+then
+  repo init -u git://github.com/couchbase/manifest -g all -m watson.xml --mirror
+fi
+repo sync --jobs=6
