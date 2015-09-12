c445f8dc4033ba63d7c71970e8a521a04fb4bc5f
hkodungallur <hari.kodungallur@couchbase.com>
2015-09-12 09:50:12 -0700

    single script to do both single node and 4 node sanity
    Change-Id: I0750fbbedaf5e7530712d1e15fb603754d93c5fe
    Reviewed-on: http://review.couchbase.org/55210
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

---
+++ scripts/jenkins/system_test/build-sanity/run_sanity.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+read -a node_list <<< $NODE_IPS
+num_nodes=${#node_list[@]}
+
+if [ $num_nodes -ne 1 -a $num_nodes -ne 4 ]; then
+    echo "Supports only a sigle node or 4 node runs"
+    exit 1
+fi
+
+
+TR_CONF="conf/py-1node-sanity.conf"
+NODE_1=${node_list[0]}
+if [ $num_nodes -eq 4 ]; then
+    NODE_2=${node_list[1]}
+    NODE_3=${node_list[2]}
+    NODE_4=${node_list[3]}
+    TR_CONF="conf/py-4node-sanity.conf"
+fi
+
+echo "[global]
+username:root
+password:couchbase
+
+[membase]
+rest_username:Administrator
+rest_password:password
+
+[_1]
+ip:${NODE_1}
+port:8091
+n1ql_port:8093
+index_port:9102
+services:kv,index,n1ql
+" > node_conf.ini
+
+if [ $num_nodes -eq 1 ]; then
+echo "[servers]
+1:_1
+" >> node_conf.ini
+
+else
+
+echo "[cluster1]
+1:_1
+2:_2
+
+[cluster2]
+1:_3
+2:_4
+
+[servers]
+1:_1
+2:_2
+3:_3
+4:_4
+
+[_2]
+ip:${NODE_2}
+port:8091
+
+[_3]
+ip:${NODE_3}
+port:8091
+
+[_4]
+ip:${NODE_4}
+port:8091
+" >> node_conf.ini
+
+fi
+
+version_number=${VERSION}-${CURRENT_BUILD_NUMBER}
+echo version=${version_number}
+
+PARAMS="version=${version_number},product=cb,parallel=True"
+if [ "x${BIN_URL}" != "x" ]; then
+  PARAMS="${PARAMS},url=$BIN_URL"
+fi
+
+COUCHBASE_NUM_VBUCKETS=64 python scripts/install.py -i node_conf.ini -p $PARAMS
+python testrunner.py -i node_conf.ini -c $TR_CONF -p get-cbcollect-info=True
