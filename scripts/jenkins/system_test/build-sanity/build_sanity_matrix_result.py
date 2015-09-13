d1e15d4b1f5daa75b440d37d697cf4789b634a7f
hkodungallur <hari.kodungallur@couchbase.com>
2015-09-13 16:49:53 -0700

    script to eval our own result for build_sanity_matrix
    for now centos 1node and 4node pass == success
    so, we need a way to look at only part of the matrix result
    
    Change-Id: I410635090f7b5fc2b7024c2cf686544f938ba33c
    Reviewed-on: http://review.couchbase.org/55212
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

---
+++ scripts/jenkins/system_test/build-sanity/build_sanity_matrix_result.py
@@ -0,0 +1,25 @@
+import sys
+import urllib2
+import json
+import re
+
+build_number = sys.argv[1]
+matrix_url = 'http://server.jenkins.couchbase.com/job/build_sanity_matrix/'
+specific_build_url = matrix_url + build_number + '/api/json'
+ret = urllib2.urlopen(specific_build_url)
+results = json.loads(ret.read())
+
+overall_result = 0
+for res in results['runs']:
+    url = res['url']
+    p = r'.*/DISTRO=([a-z0-9]*),TYPE=([a-z0-9]*).*'
+    m = re.findall(p, url)
+    if m:
+        (dis, typ) = m[0]
+        ret = urllib2.urlopen(url + 'api/json/')
+        bld_res = json.loads(ret.read())['result']
+        if dis == 'centos7' and bld_res in ['FAILURE', 'UNSTABLE']:
+            overall_result = 1
+        print '%10s / %s - %8s - %s' %(dis, typ, bld_res, url)
+
+sys.exit(overall_result)
