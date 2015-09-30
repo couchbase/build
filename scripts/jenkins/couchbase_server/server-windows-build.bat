e34b2b2eb1ca944223eecf7d8d9bc7b976b7e6f8
Dave Rigby <daver@couchbase.com>
2015-09-30 13:54:35 +0100

    server-windows-build: Improve debug output
    Restore the echoing of commands after calling tlm/win32/environment
    (as that turns off echo).
    
    Change-Id: I1e4cf76d1957e0fbe902753f79d4aec79bf3fb9a
    Reviewed-on: http://review.couchbase.org/55692
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Well-Formed: Chris Hillery <ceej@couchbase.com>

--- scripts/jenkins/couchbase_server/server-windows-build.bat
+++ scripts/jenkins/couchbase_server/server-windows-build.bat
@@ -1,6 +1,6 @@
 @echo on
 
-rem Parameters
+@rem Parameters
 
 set VERSION=%1
 set BLD_NUM=%2
@@ -9,10 +9,10 @@ set MANIFEST_FILE=%3
 set LICENSE=%4
 set ARCHITECTURE=%5
 
-rem In addition to the above arguments, this scripts expects
-rem to be launched from the top level of a sherlock repo directory.
+:: In addition to the above arguments, this scripts expects
+:: to be launched from the top level of a sherlock repo directory.
 set
-echo ======== %DATE% =============================
+@echo ======== %DATE% =============================
 
 if not exist install mkdir install
 repo manifest -r > install/manifest.txt
@@ -21,13 +21,16 @@ set target_arch=%ARCHITECTURE%
 set source_root=%CD%
 call %source_root%\tlm\win32\environment.bat
 
-echo ==============================================
-set
-echo ======== build ===============================
+:: environment.bat disables echo; re-enable it.
+@echo on
 
-rem Delete previous run go artifacts - the go compiler doesn't always
-rem rebuild the right stuff. We could run 'nmake clean' here, but that
-rem deletes the whole build directory too, which slows things down.
+@echo ==============================================
+set
+@echo ======== build ===============================
+
+:: Delete previous run go artifacts - the go compiler doesn't always
+:: rebuild the right stuff. We could run 'nmake clean' here, but that
+:: deletes the whole build directory too, which slows things down.
 rmdir /s /q godeps\pkg goproj\pkg goproj\bin
 
 if "%LICENSE%" == "enterprise" (
@@ -37,8 +40,8 @@ if "%LICENSE%" == "enterprise" (
 )
 
 set MAKETYPE="Ninja"
-rem Ninja requires that we explictly tell cmake to use cl (MSVC),
-rem otherwise it will auto-select gcc (MinGW) if that is installed.
+:: Ninja requires that we explictly tell cmake to use cl (MSVC),
+:: otherwise it will auto-select gcc (MinGW) if that is installed.
 set EXTRA_CMAKE_OPTIONS=-DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl
 
 cmake -E touch CMakeLists.txt
@@ -65,7 +68,7 @@ rem Archive all Windows debug files for future reference.
 rem Pre-clean all unnecessary files
 ruby voltron\cleanup.rb %WORKSPACE%\couchbase\install
 
-echo ============================================== %DATE%
+@echo ============================================== %DATE%
 goto eof
 
 :error
