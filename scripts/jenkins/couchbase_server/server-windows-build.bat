d843941a5d75cbf93e62f09ec552f4c62bc603c5
Dave Rigby <daver@couchbase.com>
2015-09-30 13:54:35 +0100

    server-windows-build: Build directly using CMake, using Ninja
    Change server-windows-build.bat to use Ninja[1] instead of NMake as
    the build generator.
    
    Ninja (to quote it's homepage) is ".. a small build system with a
    focus on speed." By switching to Ninja from NMake we see a significant
    improvement in build times - from 51 mins with NMake down to 18 mins
    with Ninja (server-windows-build; 3-core VM).
    
    [1]: http://martine.github.io/ninja/
    
    Change-Id: I707c463ccb0b6625d264dce6b192bf394c14a9a0
    Reviewed-on: http://review.couchbase.org/55687
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Well-Formed: Chris Hillery <ceej@couchbase.com>

--- scripts/jenkins/couchbase_server/server-windows-build.bat
+++ scripts/jenkins/couchbase_server/server-windows-build.bat
@@ -36,9 +36,28 @@ if "%LICENSE%" == "enterprise" (
    set BUILD_ENTERPRISE=False
 )
 
+set MAKETYPE="Ninja"
+rem Ninja requires that we explictly tell cmake to use cl (MSVC),
+rem otherwise it will auto-select gcc (MinGW) if that is installed.
+set EXTRA_CMAKE_OPTIONS=-DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl
+
 cmake -E touch CMakeLists.txt
 
-nmake BUILD_ENTERPRISE=%BUILD_ENTERPRISE% EXTRA_CMAKE_OPTIONS="-D PRODUCT_VERSION=%VERSION%-%BLD_NUM% -D CMAKE_ERL_LIB_INSTALL_PREFIX=lib -D CMAKE_BUILD_TYPE=RelWithDebInfo" || goto error
+mkdir build
+pushd build
+
+cmake -G %MAKETYPE% ^
+      -D CMAKE_INSTALL_PREFIX="%source_root%\install" ^
+      -D CMAKE_PREFIX_PATH=";%source_root%\install" ^
+      -D PRODUCT_VERSION=%VERSION%-%BLD_NUM% ^
+      -D BUILD_ENTERPRISE=%BUILD_ENTERPRISE% ^
+      -D CMAKE_BUILD_TYPE=RelWithDebInfo ^
+      -D CMAKE_ERL_LIB_INSTALL_PREFIX=lib ^
+      %EXTRA_CMAKE_OPTIONS% ^
+      %source_root% || goto error
+
+cmake --build . || goto error
+popd
 
 rem Archive all Windows debug files for future reference.
 7za a -tzip -mx9 -ir!*.pdb couchbase-server-%LICENSE%_%VERSION%-%BLD_NUM%-windows_%ARCHITECTURE%-PDB.zip
