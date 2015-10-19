91d3e6379f997ddee5ac26b63b6043d3712f44d5
Michael Kwok <michael.kwok@couchbase.com>
2015-10-18 22:28:00 -0700

    Upgraded to SDK-22 and added new tools NDK and CLang-3.6 for building ForestDB.
    Change-Id: If4ea989c62ee2ace0ac335dae513b3474451032c
    Reviewed-on: http://review.couchbase.org/56210
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Michael Kwok <michael.kwok@couchbase.com>
    Tested-by: Michael Kwok <michael.kwok@couchbase.com>

--- docker/buildslaves/ubuntu-14.04/android/restart.sh
+++ docker/buildslaves/ubuntu-14.04/android/restart.sh
@@ -2,12 +2,13 @@
 
 docker stop mobile-light
 docker rm -f mobile-light
-docker run --name="mobile-light" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2300:22 -d ceejatec/ubuntu1404-mobile-android-docker:3a1d2.feb04
+docker run --name="mobile-light" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2300:22 -d ceejatec/ubuntu1404-mobile-android-docker:20151001
 
 docker stop mobile-android
 docker rm -f mobile-android
-docker run --name="mobile-android" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2422:22 -d ceejatec/ubuntu1404-mobile-android-docker:3a1d2.feb04
+docker run --name="mobile-android" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2422:22 -d ceejatec/ubuntu1404-mobile-android-docker:20151001
 
 docker stop mobile-java
 docker rm -f mobile-java
-docker run --name="mobile-java" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2421:22 -d ceejatec/ubuntu1404-mobile-android-docker:3a1d2.feb04
+docker run --name="mobile-java" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2424:22 -d ceejatec/ubuntu1404-mobile-android-docker:20151001
+
