82deaf34a9cfb50a9b576fae21a9a97cfc46bc3e
Chris Hillery <ceej@couchbase.com>
2015-10-19 22:49:13 -0700

    Add script to watch for 3.0.x commits
    Change-Id: Ic51ab3cac96e4542bbd5f78070098cb0aa9540fe
    Reviewed-on: http://review.couchbase.org/56247
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Well-Formed: Chris Hillery <ceej@couchbase.com>
    Well-Formed: buildbot <build@couchbase.com>

---
+++ scripts/jenkins/reports/check-30x-branches.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Sync the primary repo
+if [ ! -d repo ]
+then
+  mkdir repo
+fi
+cd repo
+# Conveniently this does NOT delete the .repo directory itself
+rm -rf * .repo/manifest*
+repo init -u git://github.com/ceejatec/manifest -g all -m rel-3.0.x.xml
+repo sync --jobs=6
+
+repo manifest -r > rel30x.xml
+
+repo init -u git://github.com/ceejatec/manifest -g all -m real-3.0.x.xml
+repo sync --jobs=6
+
+repo manifest -r > real30x.xml
+
+repo diffmanifests `pwd`/rel30x.xml `pwd`/real30x.xml > diff-report.txt
+
+size=`wc -c < diff-report.txt`
+if [ "0" != "$size" ]
+then
+  echo "There are 3.0.x changes to investigate"
+  exit 2
+fi
+
