3662888bbf5fe8178748e350adf09f90459927b6
hkodungallur <hkodungallur@gmail.com>
2015-11-11 17:01:58 -0800

    disable log collection on macos
    macos tests are failing, likely because cbserver doesn't work
    on mavericks anymore. but don't want to disable it altogether
    as 4.1.0 looks like it may be working (still to verify).
    in any case, these logs are useless on macos at this point of
    time. it is just adding up on storage on mega
    
    Change-Id: Ifbb97206f028881dd0516cfe0203be3d4c19a7b9
    Reviewed-on: http://review.couchbase.org/56950
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- scripts/jenkins/system_test/build-sanity/run_sanity.sh
+++ scripts/jenkins/system_test/build-sanity/run_sanity.sh
@@ -127,4 +127,8 @@ if [ "$install_only" = "yes" ]; then
 fi
 
 echo "Running python testrunner.py -i node_conf.ini -c $TR_CONF -p get-cbcollect-info=True"
-python testrunner.py -i node_conf.ini -c $TR_CONF -p get-cbcollect-info=True,get-couch-dbinfo=True,skip_cleanup=True
+if [ "$DISTRO" = "macos" ]; then
+    python testrunner.py -i node_conf.ini -c $TR_CONF 
+else
+    python testrunner.py -i node_conf.ini -c $TR_CONF -p get-cbcollect-info=True,get-couch-dbinfo=True,skip_cleanup=True
+fi
