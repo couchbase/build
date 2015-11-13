c6acf099e949d759f4cd32eb5c17d1b55d598da7
hkodungallur <hkodungallur@gmail.com>
2015-11-12 17:52:40 -0800

    A few-liners to run go tests for a bunch of components
    Change-Id: Ic0b2a0a3bd8fc1cadbde12f524289f0d89c636c2
    Reviewed-on: http://review.couchbase.org/57021
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

---
+++ scripts/jenkins/couchbase_server/run-go-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir -p ~/go
+GOPATH=~/go go get -u github.com/jstemmer/go-junit-report
+export PATH=$PATH:~/go/bin
+
+export GOPATH=${WORKSPACE}/godeps:${WORKSPACE}/goproj
+
+function go_test() {
+   dir=$1
+   pushd $dir
+
+   echo "Running Go tests in dir: $dir"
+   echo 
+   go test ./... -v | go-junit-report > gotest-report.xml
+
+   popd
+}
+
+#go_test  godeps/src/github.com/blevesearch/bleve
+go_test  godeps/src/github.com/blevesearch/go-porterstemmer
+go_test  godeps/src/github.com/blevesearch/segment
+go_test  godeps/src/github.com/couchbase/blance
+go_test  godeps/src/github.com/couchbase/cbgt
+go_test  goproj/src/github.com/couchbase/query/test/filestore
