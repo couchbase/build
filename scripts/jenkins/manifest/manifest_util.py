775945444388c6a683f2fef4f44089abbbaa81b1
Chris Hillery <ceej@couchbase.com>
2015-11-30 23:05:39 -0800

    Implementation of restricted-branch-check; refactoring.
    Change-Id: I5d8443b6bdc35228b859e20c14ec901c4eb49215
    Reviewed-on: http://review.couchbase.org/57356
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Well-Formed: Chris Hillery <ceej@couchbase.com>

---
+++ scripts/jenkins/manifest/manifest_util.py
@@ -0,0 +1,56 @@
+import os
+from subprocess import check_call
+import json
+import contextlib
+
+@contextlib.contextmanager
+def remember_cwd():
+  curdir = os.getcwd()
+  try: yield
+  finally: os.chdir(curdir)
+
+def scan_manifests(manifest_repo = "couchbase"):
+  """
+  Syncs to the "manifest" project from the given repository, and
+  returns a list of metadat about all discovered manifests
+  """
+  # Sync manifest project
+  if not os.path.isdir("manifest"):
+   check_call(["git", "clone",
+     "git://github.com/{}/manifest".format(manifest_repo)])
+  with remember_cwd():
+    os.chdir("manifest")
+    print "Updating manifest repository..."
+    check_call(["git", "pull"])
+
+    # Scan the current directory for build manifests.
+    manifests = {}
+    for root, dirs, files in os.walk("."):
+    # Prune all legacy manifests, including those in the top-level dir
+      if root == ".":
+        dirs.remove(".git")
+        if "toy" in dirs: dirs.remove("toy")
+        if "released" in dirs: dirs.remove("released")
+        continue
+
+      # Load manifest metadata where specified
+      if "product-config.json" in files:
+        with open(os.path.join(root, "product-config.json"), "r") as conffile:
+          config = json.load(conffile)
+          if not "manifests" in config:
+            continue
+          for manifest in config["manifests"]:
+            manifests[os.path.join(root, manifest)[2:]] = config["manifests"][manifest]
+
+      # Identify each .xml file
+      for filename in files:
+        if filename[-4:] == ".xml":
+          # If this manifest is listed in a product-config.json, it will have
+          # already been read since we're doing a top-down walk. So if we don't
+          # find it here, initialize it with an empty dict.
+          full_filename = os.path.join(root, filename)[2:]
+          if full_filename not in manifests:
+            manifests[full_filename] = {}
+
+  return manifests
+
