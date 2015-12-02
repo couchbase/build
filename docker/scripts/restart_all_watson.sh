6488dbc4b0c080c562bcb3dd3187b10c21c26fc9
hkodungallur <hkodungallur@gmail.com>
2015-12-02 09:16:38 -0800

    CBD-1693 use paramiko-enabled version of Watson slave (centos6)
    Change-Id: Ie5678c404de84304c2dcd1816b17b5407c997d71
    Reviewed-on: http://review.couchbase.org/57420
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- docker/scripts/restart_all_watson.sh
+++ docker/scripts/restart_all_watson.sh
@@ -6,7 +6,8 @@ restart_dock() {
 }
 
 # New hostness Watson docker containers
-restart_dock ceejatec/centos-65-couchbase-build:20151020 watson-centos6 2222
+restart_dock ceejatec/centos-65-couchbase-build:20151201 watson-centos6-01 2222
+restart_dock ceejatec/centos-65-couchbase-build:20151201 watson-centos6-02 2232
 restart_dock ceejatec/ubuntu-1204-couchbase-build:latest zz-server-lightweight 3322
 restart_dock ceejatec/ubuntu-1204-couchbase-build:20151020 watson-ubuntu12.04 2223
 restart_dock ceejatec/debian-7-couchbase-build:20151020 watson-debian7 2224
