266e5caad7e241e553ce30f5a510e6bca74a1f25
hkodungallur <hkodungallur@gmail.com>
2015-12-11 14:37:56 -0800

    typo fix
    Change-Id: I859ab85080ab5b2199f5d9397f58ba143f0d28a9
    Reviewed-on: http://review.couchbase.org/57743
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- scripts/jenkins/system_test/build-sanity/run_sanity.sh
+++ scripts/jenkins/system_test/build-sanity/run_sanity.sh
@@ -118,7 +118,7 @@ if [ "$DISTRO" != "win64" ]; then
     PARAMS="${PARAMS},parallel=True"
 fi
 if [ -n "${EXTRA_INSTALL_PARAMS}" ]; then
-    EXTRA_PARAMS="${PARAMS},${EXTRA_INSTALL_PARAMS}"
+    PARAMS="${PARAMS},${EXTRA_INSTALL_PARAMS}"
 fi
 
 echo "Running: COUCHBASE_NUM_VBUCKETS=64 python scripts/install.py -i node_conf.ini -p $PARAMS"
