cf1cbac68c1890384d6b3d52e8b2a6cac3151a71
Volker Mische <volker.mische@gmail.com>
2016-01-28 15:54:48 +0100

    Checkout all projects with the same Change-Id
    If there's more than one project with a change in review with the
    same Change-Id, check it out as well. This way you can easily
    have dependencies between changes and make the build bots aware
    of it.
    
    Change-Id: Ia93bb227751e5b8645df1ec1d5b716baaa76cd01
    Reviewed-on: http://review.couchbase.org/59193
    Tested-by: Will Gardner <will.gardner@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Harsha H S <hhs.couchbase@gmail.com>
    Reviewed-by: Volker Mische <volker.mische@gmail.com>

--- scripts/jenkins/commit_validation/fetch_project.sh
+++ scripts/jenkins/commit_validation/fetch_project.sh
@@ -10,8 +10,17 @@ PROJECT=$1
 PROJECT_PATH=$2
 REFSPEC=$3
 
+if [ -z "$GERRIT_HOST" ]; then
+    echo "Error: Required environment variable 'GERRIT_HOST' not set."
+    exit 1
+fi
+if [ -z "$GERRIT_PORT" ]; then
+    echo "Error: Required environment variable 'GERRIT_PORT' not set."
+    exit 2
+fi
+
 cd $PROJECT_PATH
 git reset --hard HEAD
-git fetch ssh://review.couchbase.org:29418/$PROJECT $REFSPEC
+git fetch ssh://$GERRIT_HOST:$GERRIT_PORT/$PROJECT $REFSPEC
 git checkout FETCH_HEAD
 cd ..
