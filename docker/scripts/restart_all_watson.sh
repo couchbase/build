5028b6f9bec87613ea9fda0c0fa30b50dd56a2be
hkodungallur <hkodungallur@gmail.com>
2016-01-27 23:07:15 -0800

    restart script to use latest image (go 152)
    Change-Id: Icc270998e40f042cc945edab4b5bef3c357919cf
    Reviewed-on: http://review.couchbase.org/59174
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- docker/scripts/restart_all_watson.sh
+++ docker/scripts/restart_all_watson.sh
@@ -6,16 +6,16 @@ restart_dock() {
 }
 
 # New hostness Watson docker containers
-restart_dock ceejatec/centos-65-couchbase-build:20151201 watson-centos6-01 2222
-restart_dock ceejatec/centos-65-couchbase-build:20151201 watson-centos6-02 2232
+restart_dock ceejatec/centos-65-couchbase-build:20151223 watson-centos6-01 2222
+restart_dock ceejatec/centos-65-couchbase-build:20151223 watson-centos6-02 2232
 restart_dock ceejatec/ubuntu-1204-couchbase-build:latest zz-server-lightweight 3322
-restart_dock ceejatec/ubuntu-1204-couchbase-build:20151020 watson-ubuntu12.04 2223
-restart_dock ceejatec/debian-7-couchbase-build:20151020 watson-debian7 2224
-restart_dock ceejatec/ubuntu-1404-couchbase-build:20151020 watson-ubuntu14.04 2226
-restart_dock ceejatec/suse-11-couchbase-build:20160125 watson-suse11 2228
-restart_dock ceejatec/centos-70-couchbase-build:20151020 watson-centos7-01 2227
-restart_dock ceejatec/centos-70-couchbase-build:20151020 watson-centos7-02 2237
-restart_dock ceejatec/debian-7-couchbase-build-gcc49:20160125 watson-debian7-gcc49 2238
+restart_dock ceejatec/ubuntu-1204-couchbase-build:20151223 watson-ubuntu12.04 2223
+restart_dock ceejatec/debian-7-couchbase-build:20151223 watson-debian7 2224
+restart_dock ceejatec/ubuntu-1404-couchbase-build:20151223 watson-ubuntu14.04 2226
+restart_dock ceejatec/suse-11-couchbase-build:20151223 watson-suse11 2228
+restart_dock ceejatec/centos-70-couchbase-build:20151223 watson-centos7-01 2227
+restart_dock ceejatec/centos-70-couchbase-build:20151223 watson-centos7-02 2237
+restart_dock ceejatec/debian-8-couchbase-build:20160112 watson-debian8 2229
 
 wait
 
