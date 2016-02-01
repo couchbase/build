7aedc62666676e9a5614706c1a9cb5499eabf226
Michael Kwok <michael.kwok@couchbase.com>
2016-02-01 15:48:01 -0800

    Add Installshield support for sg-accel and new service wrapper.
    Change-Id: If69bc2109b4c8049b8477c1c63d59abf82fda6b6
    Reviewed-on: http://review.couchbase.org/59365
    Reviewed-by: Michael Kwok <michael.kwok@couchbase.com>
    Tested-by: Michael Kwok <michael.kwok@couchbase.com>

--- scripts/jenkins/mobile/build_sgw_windows.bat
+++ scripts/jenkins/mobile/build_sgw_windows.bat
@@ -1,16 +1,16 @@
 @echo off
-::          
+::
 ::    run by Jenkins Windows build jobs:
-::          
+::
 ::        sync_gateway_windows_builds
-::          
+::
 ::    with required paramters:
-::   
-::          branch_name   git_commit  version  build_number  Edition  platform     
-::             
-::    e.g.: master         123456   0.0.0 0000   community    windows-x64   
-::          release/1.0.0  123456   1.1.0 1234   enterprise   windows-x64   
-::          
+::
+::          branch_name   git_commit  version  build_number  Edition  platform
+::
+::    e.g.: master         123456   0.0.0 0000   community    windows-x64
+::          release/1.0.0  123456   1.1.0 1234   enterprise   windows-x64
+::
 set THIS_SCRIPT=%0
 
 set  GITSPEC=%1
@@ -38,7 +38,11 @@ if not defined GO_RELEASE (
 )
 
 set VERSION=%REL_VER%-%BLD_NUM%
-set LATESTBUILDS_SGW="http://latestbuilds.hq.couchbase.com/couchbase-sync-gateway/%GITSPEC%/%VERSION%"
+if "%GITSPEC%" == "0.0.0" (
+    set LATESTBUILDS_SGW="http://latestbuilds.hq.couchbase.com/couchbase-sync-gateway/%REL_VER%/%VERSION%"
+else (
+    set LATESTBUILDS_SGW="http://latestbuilds.hq.couchbase.com/couchbase-sync-gateway/0.0.1/%GITSPEC%/%VERSION%"
+)
 
 for /f "tokens=1-2 delims=-" %%A in ("%PLATFRM%") do (
     set OS=%%A
@@ -46,7 +50,10 @@ for /f "tokens=1-2 delims=-" %%A in ("%PLATFRM%") do (
 )
 
 set GOOS=%OS%
-set EXEC=sync_gateway.exe
+set SGW_EXEC=sync_gateway.exe
+set SGW_NAME=sync-gateway
+set ACCEL_EXEC=sg_accel.exe
+set ACCEL_NAME=sg-accel
 
 if "%PROC_ARCH%" == "x64" (
     set ARCH=amd64
@@ -72,13 +79,16 @@ set PKGTYPE=exe
 set PKG_NAME=setup_couchbase-sync-gateway_%VERSION%_%ARCHP%.%PKGTYPE%
 set NEW_PKG_NAME=couchbase-sync-gateway-%EDITION%_%VERSION%_%PARCH%.%PKGTYPE%
 
+set ACCEL_PKG_NAME=setup_couchbase-sg-accel_%VERSION%_%ARCHP%.%PKGTYPE%
+set ACCEL_NEW_PKG_NAME=couchbase-sg-accel-%EDITION%_%VERSION%_%PARCH%.%PKGTYPE%
+
 set GOROOT=c:\usr\local\go\%GO_RELEASE%\go
 set PATH=%PATH%;%GOROOT%\bin\
 
 set
 echo ============================================== %DATE%
 
-:: package-win is tightly coupled to Jenkins workspace. 
+:: package-win is tightly coupled to Jenkins workspace.
 :: Changes needed to support concurrent builds later
 ::set TARGET_DIR=%WORKSPACE%\%GITSPEC:/=\%\%EDITION%
 set LIC_DIR=%WORKSPACE%\build\license\sync_gateway
@@ -150,8 +160,7 @@ move     %TEMPLATE_FILE%.new   %TEMPLATE_FILE%
 cd %SGW_DIR%
 echo ======== build ===============================
 del /s/f/q bin
-echo ................. %PLAT_DIR%
-set    DEST_DIR=%SGW_DIR%\bin\%PLAT_DIR%
+set    DEST_DIR=%SGW_DIR%\bin
 mkdir %DEST_DIR%
 
 set GOPATH=%SGW_DIR%;%SGW_DIR%\vendor
@@ -163,12 +172,29 @@ if EXIST %SGW_DIR%\pkg           rmdir /s/q %SGW_DIR%\pkg
 
 go build -v github.com\couchbase\sync_gateway
 
-if NOT EXIST %SGW_DIR%\%EXEC% (
-    echo "############################# FAIL! no such file: %SGW_DIR%\%EXEC%"
+if NOT EXIST %SGW_DIR%\%SGW_EXEC% (
+    echo "############################# Sync-Gateway FAIL! no such file: %SGW_DIR%\%SGW_EXEC%"
     exit 1
     )
-move   %SGW_DIR%\%EXEC% %DEST_DIR%
-echo "..................................Success! Output is: %DEST_DIR%\%EXEC%"
+move   %SGW_DIR%\%SGW_EXEC% %DEST_DIR%
+echo "..................................Sync-Gateway Success! Output is: %DEST_DIR%\%SGW_EXEC%"
+
+if "%GITSPEC%" == "0.0.0" GOTO build_sg_accel
+if "%GITSPEC%" GEQ "1.2.0" GOTO build_sg_accel
+
+GOTO skip_build_sg_accel
+
+:build_sg_accel
+    go build -v github.com\couchbase\sync_gateway\sg_accel
+
+    if NOT EXIST %SGW_DIR%\%ACCEL_EXEC% (
+        echo "############################# SG-ACCEL FAIL! no such file: %SGW_DIR%\%ACCEL_EXEC%"
+        exit 1
+    )
+    move   %SGW_DIR%\%ACCEL_EXEC% %DEST_DIR%
+    echo "..................................SG-ACCEL Success! Output is: %DEST_DIR%\%ACCEL_EXEC%"
+
+:skip_build_sg_accel
 
 echo ======== remove build meta-data ==============
 move  %TEMPLATE_FILE%.orig  %TEMPLATE_FILE%
@@ -180,6 +206,31 @@ echo ................... running tests from test.sh
     go test -i ./...
     go test -cpu 4 -race ./...
 
+echo ======== build service wrappers ==============
+
+set ACCEL_SERVICED=%SGW_DIR%\service\sg-windows
+set ACCEL_SERVICE=%ACCEL_SERVICED%\sg-accel-service.exe
+
+if "%GITSPEC%" == "0.0.0" GOTO build_service_wrapper
+if "%GITSPEC%" GEQ "1.2.0" GOTO build_service_wrapper
+
+GOTO build_service_wrapper
+
+:build_service_wrapper
+    if EXIST %ACCEL_SERVICED%\build.cmd (
+	cd %ACCEL_SERVICED%
+	call build.cmd
+	) else (
+	echo "############################# WINDOWS SERVICE WRAPPER build FAIL! no such file: %ACCEL_SERVICED%\build.cmd"
+    )
+
+    if NOT EXIST %ACCEL_SERVICE% (
+        echo "############################# SG-ACCEL-SERVICE FAIL! no such file: %ACCEL_SERVICE%"
+        exit 1
+    )
+
+:skip_build_service_wrapper
+
 :: Jenkins seems to have a bug in the way how the slave starts (ssh vs jnlp)
 :: Starting via ssh caused an issue with parsing long strings with spaces.
 :: Temporary disable TEST_OPTIONS until slave can be started in jnlp mode.
@@ -189,9 +240,9 @@ echo ................... running tests from test.sh
 ::    go test %TEST_OPTIONS% ./...
 ::)
 
-echo ======== package =============================
-echo ".................staging files to %STAGING%" 
-copy  %DEST_DIR%\%EXEC%                 %STAGING%\bin\
+echo ======== sync-gateway package ==========================
+echo ".................staging sgw files to %STAGING%"
+copy  %DEST_DIR%\%SGW_EXEC%             %STAGING%\bin\
 copy  %BLD_DIR%\README.txt              %STAGING%\README.txt
 echo  %VERSION%                       > %STAGING%\VERSION.txt
 copy  %LIC_DIR%\LICENSE_%EDITION%.txt   %STAGING%\LICENSE.txt
@@ -204,18 +255,56 @@ unix2dos  %STAGING%\README.txt
 unix2dos  %STAGING%\VERSION.txt
 unix2dos  %STAGING%\LICENSE.txt
 
-echo %BLD_DIR%' => ' .\%PKGR% %PREFIX% %PREFIXP% %VERSION% %REPO_SHA% %PLATFORM% %ARCHP%
+echo %BLD_DIR%' => ' .\%PKGR% %PREFIX% %PREFIXP% %VERSION% %REPO_SHA% %PLATFORM% %ARCHP% %SGW_NAME%
 cd   %BLD_DIR%
-                     .\%PKGR% %PREFIX% %PREFIXP% %VERSION% %REPO_SHA% %PLATFORM% %ARCHP%
+                     .\%PKGR% %PREFIX% %PREFIXP% %VERSION% %REPO_SHA% %PLATFORM% %ARCHP% %SGW_NAME%
 
 if %ERRORLEVEL% NEQ 0 (
-    echo "############################# Installer warning!"
+    echo "############################# Sync-Gateway Installer warning!"
     )
 
-echo  ======= upload ==============================
+echo  ======= prep sync-gateway upload ==============================
 copy %STAGING%\%PKG_NAME% %SGW_DIR%\%NEW_PKG_NAME%
-cd                        %SGW_DIR%
+
+echo ======== sg-accel package step 1==========================
+set ACCEL_PREFIX=\opt\couchbase-sg-accel
+set ACCEL_PREFIXP=.\opt\couchbase-sg-accel
+set ACCEL_STAGING=%BLD_DIR%\opt\couchbase-sg-accel
+
+if "%GITSPEC%" == "0.0.0" GOTO package_sg_accel
+if "%GITSPEC%" GEQ "1.2.0" GOTO package_sg_accel
+
+GOTO skip_package_sg_accel
+
+:package_sg_accel
+    cd   %BLD_DIR%
+    if EXIST %ACCEL_PREFIX%  del /s/f/q %ACCEL_PREFIX%
+    if EXIST %ACCEL_STAGING% del /s/f/q %ACCEL_STAGING%
+
+    mkdir %ACCEL_STAGING%
+    xcopy /s %STAGING% %ACCEL_STAGING%
+
+    echo ======== sg-accel package step 2==========================
+    echo ".................staging sg-accel files to %ACCEL_STAGING%"
+    del /q %ACCEL_STAGING%\%PKG_NAME%
+    del /q %ACCEL_STAGING%\bin\%SGW_EXEC%
+    copy %DEST_DIR%\%ACCEL_EXEC%                 %ACCEL_STAGING%\bin\
+
+    echo %BLD_DIR%' => ' .\%PKGR% %ACCEL_PREFIX% %ACCEL_PREFIXP% %VERSION% %REPO_SHA% %PLATFORM% %ARCHP% %ACCEL_NAME%
+    .\%PKGR% %ACCEL_PREFIX% %ACCEL_PREFIXP% %VERSION% %REPO_SHA% %PLATFORM% %ARCHP% %ACCEL_NAME%
+
+    if %ERRORLEVEL% NEQ 0 (
+        echo "#############################  SG-ACCEL Installer warning!"
+        )
+
+    echo  ======= prep sg_accel upload ==============================
+    copy %ACCEL_STAGING%\%ACCEL_PKG_NAME% %SGW_DIR%\%ACCEL_NEW_PKG_NAME%
+
+:skip_package_sg_accel
+
+::cd                        %SGW_DIR%
 ::md5sum  %NEW_PKG_NAME%  > %NEW_PKG_NAME%.md5
+::md5sum  %ACCEL_NEW_PKG_NAME%  > %ACCEL_NEW_PKG_NAME%.md5
 echo        ........................... uploading internally to %LATESTBUILDS_SGW%
 
 echo ============================================== %DATE%
