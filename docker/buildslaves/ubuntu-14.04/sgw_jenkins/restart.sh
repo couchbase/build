b3ef1725ffacddbcb9ae4cc5131e1ff188103f6a
Michael Kwok <michael.kwok@couchbase.com>
2016-02-03 13:20:56 -0800

    Add support for GO version 1.5.3
    Change-Id: If41d4f5306ba6c1f047787255c2db49a5263bee2
    Reviewed-on: http://review.couchbase.org/59473
    Reviewed-by: Michael Kwok <michael.kwok@couchbase.com>
    Tested-by: Michael Kwok <michael.kwok@couchbase.com>

--- docker/buildslaves/ubuntu-14.04/sgw_jenkins/restart.sh
+++ docker/buildslaves/ubuntu-14.04/sgw_jenkins/restart.sh
@@ -6,5 +6,5 @@ echo "docker rm mobile-sgw-ubuntu14"
 docker rm mobile-sgw-ubuntu14 
 
 # Port number 23xx used by SGW
-docker run --name="mobile-sgw-ubuntu14" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2321:22 -d ceejatec/ubuntu1404-sgw-build:20151210
+docker run --name="mobile-sgw-ubuntu14" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2321:22 -d ceejatec/ubuntu1404-sgw-build:20160126
 
