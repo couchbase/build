794975b934a4b2413c22b345a60a3c2b0a7d2fd2
Chris Hillery <ceej@couchbase.com>
2016-02-12 16:12:15 -0800

    Don't kill old images on restart
    Change-Id: Ia4e314fbf29904b90566bc0060bb0411b74eeb2b
    Reviewed-on: http://review.couchbase.org/59902
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_cv_builders.sh
+++ docker/scripts/restart_cv_builders.sh
@@ -2,12 +2,5 @@
 
 # No longer using these - cv.jenkins uses Docker plugin to spin
 # up instances on demand. The zz-lightweight one is still necessary.
-./restart_swarmdocker.py cv ubuntu-1404 3224 http://172.23.113.52:8081 10 cv-zz-lightweight lightweight
-
-# Clean up abandoned images
-echo "Done!"
-echo
-echo "The following cleaning steps may raise errors; this is OK."
-docker ps -q -a | xargs docker rm
-docker images -q --filter "dangling=true" | xargs docker rmi
+./restart_swarmdocker.py cv ubuntu-1204 3224 http://172.23.113.52:8081 10 cv-zz-lightweight lightweight
 
