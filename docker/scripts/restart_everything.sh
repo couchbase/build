d4074d068e9e76a6dbec116d8ea9e3ce3fa31966
Chris Hillery <ceej@couchbase.com>
2016-02-16 02:18:50 -0800

    Add zz-mobile-lightweight
    Change-Id: I18b2cbd51659c3c4bf6f5f0420f6f5e0304f786a
    Reviewed-on: http://review.couchbase.org/60030
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Well-Formed: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_everything.sh
+++ docker/scripts/restart_everything.sh
@@ -11,7 +11,5 @@ echo @ Recreating slaves
 echo @@@@@@@@@@@@@@@@@@@@@@
 ./restart_all_builddockers.sh
 ./restart_cv_builders.sh
-../buildslaves/centos-65/sgw_jenkins/restart.sh
-../buildslaves/ubuntu-14.04/sgw_jenkins/restart.sh
-../buildslaves/ubuntu-14.04/android/restart.sh
+./restart_mobile.sh
 
