c3a1b2dfe2c22be04480877ffbb03dbfe82be113
Volker Mische <volker.mische@gmail.com>
2016-02-25 14:12:50 +0100

    Make view-engine commit validation work on Windows
    The commit validation scripts for Windows of the view-engine were outdated.
    They are now based on the current version of `single-projecct-gerrit.bat`.
    
    Change-Id: I3b298ef09468100d94f0725d741c0452224eff7b
    Reviewed-on: http://review.couchbase.org/60484
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Volker Mische <volker.mische@gmail.com>

--- scripts/jenkins/commit_validation/couchdb/couchdb-gerrit-views.bat
+++ scripts/jenkins/commit_validation/couchdb/couchdb-gerrit-views.bat
@@ -1,62 +1,19 @@
-@REM script for doing CV on windows for couchdb project
-@IF NOT DEFINED target_arch (
-    set target_arch=amd64
-    @echo Notice: environment variable 'target_arch' not set. Defaulting to 'amd64'.
-)
+@REM Script for doing CV on windows for couchdb project
+
+SET SKIP_UNIT_TESTS=1
 
 SET CURDIR=%~dp0
+call %CURDIR%\single-project-gerrit.bat %*
 
 @echo.
 @echo ============================================
-@echo ===    environment                       ===
+@echo ===         Run viewmerge tests          ===
 @echo ============================================
 
-set
-@echo.
-set source_root=%WORKSPACE%
-call tlm\win32\environment
-@echo on
-
-@echo.
-@echo ============================================
-@echo ===    clean                             ===
-@echo ============================================
-
-@REM Windows build is serial and there is no ccache, hence very slow.
-@REM To try to alleviate this, we *don't* perform a top-level clean, only for
-@REM the project subdirectory and Go objects (as it's incremental build is a bit flaky).
-@REM This should speed things up a little, but still
-@REM ensures that the correct number of warnings are reported for this project.
-pushd build\couchdb
-nmake clean
-popd
-del /F/Q/S godeps\pkg goproj\pkg goproj\bin
-
-@echo.
-@echo ============================================
-@echo ===    update %GERRIT_PROJECT%           ===
-@echo ============================================
-for /f "tokens=1-3" %%i in ('%CURDIR%..\alldependencies.py %GERRIT_PATCHSET_REVISION%') do (
-    call %CURDIR%..\fetch_project.bat %%i %%j %%k
-)
-
-@echo.
-@echo ============================================
-@echo ===               Build                  ===
-@echo ============================================
-
-nmake EXTRA_CMAKE_OPTIONS="" || goto :error
-
-@echo.
-@IF NOT DEFINED SKIP_UNIT_TESTS (
-    cd testrunner
-    python scripts/start_cluster_and_run_tests.sh b/resources/dev-single-node.ini conf/view-conf/py-viewmerge.conf
-    python scripts/start_cluster_and_run_tests.sh b/resources/dev-4-nodes.ini conf/view-conf/py-viewmerge.conf
-) ELSE (
-    @echo ============================================
-    @echo ===    SKIP_UNIT_TESTS set - skipping unit tests
-    @echo ============================================
-)
+cd testrunner
+python scripts/start_cluster_and_run_tests.py nmake b/resources/dev-single-node.ini conf/view-conf/py-viewmerge.conf || goto :error
+python scripts/start_cluster_and_run_tests.py nmake b/resources/dev-4-nodes.ini conf/view-conf/py-viewmerge.conf || goto :error
+cd ..
 
 :end
 exit /b 0
