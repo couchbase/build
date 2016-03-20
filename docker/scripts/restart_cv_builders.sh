5c5107b431495da0b91bfcc7ce3d7af2eb9cba3a
Chris Hillery <ceej@couchbase.com>
2016-03-19 23:28:33 -0700

    Update cv.jenkins slaves to latest image
    Change-Id: I36b459188cf828c1a78114b1fd85460c6042d52d
    Reviewed-on: http://review.couchbase.org/61751
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_cv_builders.sh
+++ docker/scripts/restart_cv_builders.sh
@@ -3,15 +3,15 @@
 # Currently these slaves are all hosted on mega2
 
 # cv.jenkins slaves
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160218 cv-zz-lightweight 3224 cv.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 cv-zz-lightweight 3224 cv.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160218 ubuntu12-cv-01 2322 cv.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-01 2322 cv.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160218 ubuntu12-cv-02 2323 cv.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-02 2323 cv.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160218 ubuntu12-cv-03 2324 cv.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-03 2324 cv.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160218 ubuntu12-cv-04 2325 cv.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-04 2325 cv.jenkins.couchbase.com &
 sleep 5
 
 # Factory CV slaves
