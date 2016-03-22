da739d0896290090f40b708d051e3c824df41975
hkodungallur <hkodungallur@gmail.com>
2016-03-21 22:28:03 -0700

    typo fix
    Change-Id: I92fb3b053cb3b7b6660c8c95851257ca879c52db
    Reviewed-on: http://review.couchbase.org/61803
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- scripts/release/linux_repo/couchbase-release/build_rpm.sh
+++ scripts/release/linux_repo/couchbase-release/build_rpm.sh
@@ -13,4 +13,4 @@ rpmbuild -bb couchbase-release.spec
 
 popd
 
-cp ~/rpmbuild/RPMS/x86_64/couchbase-release-1.0-0_x86_64.rpm couchbase-release-1.0-0-x86_64.rpm
+cp ~/rpmbuild/RPMS/x86_64/couchbase-release-1.0-0.x86_64.rpm couchbase-release-1.0-0-x86_64.rpm
