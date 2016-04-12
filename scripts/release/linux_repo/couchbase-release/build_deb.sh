d64a300ad7249c9eab2fe7739cf1adf77e9cedcd
hkodungallur <hkodungallur@gmail.com>
2016-04-11 22:45:42 -0700

    update version number in the build scripts
    Change-Id: Ieb53c3be10b36ebdc1d3f0709b96db8b5ccee783
    Reviewed-on: http://review.couchbase.org/62709
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

--- scripts/release/linux_repo/couchbase-release/build_deb.sh
+++ scripts/release/linux_repo/couchbase-release/build_deb.sh
@@ -5,7 +5,7 @@ pushd $thisdir
 VERSION=1.0-1
 
 sudo rm -rf deb/couchbase-release-$VERSION
-cp -rp deb/deb_control_files deb/couchbase-release-$VERSION
+cp -rp deb/debian_control_files deb/couchbase-release-$VERSION
 sed -e "s/%VERSION%/$VERSION/g" -i deb/couchbase-release-$VERSION/DEBIAN/control
 mkdir -p deb/couchbase-release-$VERSION/etc/apt/trusted.gpg.d/
 cp -p GPG-KEY-COUCHBASE-1.0 deb/couchbase-release-$VERSION/etc/apt/trusted.gpg.d/
