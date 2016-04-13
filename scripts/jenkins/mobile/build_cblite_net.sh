1b0a2879deefc1ae87be4d9aa557ecfffaeee016
Michael Kwok <michael.kwok@couchbase.com>
2016-04-13 15:28:11 -0700

    New build and packaging scripts for couchbase-lite-net
    Change-Id: Iea6215a64fb67156d49ae2dd544aa521b7e0ca61
    Reviewed-on: http://review.couchbase.org/62806
    Reviewed-by: Michael Kwok <michael.kwok@couchbase.com>
    Tested-by: Michael Kwok <michael.kwok@couchbase.com>

---
+++ scripts/jenkins/mobile/build_cblite_net.sh
@@ -0,0 +1,176 @@
+#!/bin/bash -ex
+#          
+#    run by jenkins couchbase-lite-net jobs:
+#          
+#    with required paramters:
+#   
+#          branch_name    framework   platform    version    bld_num   target    toolchain
+#             
+#    e.g.: master         net35       osx         1.3.0      0000      Release   mono
+#
+#    and optional parameters:
+#    
+#        REPO_SHA  --  
+#        SKIP_TEST --  
+#          
+#    ErrorCode:
+#        -1 = Incorrect input parameters
+#        -2 = Build failed
+#        -3 = Unit test failed
+#
+set -e
+
+function usage
+    {
+    echo "Incorrect parameters..."
+    echo -e "\nUsage:  ${0}   branch_name  target  platform  version  bld_num  commit_sha\n\n"
+    }
+
+if [[ "$#" < 5 ]] ; then usage ; exit 11 ; fi
+
+# enable nocasematch
+shopt -s nocasematch
+
+BRANCH=${1}
+
+FRAMEWORK=${2}
+
+PLATFORM=${3}
+
+VERSION=${4}
+
+BLD_NUM=${5}
+
+if [[ $6 ]] ; then  echo "setting REPO_SHA to $6"       ; REPO_SHA=$6       ; else REPO_SHA="no_sha"    ; fi
+if [[ $7 ]] ; then  echo "using TOOLCHAIN $7"       	; TOOLCHAIN=$7      ; else TOOLCHAIN="mono"     ; fi
+if [[ $8 ]] ; then  echo "using TARGET $8"              ; TARGET=$8         ; else TARGET="Release"     ; fi
+if [[ $9 ]] ; then  echo "setting TEST_OPTIONS to $9"   ; TEST_OPTIONS=$9   ; else TEST_OPTIONS="None"  ; fi
+
+if [[ $BRANCH =~ feature  ]]
+then
+    LATESTBUILDS=http://latestbuilds.hq.couchbase.com/couchbase-lite-net/0.0.1/${BRANCH}/${VERSION}-${BLD_NUM}
+else
+    LATESTBUILDS=http://latestbuilds.hq.couchbase.com/couchbase-lite-net/${BRANCH}/${VERSION}-${BLD_NUM}
+fi
+
+echo ============================================== `date`
+
+FRAMEWORK_DIR=${WORKSPACE}/${VERSION}/${FRAMEWORK}
+BASE_DIR=${FRAMEWORK_DIR}/couchbase-lite-net
+SRC_DIR=${BASE_DIR}/src
+BUILD_FRAMEWORK=Couchbase.Lite.${FRAMEWORK}
+BUILD_SLN=${SRC_DIR}/${BUILD_FRAMEWORK}.sln
+NATIVES_DIR=/latestbuilds/mobiledeps/cbforest/${VERSION}
+
+STAGING_DIR=${BASE_DIR}/staging
+LITE_BIN=${STAGING_DIR}/Couchbase.Lite
+LSTNR_BIN=${STAGING_DIR}/Couchbase.Lite.Listener
+LSTNR_BNJR_BIN=${STAGING_DIR}/Couchbase.Lite.Listener.Bonjour
+SQLITE_BIN=${STAGING_DIR}/Couchbase.Lite.Storage.SystemSQLite
+SQLCIPHER_BIN=${STAGING_DIR}/Couchbase.Lite.Storage.SQLCipher
+FORESTDB_BIN=${STAGING_DIR}/Couchbase.Lite.Storage.ForestDB
+
+BUILD_OUTPUT=("${LITE_BIN}" "${LSTNR_BIN}" "${LSTNR_BNJR_BIN}" "${SQLITE_BIN}" "${SQLCIPHER_BIN}" "${FORESTDB_BIN}")
+
+# disable nocasematch
+shopt -u nocasematch
+
+if [[ ! -d ${FRAMEWORK_DIR} ]] ; then  mkdir -p ${FRAMEWORK_DIR} ; fi
+cd         ${FRAMEWORK_DIR}
+echo ======== sync couchbase-lite-net ===================
+pwd
+if [[ ! -d couchbase-lite-net ]] ; then git clone https://github.com/couchbase/couchbase-lite-net.git ; fi
+cd         couchbase-lite-net
+
+git checkout --track -B ${BRANCH} origin/${BRANCH}
+
+if [ ${REPO_SHA} == "no_sha" ]
+then
+    git pull origin ${BRANCH}
+else
+    git checkout ${REPO_SHA}
+fi
+
+git submodule update --init --recursive
+git show --stat
+
+REPO_SHA=`git log --oneline --pretty="format:%H" -1`
+
+if [[ ! -d ${NATIVES_DIR} ]]
+then 
+    echo "Missing native components at ${NATIVES_DIR}" 
+    exit -2
+fi
+
+echo ======== Import Natives Dependency =============================
+cd ${NATIVES_DIR}
+dirs=(*/)                       # array of dirs
+num_dirs=${#dirs[@]}            # size of array
+last_index=$(($num_dirs -1))    # calc index of last dir
+last_dir=${dirs[$last_index]}
+
+BUILD_OPTIONS=""
+
+if [[ ${FRAMEWORK} =~ "Net45" ]] || [[ ${FRAMEWORK} =~ "Net35" ]] 
+then
+    cd ${last_dir}
+    cp -f libCBForest-Interop.so ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt
+    cp -f libCBForest-Interop.dylib ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt
+    cp -rf x86 ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt/
+    cp -rf x64 ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt/
+elif [[ ${FRAMEWORK} =~ "Android" ]]
+then
+    cd ${last_dir}
+    cp -rf x86 ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt/
+    cp -rf x86_64 ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt/
+    cp -rf mips64 ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt/
+    cp -rf arm64-v8a ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt/
+    cp -rf armeabi-v7a ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt/
+elif [[ ${FRAMEWORK} =~ "iOS" ]]
+then
+    cd ${last_dir}
+    cp -f libCBForest-Interop.a ${SRC_DIR}/StorageEngines/ForestDB/CBForest/CSharp/prebuilt
+    BUILD_OPTIONS=/p:Platform=iPhone
+fi
+
+cd ${BASE_DIR}
+echo ================ Build ==========================
+echo "Building product=${BUILD_FRAMEWORK} ${PLATFORM}"
+LOG_FILE=build_results.log
+BUILD_CMD="xbuild /p:Configuration=${TARGET} /p:Archive=true"
+
+set +e
+${BUILD_CMD} ${BUILD_OPTIONS} ${BUILD_SLN} 2>&1 >> ${LOG_FILE}
+
+echo ======== Verify Build =======================
+for bld_bin in "${BUILD_OUTPUT[@]}"
+  do
+    if [[ ! -d ${bld_bin} ]]
+    then
+        echo "..............................BUILD FAILED! No Output: ${bld_bin}"
+        exit -2
+    fi
+done
+
+echo "..............................BUILD Success!" 
+ls -l ${STAGING_DIR}
+
+echo ======== Test ================================ `date`
+echo ........................ running unit test
+if [[ ${TEST_OPTIONS} =~ "None" ]]
+then
+    echo "..............................SKIP Test!"
+else
+    echo "..............................Running Test!"
+fi
+
+test_result=$?
+if [ ${test_result} -ne "0" ]
+then
+    echo "########################### Unit Test FAILED! Results = ${test_result}"
+    exit -3
+fi
+
+echo ................................... upload ${FRAMEWORK} internally to ${LATESTBUILDS}
+
+echo ============================================== `date`
---
+++ scripts/jenkins/mobile/package_cblite_net.sh
@@ -0,0 +1,126 @@
+#!/bin/bash -ex
+#          
+#    run by jenkins couchbase-lite-net-packaging job:
+#          
+#    with required paramters:
+#   
+#          branch_name    platform    version    bld_num   target    toolchain
+#             
+#    e.g.: master         osx         1.3.0      0000      Release   mono
+#
+#    and optional parameters:
+#    
+#        REPO_SHA  --  
+#          
+#    ErrorCode:
+#        -1 = Incorrect input parameters
+#        -2 = Failed importing binary packages 
+#        -3 = Failed nuget packing
+#
+set -e
+
+function usage
+    {
+    echo "Incorrect parameters..."
+    echo -e "\nUsage:  ${0}   branch_name  target  platform  version  bld_num  commit_sha\n\n"
+    }
+
+if [[ "$#" < 3 ]] ; then usage ; exit -1 ; fi
+
+shopt -s nocasematch
+
+BRANCH=${1}
+
+VERSION=${2}
+
+BLD_NUM=${3}
+
+if [[ $4 ]] ; then  echo "setting REPO_SHA to $4"       ; REPO_SHA=$4       ; else REPO_SHA="no_sha"    ; fi
+
+if [[ $BRANCH =~ feature  ]]
+then
+    LATESTBUILDS=http://latestbuilds.hq.couchbase.com/couchbase-lite-net/0.0.1/${BRANCH}/${BLD_NUM}
+else
+    LATESTBUILDS=http://latestbuilds.hq.couchbase.com/couchbase-lite-net/${VERSION}/${BLD_NUM}
+fi
+
+echo ============================================== `date`
+
+PROD_DIR=${WORKSPACE}/${VERSION}/packaging
+BASE_DIR=${PROD_DIR}/couchbase-lite-net
+REL_DIR=${BASE_DIR}/release
+STAGING_DST=${BASE_DIR}/staging
+STAGING_SRC=/latestbuilds/couchbase-lite-net/${VERSION}/${BLD_NUM}/staging
+
+BUILD_PKGS=("Couchbase.Lite" "Couchbase.Lite.Listener" "Couchbase.Lite.Listener.Bonjour" "Couchbase.Lite.Storage.SystemSQLite" "Couchbase.Lite.Storage.SQLCipher" "Couchbase.Lite.Storage.ForestDB")
+NUGET_PKGS=("couchbase-lite" "couchbase-lite-listener" "couchbase-lite-listener-bonjour" "couchbase-lite-storage-systemsqlite" "couchbase-lite-storage-sqlcipher" "couchbase-lite-storage-forestdb")
+
+# disable nocasematch
+shopt -u nocasematch
+
+if [[ ! -d ${PROD_DIR} ]] ; then  rm -rf ${PROD_DIR} ; fi
+mkdir -p ${PROD_DIR}
+cd ${PROD_DIR}
+
+echo ======== sync couchbase-lite-net ===================
+if [[ ! -d couchbase-lite-net ]] ; then git clone https://github.com/couchbase/couchbase-lite-net.git ; fi
+
+cd ${BASE_DIR}
+if [ ${REPO_SHA} == "no_sha" ]
+then
+    git pull origin ${BRANCH}
+else
+    git checkout ${REPO_SHA}
+fi
+
+git pull origin ${BRANCH}
+git submodule update --init --recursive
+git show --stat
+
+if [ ${REPO_SHA} == "no_sha" ]
+then
+    REPO_SHA=`git log --oneline --pretty="format:%H" -1`
+fi
+
+echo ======== Import Bin Packages =============================
+if [[ -d ${STAGING_DST} ]] ; then  rm -rf ${STAGING_DST} ; fi
+cp -r ${STAGING_SRC}  ${BASE_DIR}
+
+cd ${STAGING_DST}
+
+shopt -u nocasematch
+echo ======== Verify all builds available =======================
+for pkg in "${BUILD_PKGS[@]}"
+  do
+    if [[ ! -d ${pkg} ]]
+    then
+        echo "..............................PACKAGING FAILED! Missing BUILD: ${pkg}"
+        exit -2
+    fi
+done
+
+echo ======== NUGET Packing =============================
+cd ${BASE_DIR}/packaging/nuget
+for nupkg in "${NUGET_PKGS[@]}"
+  do
+    nuget pack -BasePath ${BASE_DIR} ${nupkg}.nuspec 
+    result=$?
+    if [ ${result} -ne "0" ]
+    then
+        echo "########################### FAILED NUGET PACKING ${nupkg}: ${result}"
+        exit -3
+    fi
+done
+
+echo ======== Clean up remote staging =============================
+rm -rf ${STAGING_SRC}
+
+echo ======== Copy nuget packages for release =============================
+if [[ -d ${REL_DIR} ]] ; then  rm -rf ${REL_DIR} ; fi
+mkdir -p ${REL_DIR}
+mv ${BASE_DIR}/packaging/nuget/*.nupkg ${REL_DIR}
+ls ${REL_DIR}
+
+echo ................................... upload ${FRAMEWORK} internally to ${LATESTBUILDS}
+
+echo ============================================== `date`
