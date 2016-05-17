52efb4e2acb207274d8f113ebb0bde856daf4233
Michael Kwok <michael.kwok@couchbase.com>
2016-05-17 11:24:35 -0700

    Mount latestbuilds on CBNAS01
    Change-Id: I660894a7a3976e5ee0ff93dc499bb7414ac95b19
    Reviewed-on: http://review.couchbase.org/64130
    Reviewed-by: Michael Kwok <michael.kwok@couchbase.com>
    Tested-by: Michael Kwok <michael.kwok@couchbase.com>

--- docker/buildslaves/ubuntu-14.04/android/restart.sh
+++ docker/buildslaves/ubuntu-14.04/android/restart.sh
@@ -2,7 +2,9 @@
 
 docker stop mobile-light
 docker rm -f mobile-light
-docker run --name="mobile-light" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2300:22 -d ceejatec/ubuntu1404-mobile-android-docker:20151001
+docker run --name="mobile-light" -v /home/couchbase/jenkinsdocker-ssh:/ssh \
+        --volume=/home/couchbase/latestbuilds:/latestbuilds \
+        -p 2300:22 -d ceejatec/ubuntu1404-mobile-android-docker:20151001
 
 docker stop mobile-android
 docker rm -f mobile-android
