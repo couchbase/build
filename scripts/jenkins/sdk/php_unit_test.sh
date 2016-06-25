51ecf6306c37e833b25e52172205e7413f5a4019
Sergey Avseyev <sergey.avseyev@gmail.com>
2016-06-25 14:47:26 +0300

    Preserve core dumps after failure

--- scripts/jenkins/sdk/php_unit_test.sh
+++ scripts/jenkins/sdk/php_unit_test.sh
@@ -8,5 +8,10 @@ PHP_DIR="${BBSDK}/php-files/build/${phpver}-${phpts}-${arch}"
 LCB_PATH="${BBSDK}/lcb-files/dist/${LCBVER}-${arch}/lib"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LCB_PATH
 
+# allow to generate coredumps
+ulimit -c
+# display coredump pattern
+sysctl kernel.core_pattern
+
 # Test
 CPDSN=${NODE_IP} ${PHP_DIR}/bin/php -d extension=phar.so -d extension=./modules/couchbase.so ${PHP_DIR}/phpunit.phar tests/
