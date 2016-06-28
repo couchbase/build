0046581e73e7f2b8bec179f275e5db0558efcea7
Michael Kwok <michael.kwok@couchbase.com>
2016-06-28 16:00:05 -0700

    Add JCE Unlimited Policy extensions from Oracle
    Change-Id: I9f2cda9ecc23435179ae7bd876a4a520429e2320
    Reviewed-on: http://review.couchbase.org/65316
    Reviewed-by: Michael Kwok <michael.kwok@couchbase.com>
    Tested-by: Michael Kwok <michael.kwok@couchbase.com>

--- docker/buildslaves/ubuntu-14.04/android/restart.sh
+++ docker/buildslaves/ubuntu-14.04/android/restart.sh
@@ -4,13 +4,13 @@ docker stop mobile-light
 docker rm -f mobile-light
 docker run --name="mobile-light" -v /home/couchbase/jenkinsdocker-ssh:/ssh \
         --volume=/home/couchbase/latestbuilds:/latestbuilds \
-        -p 2300:22 -d ceejatec/ubuntu1404-mobile-android-docker:201500616
+        -p 2300:22 -d ceejatec/ubuntu1404-mobile-android-docker:201500628
 
 docker stop mobile-android
 docker rm -f mobile-android
-docker run --name="mobile-android" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2422:22 -d ceejatec/ubuntu1404-mobile-android-docker:20150616
+docker run --name="mobile-android" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2422:22 -d ceejatec/ubuntu1404-mobile-android-docker:20150628
 
 docker stop mobile-java
 docker rm -f mobile-java
-docker run --name="mobile-java" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2424:22 -d ceejatec/ubuntu1404-mobile-android-docker:20150616
+docker run --name="mobile-java" -v /home/couchbase/jenkinsdocker-ssh:/ssh -p 2424:22 -d ceejatec/ubuntu1404-mobile-android-docker:20150628
 
