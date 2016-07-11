c7685d1901be4d6da86c87e6c50a05516509f24d
hkodungallur <hkodungallur@gmail.com>
2016-07-11 14:53:47 -0700

    Dockerfiles for creating SDK build slaves (linux)
    Change-Id: Ief084d133862019fbf0f453294a77586a31faadb
    Reviewed-on: http://review.couchbase.org/65656
    Reviewed-by: Hari Kodungallur <hari.kodungallur@couchbase.com>
    Tested-by: Hari Kodungallur <hari.kodungallur@couchbase.com>

---
+++ docker/scripts/restart_all_sdk.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+start_dock() {
+    docker run \
+        --name=$2 \
+        --detach=true \
+        --publish=$3:22 \
+        --volume=/etc/resolv.conf:/etc/resolv.conf \
+        --volume=/etc/localtime:/etc/localtime \
+        --volume=/etc/timezone:/etc/timezone \
+        --volume=/home/couchbase/s3_cache:/home/couchbase/s3_cache \
+        --volume=/home/couchbase/.s3cfg:/home/couchbase/.s3cfg \
+        --volume=/home/couchbase/.aws:/home/couchbase/.aws \
+        --volume=/home/couchbase/jenkinsdocker-ssh:/ssh \
+        $1
+
+    sleep 2
+}
+
+mkdir -p /home/couchbase/s3_cache
+docker rm -f centos65-docker-01
+start_dock couchbase/centos-65-sdk-build:20160208 centos65-docker-01 5555
+
+docker rm -f ubuntu1404-docker-01
+start_dock couchbase/ubuntu-1404-sdk-build:20160208 ubuntu1404-docker-01 5556
+
+docker rm -f centos70-docker-01
+start_dock couchbase/centos-70-sdk-build:20160208 centos70-docker-01 5557
+
+docker rm -f zz-lightweight 
+start_dock couchbase/ubuntu-1404-sdk-build:20160208 zz-lightweight 5566
+
+docker rm -f centos65-docker-devkit1.1-02
+start_dock couchbase/centos-65-sdk-build:20160419 centos65-docker-devkit1.1-02 5558
