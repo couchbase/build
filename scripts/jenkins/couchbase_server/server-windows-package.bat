dc0ed4ce74b1bc9db69b0139202907ff80c62a0d
Chris Hillery <ceej@couchbase.com>
2016-08-31 17:20:43 -0700

    Improve windows package name handling
    Includes handling new PLAT_TYPE parameter for MSVC 2015 builds.
    
    Change-Id: Ib48b1b20ae183f88ea3d1b1f9378d67df7b2bc0f
    Reviewed-on: http://review.couchbase.org/67236
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- scripts/jenkins/couchbase_server/server-windows-package.bat
+++ scripts/jenkins/couchbase_server/server-windows-package.bat
@@ -2,11 +2,11 @@ rem Parameters
 
 set VERSION=%1
 set BLD_NUM=%2
-set BUILD_NUMBER=%VERSION%-%BLD_NUM%
+set PRODUCT_VER=%VERSION%-%BLD_NUM%
 
 set MANIFEST=%3
 set LICENSE=%4
-set ARCHITECTURE=%5
+set PLAT_TYPE=%5
 set SRC_DIR_PREFIX=%6
 
 # This path is just ever so slightly too long for Windows to deal with.
@@ -16,15 +16,14 @@ cd v
 
 :package_win
 echo ======== package =============================
-ruby server-win.rb %WORKSPACE%\couchbase\install 5.10.4.0.0.1 couchbase_server %BUILD_NUMBER% %LICENSE% %ARCHITECTURE% %SRC_DIR_PREFIX%  || goto error
+ruby server-win.rb %WORKSPACE%\couchbase\install 5.10.4.0.0.1 couchbase-server %PRODUCT_VER% %LICENSE% %PLAT_TYPE% %SRC_DIR_PREFIX%  || goto error
 
-set PKG_SRC_DIR=%WORKSPACE%\v\couchbase_server\%VERSION%\%BLD_NUM%
-set PKG_SRC_NAME=couchbase_server-%LICENSE%-windows-%ARCHITECTURE%-%BUILD_NUMBER%.exe
-set PKG_DEST_NAME=couchbase-server-%LICENSE%_%BUILD_NUMBER%-windows_%ARCHITECTURE%.exe
+set PKG_SRC_DIR=%WORKSPACE%\v
+set PKG_NAME=couchbase-server-%LICENSE%_%PRODUCT_VER%-%PLAT_TYPE%_amd64.exe
 
-copy %PKG_SRC_DIR%\%PKG_SRC_NAME% %WORKSPACE%\%PKG_DEST_NAME%
+copy %PKG_SRC_DIR%\%PKG_NAME% %WORKSPACE%
 
 echo ========== creating trigger.properties ==============
 cd %WORKSPACE%
 echo PLATFORM=windows> trigger.properties
-echo INSTALLER_FILENAME=%PKG_DEST_NAME%>> trigger.properties
+echo INSTALLER_FILENAME=%PKG_NAME%>> trigger.properties
