2174f534ef91f82caca5904a96892c30e11028b5
Chris Hillery <ceej@couchbase.com>
2016-09-01 21:20:16 -0700

    CBD-1895: Add golang to Analytics build container
    Change-Id: I9ac88408144f6ac4a5343510f09dbd947ddd8eb7
    Reviewed-on: http://review.couchbase.org/67291
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_analytics.sh
+++ docker/scripts/restart_analytics.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 
 # Analytics build container (currently hosted on mega3)
-./restart_jenkinsdocker.py ceejatec/ubuntu-1404-analytics-build:20160718 analytics-01 2412 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1404-analytics-build:20160901 analytics-01 2412 server.jenkins.couchbase.com &
 
 wait
 echo "All done!"
