bdb6e88dc59f1e0ebcd548ff7069a2edc5e20e03
Chris Hillery <ceej@couchbase.com>
2016-09-29 22:22:46 -0700

    Move watson slaves to non-competing ports
    Change-Id: I64abd6fbe183cf1cda601a38c8563484dce0e096
    Reviewed-on: http://review.couchbase.org/68201
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_all_watson.sh
+++ docker/scripts/restart_all_watson.sh
@@ -3,25 +3,25 @@
 cd `dirname $0`
 
 # New hostness Watson docker containers (currently hosted on mega3)
-./restart_jenkinsdocker.py ceejatec/centos-65-couchbase-build:20151223 watson-centos6-01 2222 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/centos-65-couchbase-build:20151223 watson-centos6-01 5222 server.jenkins.couchbase.com &
 sleep 5
 ./restart_jenkinsdocker.py ceejatec/centos-65-couchbase-build:20151223 watson-centos6-02 2232 server.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:latest zz-server-lightweight 3322 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:latest zz-server-lightweight 5322 server.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:20151223 watson-ubuntu12.04 2223 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:20151223 watson-ubuntu12.04 5223 server.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/debian-7-couchbase-build:20160229 watson-debian7 2224 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/debian-7-couchbase-build:20160229 watson-debian7 5224 server.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1404-couchbase-build:20151223 watson-ubuntu14.04 2226 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1404-couchbase-build:20151223 watson-ubuntu14.04 5226 server.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/suse-11-couchbase-build:20151223 watson-suse11 2228 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/suse-11-couchbase-build:20151223 watson-suse11 5228 server.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20151223 watson-centos7-01 2227 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20151223 watson-centos7-01 5227 server.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20151223 watson-centos7-02 2237 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20151223 watson-centos7-02 5237 server.jenkins.couchbase.com &
 sleep 5
-./restart_jenkinsdocker.py ceejatec/debian-8-couchbase-build:20160112 watson-debian8 2229 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/debian-8-couchbase-build:20160112 watson-debian8 5229 server.jenkins.couchbase.com &
 
 wait
 echo "All done!"
