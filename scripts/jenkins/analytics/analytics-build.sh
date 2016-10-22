f04145ad6e1ffce53a11a9b57dba7ddbedf7c31f
Michael Blow <michael.blow@couchbase.com>
2016-10-21 22:40:53 -0400

    Hardwire CBAS version to 1.0.0-DP1
    Change-Id: I3e67f9091e20aa6430b090628315cc66494b40e7
    Reviewed-on: http://review.couchbase.org/69102
    Reviewed-by: Michael Blow <michael.blow@couchbase.com>
    Tested-by: Michael Blow <michael.blow@couchbase.com>

--- scripts/jenkins/analytics/analytics-build.sh
+++ scripts/jenkins/analytics/analytics-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
 cd "${WORKSPACE}/asterixdb"
+# hardwire the version for DP
+mvn -f asterixdb/asterix-opt/pom.xml versions:set -DnewVersion=1.0.0-DP1
 mvn clean install -DskipTests -Pcb-build
 mv asterixdb/asterix-opt/installer/target/*.zip ${WORKSPACE}
