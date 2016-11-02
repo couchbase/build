dfa31ace85374c78ce16d225e018319970524584
Michael Blow <michael.blow@couchbase.com>
2016-11-02 11:24:19 -0400

    Prep for CBD-1932 - [CX] Encapsulate CBAS build under 'cbas' not 'asterixdb'
    Change-Id: I774ea9fe43146f4bde821a80daca6624c60a3fdb
    Reviewed-on: http://review.couchbase.org/69421
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Michael Blow <michael.blow@couchbase.com>

--- scripts/jenkins/analytics/analytics-build.sh
+++ scripts/jenkins/analytics/analytics-build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -ex
 
-cd "${WORKSPACE}/asterixdb"
+# prepare for CBD-1932 - [CX] Encapsulate CBAS build under 'cbas' not 'asterixdb'
+if [ -e "${WORKSPACE}/cbas" ]; then
+  cd "${WORKSPACE}/cbas"
+else
+  cd "${WORKSPACE}/asterixdb"
+fi
 # hardwire the version for DP
 mvn -B -f asterixdb/asterix-opt/pom.xml versions:set -DnewVersion=1.0.0-DP1
 mvn -B clean install -DskipTests -Pcb-build
