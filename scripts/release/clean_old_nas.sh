eebdb7e18ca3900eb91a79e9c3fab5b2393d6a68
Chris Hillery <ceej@couchbase.com>
2016-11-11 02:55:52 -0800

    More NAS cleaning
    Change-Id: I4bf5ce472fc774d048d581aa977dc7175ddf0ecc
    Reviewed-on: http://review.couchbase.org/69792
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- scripts/release/clean_old_nas.sh
+++ scripts/release/clean_old_nas.sh
@@ -26,11 +26,13 @@ remove_glob "*debug*" 30
 remove_glob "*dbg*" 30
 remove_glob "*-PDB.zip" 30
 
-# All Ubuntu 14, Debian 8, Centos 7, Suse, Mac, "oel6" builds older than 30 days
+# All Ubuntu 12/14, Debian 8, Centos 6, Suse, Mac, "oel6" builds older than 30/60 days
 remove_glob "*macos*.zip" 30
+remove_glob "*ubuntu12.04*.deb" 60
 remove_glob "*ubuntu14.04*.deb" 30
+remove_glob "*debian7*.deb" 60
 remove_glob "*debian8*.deb" 30
-remove_glob "*centos7*.rpm" 30
+remove_glob "*centos6*.rpm" 30
 remove_glob "*suse11*.rpm" 30
 remove_glob "*oel6*.rpm" 30
 
