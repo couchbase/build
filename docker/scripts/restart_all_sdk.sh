c7b67e9211b46656ce10007cb1019626da475310
Chris Hillery <ceej@couchbase.com>
2016-11-11 00:20:58 -0800

    Updating testrunner python deps for SDK build
    Change-Id: Idde9db733ba03fb6dc5dda906d98c5138824ca82
    Reviewed-on: http://review.couchbase.org/69789
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_all_sdk.sh
+++ docker/scripts/restart_all_sdk.sh
@@ -28,7 +28,7 @@ docker rm -f centos70-docker-01
 start_dock couchbase/centos-70-sdk-build:20160208 centos70-docker-01 5557
 
 docker rm -f zz-lightweight 
-start_dock couchbase/ubuntu-1404-sdk-build:20160208 zz-lightweight 5566
+start_dock couchbase/ubuntu-1404-sdk-build:20161026 zz-lightweight 5566
 
 docker rm -f centos65-docker-devkit1.1-02
 start_dock couchbase/centos-65-sdk-build:20160419 centos65-docker-devkit1.1-02 5558
