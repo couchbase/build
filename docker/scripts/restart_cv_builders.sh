76e6da44f50f4a6b402f603ee8b7613de8fff275
Chris Hillery <ceej@couchbase.com>
2017-01-08 23:18:54 -0800

    Comment out 'old' ubuntu-cv slaves
    Change-Id: I495b48968335c0d5a3d0e17d31939e23e44c7207
    Reviewed-on: http://review.couchbase.org/71743
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_cv_builders.sh
+++ docker/scripts/restart_cv_builders.sh
@@ -16,18 +16,18 @@ sleep 5
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20151009 cv-sherlocker-ubuntu12.04-02 2230 &
 sleep 5
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20151009 cv-sherlocker-ubuntu12.04-03 2231 &
-
-# OLD cv.jenkins slaves - these commands are left here only for reference
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-01 2322 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
-sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-02 2323 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
-sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-03 2324 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
-sleep 5
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-04 2325 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
 sleep 5
 ./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20151223 centos70-cv-build-01 2422 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
-sleep 5
+
+# OLD cv.jenkins slaves - these commands are left here only for reference
+#./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-01 2322 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
+#sleep 5
+#./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-02 2323 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
+#sleep 5
+#./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-03 2324 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
+#sleep 5
+#./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 ubuntu12-cv-04 2325 cv.jenkins.couchbase.com --ccache-dir /home/couchbase/slaves/shared_ccache &
+#sleep 5
 
 wait
 echo "All done!"
