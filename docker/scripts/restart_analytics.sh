6dfec1483bfa9195e25d941ef5239c0354ff9c72
Chris Hillery <ceej@couchbase.com>
2017-02-20 04:23:17 -0800

    MB-22619: Update Analytics slave for server+analytics builds
    Change-Id: Ia2240ede6b9fbfc6fc57de691f7370f560a8de69
    Reviewed-on: http://review.couchbase.org/73959
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_analytics.sh
+++ docker/scripts/restart_analytics.sh
@@ -2,8 +2,8 @@
 
 cd `dirname $0`
 
-# Analytics build container (currently hosted on mega3)
-./restart_jenkinsdocker.py ceejatec/ubuntu-1404-analytics-build:20160901 analytics-01 2412 server.jenkins.couchbase.com &
+# Analytics build container (currently hosted on as-cluster)
+./restart_jenkinsdocker.py ceejatec/ubuntu-1404-couchbase-build:20170213 analytics-01 2412 server.jenkins.couchbase.com &
 
 wait
 echo "All done!"
