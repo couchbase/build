db4febcdc7cc5daaf4ad8b1d153e16ac181e3777
Michael Blow <michael.blow@couchbase.com>
2017-02-28 12:24:01 -0500

    MB-23064: [CBAS] Need a way to determine the current build installed
    Stop hardwiring the version to DP1
    
    Change-Id: Ic5ccce835e23bc35e0921e274c545d405fe75d20
    Reviewed-on: http://review.couchbase.org/74401
    Reviewed-by: Till Westmann <till@couchbase.com>
    Tested-by: Michael Blow <michael.blow@couchbase.com>

--- scripts/jenkins/analytics/analytics-build.sh
+++ scripts/jenkins/analytics/analytics-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -ex
 
-# prepare for CBD-1932 - [CX] Encapsulate CBAS build under 'cbas' not 'asterixdb'
-if [ -e "${WORKSPACE}/cbas" ]; then
-  cd "${WORKSPACE}/cbas"
-else
-  cd "${WORKSPACE}/asterixdb"
-fi
-# hardwire the version for DP
-mvn -B -f asterixdb/asterix-opt/pom.xml versions:set -DnewVersion=1.0.0-DP1
+cd "${WORKSPACE}/cbas"
+
+# uncomment the following to hardwire the version for a release...
+#mvn -B -f asterixdb/asterix-opt/pom.xml versions:set -DnewVersion=1.0.0-DP1
+
+# build
 mvn -B clean install -DskipTests -Pcb-build
+
+# move the installer where the jenkins job expects it
 mv asterixdb/asterix-opt/installer/target/*.zip ${WORKSPACE}
