54a433ccd168adfd0e7b78523a56a2a331793f50
Michael Blow <michael.blow@couchbase.com>
2017-03-06 13:37:59 -0500

    Set release version to DP2 for Analytics
    Change-Id: I3a943b2caa2562625116c12fa619bf2b451ba2cb
    Reviewed-on: http://review.couchbase.org/74708
    Reviewed-by: Till Westmann <till@couchbase.com>
    Tested-by: Michael Blow <michael.blow@couchbase.com>

--- scripts/jenkins/analytics/analytics-build.sh
+++ scripts/jenkins/analytics/analytics-build.sh
@@ -2,8 +2,7 @@
 
 cd "${WORKSPACE}/cbas"
 
-# uncomment the following to hardwire the version for a release...
-#mvn -B -f asterixdb/asterix-opt/pom.xml versions:set -DnewVersion=1.0.0-DP1
+mvn -B -f asterixdb/asterix-opt/pom.xml versions:set -DnewVersion=1.0.0-DP2
 
 # build
 mvn -B clean install -DskipTests -Pcb-build
