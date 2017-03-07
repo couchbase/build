25197210196813f4c7f6f4723522bc6bb3f39562
Traun Leyden <tleyden@couchbase.com>
2017-03-07 14:22:40 -0800

    Add couchbase_server_admin_user_param template params
    Security group overhaul in progress

--- scripts/jenkins/mobile/ami/cloudformation_template.py
+++ scripts/jenkins/mobile/ami/cloudformation_template.py
@@ -38,92 +38,138 @@ def gen_template(config):
         'SgAccelInstanceType', Type='String',
         Description='The InstanceType to use for Sync Gateway Accel instance'
     ))
+    couchbase_server_admin_user_param = t.add_parameter(Parameter(
+        'CouchbaseServerAdminUserParam', Type='String',
+        Description='The Couchbase Server Admin username'
+    ))
+    couchbase_server_admin_pass_param = t.add_parameter(Parameter(
+        'CouchbaseServerAdminPassParam', Type='String',
+        Description='The Couchbase Server Admin password'
+    ))
     
     # Security Group + Launch Keypair
     # ------------------------------------------------------------------------------------------------------------------
-    def createCouchbaseSecurityGroups(t):
 
-        # Couchbase security group
-        secGrpCouchbase = ec2.SecurityGroup('CouchbaseSecurityGroup')
-        secGrpCouchbase.GroupDescription = "Allow access to Couchbase Server"
-        secGrpCouchbase.SecurityGroupIngress = [
-            ec2.SecurityGroupRule(
-                IpProtocol="tcp",
-                FromPort="22",
-                ToPort="22",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(
-                IpProtocol="tcp",
-                FromPort="8091",
-                ToPort="8091",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # sync gw user port
-                IpProtocol="tcp",
-                FromPort="4984",
-                ToPort="4984",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # expvars
-                IpProtocol="tcp",
-                FromPort="9876",
-                ToPort="9876",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # couchbase server
-                IpProtocol="tcp",
-                FromPort="4369",
-                ToPort="4369",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # couchbase server
-                IpProtocol="tcp",
-                FromPort="5984",
-                ToPort="5984",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # couchbase server
-                IpProtocol="tcp",
-                FromPort="8092",
-                ToPort="8092",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # couchbase server
-                IpProtocol="tcp",
-                FromPort="11209",
-                ToPort="11209",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # couchbase server
-                IpProtocol="tcp",
-                FromPort="11210",
-                ToPort="11210",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # couchbase server
-                IpProtocol="tcp",
-                FromPort="11211",
-                ToPort="11211",
-                CidrIp="0.0.0.0/0",
-            ),
-            ec2.SecurityGroupRule(   # couchbase server
-                IpProtocol="tcp",
-                FromPort="21100",
-                ToPort="21299",
-                CidrIp="0.0.0.0/0",
-            )
 
-        ]
+    # Couchbase security group
+    secGrpCouchbase = ec2.SecurityGroup('CouchbaseSecurityGroup')
+    secGrpCouchbase.GroupDescription = "Allow access to Couchbase Server"
 
-        # Add security group to template
-        t.add_resource(secGrpCouchbase)
+    # Add security group to template
+    t.add_resource(secGrpCouchbase)
 
-        return secGrpCouchbase
+    t.add_resource(ec2.SecurityGroupIngress(
+        'IngressSSH',
+        GroupName=Ref(secGrpCouchbase),
+        IpProtocol="tcp",
+        FromPort="22",
+        ToPort="22",
+        CidrIp="0.0.0.0/0",
+    ))
 
-    
-    secGrpCouchbase = createCouchbaseSecurityGroups(t)
-    
+    t.add_resource(ec2.SecurityGroupIngress(
+        'IngressSyncGatewayUser',
+        GroupName=Ref(secGrpCouchbase),
+        IpProtocol="tcp",
+        FromPort="4984",
+        ToPort="4984",
+        CidrIp="0.0.0.0/0",
+    ))
+
+    t.add_resource(ec2.SecurityGroupIngress(
+        'IngressSyncGatewayAdmin',
+        GroupName=Ref(secGrpCouchbase),
+        IpProtocol="tcp",
+        FromPort="4985",
+        ToPort="4985",
+        SourceSecurityGroupId=GetAtt("CouchbaseSecurityGroup", "GroupId"),
+    ))
+
+
+
+
+    # secGrpCouchbase.SecurityGroupIngress = [
+    #     ec2.SecurityGroupRule(
+    #         IpProtocol="tcp",
+    #         FromPort="22",
+    #         ToPort="22",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(
+    #         IpProtocol="tcp",
+    #         FromPort="8091",
+    #         ToPort="8091",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(   # sync gw user port
+    #         IpProtocol="tcp",
+    #         FromPort="4984",
+    #         ToPort="4984",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #
+    #
+    #     ec2.SecurityGroupRule(  # sync gw user port
+    #         IpProtocol="tcp",
+    #         FromPort="4985",
+    #         ToPort="4985",
+    #         # SourceSecurityGroupId=GetAtt(secGrpCouchbase, "GroupId"),
+    #         SourceSecurityGroupId=GetAtt("CouchbaseSecurityGroup", "GroupId"),
+    #     ),
+    #     ec2.SecurityGroupRule(   # expvars
+    #         IpProtocol="tcp",
+    #         FromPort="9876",
+    #         ToPort="9876",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(   # couchbase server
+    #         IpProtocol="tcp",
+    #         FromPort="4369",
+    #         ToPort="4369",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(   # couchbase server
+    #         IpProtocol="tcp",
+    #         FromPort="5984",
+    #         ToPort="5984",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(   # couchbase server
+    #         IpProtocol="tcp",
+    #         FromPort="8092",
+    #         ToPort="8092",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(   # couchbase server
+    #         IpProtocol="tcp",
+    #         FromPort="11209",
+    #         ToPort="11209",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(   # couchbase server
+    #         IpProtocol="tcp",
+    #         FromPort="11210",
+    #         ToPort="11210",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(   # couchbase server
+    #         IpProtocol="tcp",
+    #         FromPort="11211",
+    #         ToPort="11211",
+    #         CidrIp="0.0.0.0/0",
+    #     ),
+    #     ec2.SecurityGroupRule(   # couchbase server
+    #         IpProtocol="tcp",
+    #         FromPort="21100",
+    #         ToPort="21299",
+    #         CidrIp="0.0.0.0/0",
+    #     )
+    #
+    # ]
+
+
+
+
 
 
     # Couchbase Server Instance
@@ -135,8 +181,13 @@ def gen_template(config):
     instance.SecurityGroups = [Ref(secGrpCouchbase)]
     instance.KeyName = Ref(keyname_param)
     instance.Tags = Tags(Name=name, Type="couchbaseserver")
-    instance.UserData = sgautoscale.userDataCouchbaseServer(config.build_repo_commit, config.sgautoscale_repo_commit)
-    BlockDeviceMappings=[sgautoscale.blockDeviceMapping(config, "couchbaseserver")]
+    instance.UserData = sgautoscale.userDataCouchbaseServer(
+        config.build_repo_commit,
+        config.sgautoscale_repo_commit,
+        Ref(couchbase_server_admin_user_param),
+        Ref(couchbase_server_admin_pass_param),
+    )
+    instance.BlockDeviceMappings=[sgautoscale.blockDeviceMapping(config, "couchbaseserver")]
     t.add_resource(instance)
 
     # Sync Gateway Instance
@@ -148,8 +199,11 @@ def gen_template(config):
     instance.SecurityGroups = [Ref(secGrpCouchbase)]
     instance.KeyName = Ref(keyname_param)
     instance.Tags = Tags(Name=name, Type="syncgateway")
-    instance.UserData = sgautoscale.userDataSyncGatewayOrAccel(config.build_repo_commit, config.sgautoscale_repo_commit) 
-    BlockDeviceMappings=[sgautoscale.blockDeviceMapping(config, "syncgateway")]   
+    instance.UserData = sgautoscale.userDataSyncGatewayOrAccel(
+        config.build_repo_commit,
+        config.sgautoscale_repo_commit,
+    )
+    instance.BlockDeviceMappings=[sgautoscale.blockDeviceMapping(config, "syncgateway")]
     t.add_resource(instance)
     
     # SG Accel Instance
@@ -161,8 +215,11 @@ def gen_template(config):
     instance.SecurityGroups = [Ref(secGrpCouchbase)]
     instance.KeyName = Ref(keyname_param)
     instance.Tags = Tags(Name=name, Type="sgaccel")
-    instance.UserData = sgautoscale.userDataSyncGatewayOrAccel(config.build_repo_commit, config.sgautoscale_repo_commit)
-    BlockDeviceMappings=[sgautoscale.blockDeviceMapping(config, "sgaccel")]       
+    instance.UserData = sgautoscale.userDataSyncGatewayOrAccel(
+        config.build_repo_commit,
+        config.sgautoscale_repo_commit,
+    )
+    instance.BlockDeviceMappings=[sgautoscale.blockDeviceMapping(config, "sgaccel")]
     t.add_resource(instance)
 
 
@@ -214,8 +271,8 @@ def main():
         block_device_name="/dev/xvda",  # "/dev/sda1" for centos, /dev/xvda for amazon linux ami
         block_device_volume_size_by_server_type={"couchbaseserver": 200, "syncgateway": 25, "sgaccel": 25},
         block_device_volume_type="gp2",
-        build_repo_commit="0b5217c53d25d5974859014b4f241c51de9a79b1",
-        sgautoscale_repo_commit="abba14fe90e281b5a801e6d2397cb5f152a2097f",
+        build_repo_commit="master",
+        sgautoscale_repo_commit="master",
     )
 
     templ_json = gen_template(config)
