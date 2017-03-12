8d44a9de2f101c0df32c5fe37022f754421b2e20
Sergey Avseyev <sergey.avseyev@gmail.com>
2017-03-12 21:15:31 +0300

    PHP: do not hide exit code with coredump collector
    Change-Id: I1f025b9b50ed3da5142cada17ef0dc77c5712d74
    Reviewed-on: http://review.couchbase.org/75046
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- scripts/jenkins/sdk/php_unit_test.sh
+++ scripts/jenkins/sdk/php_unit_test.sh
@@ -19,8 +19,7 @@ ulimit -c unlimited
 # Test
 PHP_CMD="${PHP_DIR}/bin/php -d extension=phar.so -d extension=igbinary.so -d extension=$(pwd)/modules/couchbase.so -d couchbase.log_level=TRACE"
 ${PHP_CMD} -m
-CPUSER=${ADMIN_NAME} CPPASS=${ADMIN_PASSWORD} CPDSN=${NODE_IP} ${PHP_CMD} ${PHP_DIR}/phpunit.phar --verbose tests/
-
+CPUSER=${ADMIN_NAME} CPPASS=${ADMIN_PASSWORD} CPDSN=${NODE_IP} ${PHP_CMD} ${PHP_DIR}/phpunit.phar --verbose tests/ || \
 if [ -x /usr/bin/gdb ]; then
   shopt -s nullglob
   for c in core* /tmp/core* /var/crash/*; do
@@ -28,4 +27,5 @@ if [ -x /usr/bin/gdb ]; then
     file $c
     gdb --batch --quiet -ex "thread apply all bt full" -ex "quit" ${PHP_DIR}/bin/php $c
   done
+  exit 1
 fi
