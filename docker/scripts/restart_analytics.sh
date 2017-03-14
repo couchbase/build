beb84a190840ed69724ebafec491ab842c800785
Chris Hillery <ceej@couchbase.com>
2017-03-13 23:06:28 -0700

    Moved analytics slaves and jenkins
    Change-Id: Ic17f6fa06bc88ede1a0c4c894dea894b8976a8b2
    Reviewed-on: http://review.couchbase.org/75112
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_analytics.sh
+++ docker/scripts/restart_analytics.sh
@@ -2,9 +2,17 @@
 
 cd `dirname $0`
 
-# Analytics build container (currently hosted on as-cluster)
-./restart_jenkinsdocker.py ceejatec/ubuntu-1404-couchbase-build:20170213 analytics-01 2412 server.jenkins.couchbase.com &
+# jenkins-slave (currently hosted on mega2)
+./restart_jenkinsdocker.py ceejatec/asterix-centos7:latest jenkins-slave 2200 analytics.jenkins.couchbase.com 
 
-wait
+# For reference only: Starting the Jenkins master
+#docker run --detach=true --publish=8081:8080 --publish=50000:50000 \
+#  --name analytics-jenkins \
+#  --restart=unless-stopped \
+#  --volume=/home/couchbase/analytics_jenkins:/var/jenkins_home \
+#  --volume=/etc/timezone:/etc/timezone:ro \
+#  --volume=/etc/localtime:/etc/localtime:ro \
+#  jenkins:1.642.1
+
 echo "All done!"
 
