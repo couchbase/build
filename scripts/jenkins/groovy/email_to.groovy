81c0a8c93108094ed3af5bcd052dfd87b7902ad4
Chris Hillery <ceej@couchbase.com>
2017-03-18 16:48:20 -0700

    CBIT-7640: Add Eric to sanity email list
    Change-Id: I4e589fbde18c720253225d0b19e8957d7ee53ca7
    Reviewed-on: http://review.couchbase.org/75397
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- scripts/jenkins/groovy/email_to.groovy
+++ scripts/jenkins/groovy/email_to.groovy
@@ -3,7 +3,7 @@ def authors = ""
 
 def distro = build.environment["DISTRO"]
 if (distro == "centos7") {
-    authors = "build-team@couchbase.com"
+    authors = "build-team@couchbase.com,eric.cooper@couchbase.com"
 }
 %>
 
