db12bf41d271312734939ec86992b7ab7d3e0738
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-03-29 17:02:24 -0700

    Update import_rpm.sh to check S3 for RPM existance and acquire from there
    Change-Id: I832206b1502f2ec2fc37b8aaaac530bc0808351b
    Reviewed-on: http://review.couchbase.org/76001
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- scripts/release/linux_repo/import_rpm.sh
+++ scripts/release/linux_repo/import_rpm.sh
@@ -10,6 +10,7 @@
 #   6.  upload keys and yum.repos.d
 #
 if [[ ! ${LOCAL_REPO_ROOT} ]] ; then  LOCAL_REPO_ROOT=~/linux_repos/couchbase-server ; fi
+if [[ ! ${S3_PACKAGE_ROOT} ]] ; then  S3_PACKAGE_ROOT=s3://packages.couchbase.com/releases/couchbase-server ; fi
 
 function usage
 {
@@ -26,15 +27,28 @@ function fetch_rpm
     package=${1}
     version=${2}
     edition=${3}
+    s3dir="${S3_PACKAGE_ROOT}/${edition}/rpm/${RPM_DIR}"
     releases="http://172.23.120.24/builds/releases/${version}"
+
     if [[ "${edition}" = "community" ]]
     then
         releases="${releases}/ce"
     fi
+
+    if [[ -n $(s3cmd ls ${s3dir}/${package}) ]]
+    then
+        EXISTS_ON_S3=true
+    fi
+
     if [[ ! -e ${package} ]]
     then
         echo "fetching ${package}"
-        wget ${releases}/${package}
+        if ${EXISTS_ON_S3}
+        then
+            s3cmd get ${s3dir}/${package}
+        else
+            wget ${releases}/${package}
+        fi
     else
         echo "already have ${package}"
     fi
@@ -54,10 +68,20 @@ RPM_GPG_KEY_V4=CD406E62
 
 for CENTOS in 6 7
 do
+    # The variable EXISTS_ON_S3 is (potentially) modified in the fetch_rpm
+    # function to allow it to do the right thing when acquiring a given RPM,
+    # based on whether it's already in S3 or not.  This update is carried
+    # through the rest of the for loop to ensure an RPM already on S3 isn't
+    # signed again (which would change its checksum).
+    EXISTS_ON_S3=false
+    RPM_DIR=${CENTOS}/x86_64
     rpm_filename=couchbase-server-${EDITION}-${VERSION}-centos${CENTOS}.x86_64.rpm
     fetch_rpm ${rpm_filename} ${VERSION} ${EDITION}
-    cp ${rpm_filename} ${REPO}/${CENTOS}/x86_64/
-    expect ./autosign_rpm.exp -D "_signature gpg" -D "_gpg_name ${RPM_GPG_KEY_V4}" ${REPO}/${CENTOS}/x86_64/${rpm_filename}
+    cp ${rpm_filename} ${REPO}/${RPM_DIR}/
+
+    if ! ${EXISTS_ON_S3}; then
+        expect ./autosign_rpm.exp -D "_signature gpg" -D "_gpg_name ${RPM_GPG_KEY_V4}" ${REPO}/${RPM_DIR}/${rpm_filename}
+    fi
 done
 
 echo ""
