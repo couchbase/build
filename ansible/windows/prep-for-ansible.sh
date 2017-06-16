0a4eb75e14689a7ee27c5aafcdc006491716ab0e
Chris Hillery <ceej@couchbase.com>
2017-06-16 02:32:37 -0700

    Fix to prep-for-ansible.sh script
    Change-Id: I5953ea5b92e7ccebd45598723a94edd581c3a7de
    Reviewed-on: http://review.couchbase.org/79600
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- ansible/windows/prep-for-ansible.sh
+++ ansible/windows/prep-for-ansible.sh
@@ -3,5 +3,5 @@
 host=$1
 passwordfile=./password.txt
 
-sshpass -f $2 scp -o StrictHostKeyChecking=no ConfigureForAnsible.ps1 Administrator@$host:
-sshpass -f $2 ssh -o StrictHostKeyChecking=no Administrator@$host powershell.exe -File ConfigureForAnsible.ps1 -SkipNetworkProfileCheck -ForceNewSSLCert
+sshpass -f $passwordfile scp -o StrictHostKeyChecking=no ConfigureForAnsible.ps1 Administrator@$host:
+sshpass -f $passwordfile ssh -o StrictHostKeyChecking=no Administrator@$host powershell.exe -File ConfigureForAnsible.ps1 -SkipNetworkProfileCheck -ForceNewSSLCert
