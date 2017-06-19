c1a3c29cd2a727d11a666b432548eae8a1d4c6cb
Kim Trang <kim.trang@couchbase.com>
2017-06-19 14:21:08 -0700

    SDKQE-938 build java-skd at SHA, default to master
    Change-Id: Iacf80cc4ceb5ce24402f4874135629fa3889e249
    Reviewed-on: http://review.couchbase.org/79682
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- scripts/jenkins/sdk/build_java_sdk.sh
+++ scripts/jenkins/sdk/build_java_sdk.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+CORE_SHA=$1
+CLIENT_SHA=$2
+
 git clone git://github.com/couchbase/couchbase-jvm-core.git
+if [ -n "${CORE_SHA}" ]
+then
+    pushd ${WORKSPACE}/couchbase-jvm-core && git checkout ${CORE_SHA} && popd
+fi
+
 git clone git://github.com/couchbase/couchbase-java-client.git
+if [ -n "${CLIENT_SHA}" ]
+then
+    pushd  ${WORKSPACE}/couchbase-java-client && git checkout ${CLIENT_SHA} && popd
+fi
 
 cd ${WORKSPACE}/couchbase-jvm-core
 mvn clean install -DskipTests=true
