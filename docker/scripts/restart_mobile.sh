48f362d7d4b025400c5e7098a2a9225b2d8aa8ae
Chris Hillery <ceej@couchbase.com>
2017-06-20 23:59:38 -0700

    Add Ubuntu 14.04-based Lite Core Linux build image and slave
    Change-Id: Idf75df5f546c5e009b7036ae54f81404eacb967a
    Reviewed-on: http://review.couchbase.org/79770
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_mobile.sh
+++ docker/scripts/restart_mobile.sh
@@ -9,4 +9,5 @@ echo @@@@@@@@@@@@@@@@@@@@@@
 ../buildslaves/ubuntu-14.04/sgw_jenkins/restart.sh
 ../buildslaves/ubuntu-14.04/android/restart.sh
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:latest zz-mobile-lightweight 2423 mobile.jenkins.couchbase.com
+./restart_jenkinsdocker.py ceejatec/ubuntu-1404-litecore-build:20170616 mobile-litecore-linux 6501 mobile.jenkins.couchbase.com
 
