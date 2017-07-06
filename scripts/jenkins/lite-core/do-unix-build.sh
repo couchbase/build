e2fc0e77300841d25de99f31185ead1c2c8ef0df
Kim Trang <kim.trang@couchbase.com>
2017-07-06 12:46:45 -0700

    MB-25129 Added build steps for apple-devices, tvos, ios
    Change-Id: I8cb7e9b3acb198f2df2c6c19ac650698091107d3
    Reviewed-on: http://review.couchbase.org/80337
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- scripts/jenkins/lite-core/do-unix-build.sh
+++ scripts/jenkins/lite-core/do-unix-build.sh
@@ -6,41 +6,84 @@ VERSION=${2}
 BLD_NUM=${3}
 
 case "${OSTYPE}" in
-    darwin*)  OS="macosx"
+    darwin*)  OS="macosx" 
               PKG_CMD='zip -r'
-              PKG_TYPE='zip';;
+              PKG_TYPE='zip'
+              PROP_FILE=${WORKSPACE}/publish.prop
+              mkdir -p ${WORKSPACE}/build_release ${WORKSPACE}/build_debug
+              if [[ ${TVOS} == 'true' ]]; then
+                  OS="macosx-tvos"
+                  BUILD_TVOS_REL_TARGET='build_tvos_release'
+                  BUILD_TVOS_DEBUG_TARGET='build_tvos_debug'
+                  PROP_FILE=${WORKSPACE}/publish_tvos.prop
+                  mkdir -p ${WORKSPACE}/${BUILD_TVOS_REL_TARGET} ${WORKSPACE}/${BUILD_TVOS_DEBUG_TARGET}
+              fi
+              if [[ ${IOS} == 'true' ]]; then
+                  OS="macosx-ios"
+                  BUILD_IOS_REL_TARGET='build_ios_release'
+                  BUILD_IOS_DEBUG_TARGET='build_ios_debug'
+                  PROP_FILE=${WORKSPACE}/publish_ios.prop
+                  mkdir -p ${WORKSPACE}/${BUILD_IOS_REL_TARGET} ${WORKSPACE}/${BUILD_IOS_DEBUG_TARGET}
+              fi;;
     linux*)   OS="linux"
               PKG_CMD='tar czf'
-              PKG_TYPE='tar.gz';;
+              PKG_TYPE='tar.gz'
+              PROP_FILE=${WORKSPACE}/publish.prop;;
     *)        echo "unknown: $OSTYPE"
               exit 1;;
 esac
-
-PROP_FILE=${WORKSPACE}/publish.prop
 # Global define end
 
-mkdir -p ${WORKSPACE}/build_release ${WORKSPACE}/build_debug
-
-echo "====  Building Release binary  ==="
-
-cd ${WORKSPACE}/build_release/
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/install -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLITECORE_BUILD_SQLITE=1  ..
-make -j8
-make install
-chmod 777 ${WORKSPACE}/couchbase-lite-core/build_cmake/scripts/test_unix.sh
-cd ${WORKSPACE}/build_release/couchbase-lite-core && ../../couchbase-lite-core/build_cmake/scripts/test_unix.sh
-cd ${WORKSPACE}
-
-if [[ "${OS}" == 'macosx' ]]
-then
-    echo "====  Building Debug binary  ==="
-
-    cd ${WORKSPACE}/build_debug/
-    cmake -DCMAKE_INSTALL_PREFIX=`pwd`/install -DCMAKE_BUILD_TYPE=Debug -DLITECORE_BUILD_SQLITE=1 ..
+if [[ ${TVOS} == 'true' ]]; then
+    echo "====  Building tvos Release binary  ==="
+    cd ${WORKSPACE}/${BUILD_TVOS_REL_TARGET}
+    xcodebuild -project  ${WORKSPACE}/couchbase-lite-core/Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath tvos -scheme "LiteCore dylib" -sdk appletvos
+    xcodebuild -project ${WORKSPACE}/couchbase-lite-core/Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath tvos -scheme "LiteCore dylib" -sdk appletvsimulator
+    lipo -create tvos/Build/Products/Release-appletvos/libLiteCore.dylib tvos/Build/Products/Release-appletvsimulator/libLiteCore.dylib -output ${WORKSPACE}/${BUILD_TVOS_REL_TARGET}/libLiteCore.dylib
+    cd ${WORKSPACE}
+elif [[ ${IOS} == 'true' ]]; then
+    echo "====  Building ios Release binary  ==="
+    cd ${WORKSPACE}/${BUILD_IOS_REL_TARGET}
+    xcodebuild -project ${WORKSPACE}/couchbase-lite-core/Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphoneos BITCODE_GENERATION_MODE=bitcode CODE_SIGNING_ALLOWED=NO
+    xcodebuild -project ${WORKSPACE}/couchbase-lite-core/Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphonesimulator CODE_SIGNING_ALLOWED=NO
+    lipo -create ios/Build/Products/Release-iphoneos/libLiteCore.dylib ios/Build/Products/Release-iphonesimulator/libLiteCore.dylib -output ${WORKSPACE}/${BUILD_IOS_REL_TARGET}/libLiteCore.dylib
+    cd ${WORKSPACE}
+else
+    echo "====  Building macosx/linux Release binary  ==="
+    cd ${WORKSPACE}/build_release
+    cmake -DCMAKE_INSTALL_PREFIX=`pwd`/install -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLITECORE_BUILD_SQLITE=1  ..
     make -j8
     make install
-    cd ${WORKSPACE}/build_debug/couchbase-lite-core && ../../couchbase-lite-core/build_cmake/scripts/test_unix.sh
+    chmod 777 ${WORKSPACE}/couchbase-lite-core/build_cmake/scripts/test_unix.sh
+    cd ${WORKSPACE}/build_release/couchbase-lite-core && ../../couchbase-lite-core/build_cmake/scripts/test_unix.sh
     cd ${WORKSPACE}
+fi
+
+if [[ "${OS}" == "macosx" ]] || [[ "${OS}" ==  "macosx-tvos" ]] || [[ "${OS}" == "macosx-ios" ]]
+then
+    if [[ ${TVOS} == 'true' ]]; then
+        echo "====  Building tvos Debug binary  ==="
+        cd ${WORKSPACE}/${BUILD_TVOS_DEBUG_TARGET}
+        xcodebuild -project  ${WORKSPACE}/couchbase-lite-core/Xcode/LiteCore.xcodeproj -configuration Debug -derivedDataPath tvos -scheme "LiteCore dylib" -sdk appletvos
+        xcodebuild -project ${WORKSPACE}/couchbase-lite-core/Xcode/LiteCore.xcodeproj -configuration Debug -derivedDataPath tvos -scheme "LiteCore dylib" -sdk appletvsimulator
+        lipo -create tvos/Build/Products/Debug-appletvos/libLiteCore.dylib tvos/Build/Products/Debug-appletvsimulator/libLiteCore.dylib -output ${WORKSPACE}/${BUILD_TVOS_DEBUG_TARGET}/libLiteCore.dylib
+        cd ${WORKSPACE}
+    elif [[ ${IOS} == 'true' ]]; then
+        echo "====  Building ios Debug binary  ==="
+        cd ${WORKSPACE}/${BUILD_IOS_DEBUG_TARGET}
+        xcodebuild -project ${WORKSPACE}/couchbase-lite-core/Xcode/LiteCore.xcodeproj -configuration Debug -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphoneos BITCODE_GENERATION_MODE=bitcode CODE_SIGNING_ALLOWED=NO
+        xcodebuild -project ${WORKSPACE}/couchbase-lite-core/Xcode/LiteCore.xcodeproj -configuration Debug -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphonesimulator CODE_SIGNING_ALLOWED=NO
+        lipo -create ios/Build/Products/Debug-iphoneos/libLiteCore.dylib ios/Build/Products/Debug-iphonesimulator/libLiteCore.dylib -output ${WORKSPACE}/${BUILD_IOS_DEBUG_TARGET}/libLiteCore.dylib
+        cd ${WORKSPACE}
+    else
+        echo "====  Building macosx/linux Debug binary  ==="
+        cd ${WORKSPACE}/build_debug/
+        cmake -DCMAKE_INSTALL_PREFIX=`pwd`/install -DCMAKE_BUILD_TYPE=Debug -DLITECORE_BUILD_SQLITE=1 ..
+        jmake -j8
+        make install
+        cd ${WORKSPACE}/build_debug/couchbase-lite-core && ../../couchbase-lite-core/build_cmake/scripts/test_unix.sh
+        cd ${WORKSPACE}
+    fi
 fi
 
 VERSION=$(git -C "${WORKSPACE}/couchbase-lite-core" rev-parse HEAD)
@@ -52,17 +95,47 @@ do
     then
         continue
     fi
+
     PACKAGE_NAME=${PRODUCT}-${OS}-${VERSION}-${FLAVOR}.${PKG_TYPE}
     echo
     echo  "=== Creating ${WORKSPACE}/${PACKAGE_NAME} package ==="
     echo
-    cd ${WORKSPACE}/build_${FLAVOR}/install
-    ${PKG_CMD} ${WORKSPACE}/${PACKAGE_NAME} *
+
     if [[ "${FLAVOR}" == 'debug' ]]
     then
-        DEBUG_PKG_NAME=${PACKAGE_NAME}
+        if [[ ${TVOS} == 'true' ]]; then
+            cd ${WORKSPACE}/${BUILD_TVOS_DEBUG_TARGET}
+            ${PKG_CMD} ${WORKSPACE}/${PACKAGE_NAME} libLiteCore.dylib
+            cd ${WORKSPACE}
+            DEBUG_TVOS_PKG_NAME=${PACKAGE_NAME}
+        elif [[ ${IOS} == 'true' ]]; then
+            cd ${WORKSPACE}/${BUILD_IOS_DEBUG_TARGET}
+            ${PKG_CMD} ${WORKSPACE}/${PACKAGE_NAME} libLiteCore.dylib
+            cd ${WORKSPACE}
+            DEBUG_IOS_PKG_NAME=${PACKAGE_NAME}
+        else
+            DEBUG_PKG_NAME=${PACKAGE_NAME}
+            cd ${WORKSPACE}/build_${FLAVOR}/install
+            ${PKG_CMD} ${WORKSPACE}/${PACKAGE_NAME} *
+            cd ${WORKSPACE}
+        fi
     else
-        RELEASE_PKG_NAME=${PACKAGE_NAME}
+        if [[ ${TVOS} == 'true' ]]; then
+            cd ${WORKSPACE}/${BUILD_TVOS_REL_TARGET}
+            ${PKG_CMD} ${WORKSPACE}/${PACKAGE_NAME} libLiteCore.dylib
+            cd ${WORKSPACE}
+            RELEASE_TVOS_PKG_NAME=${PACKAGE_NAME}
+        elif [[ ${IOS} == 'true' ]]; then
+            cd ${WORKSPACE}/${BUILD_IOS_REL_TARGET}
+            ${PKG_CMD} ${WORKSPACE}/${PACKAGE_NAME} libLiteCore.dylib
+            cd ${WORKSPACE}
+            RELEASE_IOS_PKG_NAME=${PACKAGE_NAME}
+        else
+            RELEASE_PKG_NAME=${PACKAGE_NAME}
+            cd ${WORKSPACE}/build_${FLAVOR}/install
+            ${PKG_CMD} ${WORKSPACE}/${PACKAGE_NAME} *
+            cd ${WORKSPACE}
+        fi
     fi
 done
 
@@ -72,10 +145,18 @@ echo "PRODUCT=${PRODUCT}"  >> ${PROP_FILE}
 echo "BLD_NUM=${BLD_NUM}"  >> ${PROP_FILE}
 echo "VERSION=${VERSION}" >> ${PROP_FILE}
 echo "PKG_TYPE=${PKG_TYPE}" >> ${PROP_FILE}
-echo "DEBUG_PKG_NAME=${DEBUG_PKG_NAME}" >> ${PROP_FILE}
-echo "RELEASE_PKG_NAME=${RELEASE_PKG_NAME}" >> ${PROP_FILE}
+if [[ ${TVOS} == 'true' ]]; then
+    echo "DEBUG_TVOS_PKG_NAME=${DEBUG_TVOS_PKG_NAME}" >> ${PROP_FILE}
+    echo "RELEASE_TVOS_PKG_NAME=${RELEASE_TVOS_PKG_NAME}" >> ${PROP_FILE}
+elif [[ ${IOS} == 'true' ]]; then
+    echo "DEBUG_IOS_PKG_NAME=${DEBUG_IOS_PKG_NAME}" >> ${PROP_FILE}
+    echo "RELEASE_IOS_PKG_NAME=${RELEASE_IOS_PKG_NAME}" >> ${PROP_FILE}
+else
+    echo "DEBUG_PKG_NAME=${DEBUG_PKG_NAME}" >> ${PROP_FILE}
+    echo "RELEASE_PKG_NAME=${RELEASE_PKG_NAME}" >> ${PROP_FILE}
+fi
 
-echo
+echo  
 echo  "=== Created ${WORKSPACE}/${PROP_FILE} ==="
 echo
 
