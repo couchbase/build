b6a26aa0752c2ea8f12a43ab0b1f682d8d5a7a31
Chris Hillery <ceej@couchbase.com>
2017-07-05 23:54:16 -0700

    CBIT-8457: Restored/updated Centos 6 SGW build slave
    Change-Id: I8c9593d0c08a203a84db5e7cf15c176c260aadad
    Reviewed-on: http://review.couchbase.org/80307
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_mobile.sh
+++ docker/scripts/restart_mobile.sh
@@ -5,9 +5,10 @@
 echo @@@@@@@@@@@@@@@@@@@@@@
 echo @ Recreating slaves
 echo @@@@@@@@@@@@@@@@@@@@@@
+../buildslaves/centos-65/sgw_jenkins/restart.sh
 ../buildslaves/centos-70/sgw_jenkins/restart.sh
 ../buildslaves/ubuntu-14.04/sgw_jenkins/restart.sh
 ../buildslaves/ubuntu-14.04/android/restart.sh
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:latest zz-mobile-lightweight 2423 mobile.jenkins.couchbase.com
+./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:latest zz-mobile-lightweight 2423 mobile.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1404-litecore-build:20170616 mobile-litecore-linux 6501 mobile.jenkins.couchbase.com
 
