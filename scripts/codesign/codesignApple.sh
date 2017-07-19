4fdd1c3b56e0119fb1c290fce0d12070b4b194e9
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-07-19 13:45:20 -0700

    Add support to macOS codesign for Vulcan (5.1)
    Change-Id: I4f4f0152fab6c7c419d768a5005a72f7d743d34f
    Reviewed-on: http://review.couchbase.org/81127
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- scripts/codesign/codesignApple.sh
+++ scripts/codesign/codesignApple.sh
@@ -36,7 +36,10 @@ DOWNLOAD_NEW_PKG=${5}  # Get new build
 result="rejected"
 
 rel_code=""
-if [[ ${PKG_VERSION} == 4.7* ]] || [[ ${PKG_VERSION} == 5* ]]
+if [[ ${PKG_VERSION} == 5.1* ]]
+then
+    rel_code="vulcan"
+elif [[ ${PKG_VERSION} == 4.7* ]] || [[ ${PKG_VERSION} == 5.0* ]]
 then
     rel_code="spock"
 elif [[ ${PKG_VERSION} == 4.5* ]] || [[ ${PKG_VERSION} == 4.6* ]]
