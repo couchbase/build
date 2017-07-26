1b6d64e60a15e187215b53c87e8c28157c373fad
Chris Hillery <ceej@couchbase.com>
2017-07-26 03:47:05 -0700

    MB-24815: Create additional zz-server-lightweight slave
    Change-Id: I64ace41e8ba105015de36333682665607712184d
    Reviewed-on: http://review.couchbase.org/81409
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_all_watson.sh
+++ docker/scripts/restart_all_watson.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 # New hostness Watson docker containers (currently hosted on mega3)
 ./restart_jenkinsdocker.py ceejatec/centos-65-couchbase-build:20170522 watson-centos6-01 5222 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/centos-65-couchbase-build:20170522 watson-centos6-02 5232 server.jenkins.couchbase.com
-./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:latest zz-server-lightweight 5322 server.jenkins.couchbase.com
+./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:latest zz-server-lightweight-backup 5322 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:20151223 watson-ubuntu12.04 5223 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/debian-7-couchbase-build:20170522 watson-debian7 5224 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1404-couchbase-build:20170522 watson-ubuntu14.04 5226 server.jenkins.couchbase.com
