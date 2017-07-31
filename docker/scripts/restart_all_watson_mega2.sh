75d0a977917008ac836b35de51ebe1391a0b258f
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-07-31 15:46:43 -0700

    MB-25438: Adding Debian-9.1 buildslave
    Change-Id: Ia7592e33547b73f7d4bb90a01c2d2f1e94e02f5b
    Reviewed-on: http://review.couchbase.org/81665
    Reviewed-by: Kim Trang <kim.trang@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- docker/scripts/restart_all_watson_mega2.sh
+++ docker/scripts/restart_all_watson_mega2.sh
@@ -7,6 +7,8 @@ cd `dirname $0`
 # Spock Ubuntu 16.04 builder - using CV image because that helps some
 # cbdeps builds, notably jemalloc needing valgrind headers
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-cv:20170522 spock-ubuntu16.04 5238 server.jenkins.couchbase.com
+# Spock Debian 9.1 builder
+./restart_jenkinsdocker.py ceejatec/debian-9-couchbase-build:20170731 spock-debian9 5230 server.jenkins.couchbase.com
 
 wait
 echo "All done!"
