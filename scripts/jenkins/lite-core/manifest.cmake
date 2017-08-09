20c8899f7e4c063b8f043f2749ff4428dc4e297a
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-08-09 15:05:51 -0700

    CBD-2035: Adding icu4c for centos7 as a dep for lite-core
    Change-Id: I3a15577a69832cb875a694c4ae04af917d11c1ee
    Reviewed-on: http://review.couchbase.org/82131
    Reviewed-by: Jim Borden <jim.borden@couchbase.com>
    Tested-by: Jim Borden <jim.borden@couchbase.com>

--- scripts/jenkins/lite-core/manifest.cmake
+++ scripts/jenkins/lite-core/manifest.cmake
@@ -4,4 +4,5 @@
 #
 # DECLARE_DEP (name VERSION version-revision PLATFORMS platform1 platform2)
 #
+DECLARE_DEP (icu4c VERSION 54.1-cb10 PLATFORMS centos7)
 DECLARE_DEP (libcxx VERSION 3.9.1-cb2 PLATFORMS centos7)
