7572af94165e1facaeb0864a8a3c0a348933372b
Kim Trang <kim.trang@couchbase.com>
2017-08-22 12:03:14 -0700

    MB-25753 Added publish to S3 for mobile
    Change-Id: If6fd250b9b8eae3b90a91366eb079aca7c8792f3
    Reviewed-on: http://review.couchbase.org/82588
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

---
+++ scripts/jenkins/mobile/mobile-release-binaries-to-s3.sh
@@ -0,0 +1,130 @@
+#!/bin/bash -e
+
+function usage() {
+    echo
+    echo "$0 -r <product>  -r <release> -v <version> -b <build-number>"
+    echo "where:"
+    echo "  -p: product name: sync_gateway, couchbase-lite-ios"
+    echo "  -r: release branch: master, 1.5.0, etc."
+    echo "  -v: version number: 1.5.0, 2.0DB15, etc."
+    echo "  -b: build number: 128, etc."
+    echo
+}
+
+while getopts "p:r:v:b:h" opt; do
+    case $opt in
+        p) PRODUCT=$OPTARG;;
+        r) RELEASE=$OPTARG;;
+        v) VERSION=$OPTARG;;
+        b) BLD_NUM=$OPTARG;;
+        h|?) usage
+           exit 0;;
+        *) echo "Invalid argument $opt"
+           usage
+           exit 1;;
+    esac
+done
+
+if [ "x${PRODUCT}" = "x" ]; then
+    echo "Product name not set"
+    usage
+    exit 2
+fi
+
+if [ "x${RELEASE}" = "x" ]; then
+    echo "Release product name not set"
+    usage
+    exit 2
+fi
+
+if [ "x${VERSION}" = "x" ]; then
+    echo "Version number not set"
+    usage
+    exit 2
+fi
+
+if [ "x${BLD_NUM}" = "x" ]; then
+    echo "Build number not set"
+    usage
+    exit 2
+fi
+
+LB_MOUNT=/latestbuilds
+if [ ! -e ${LB_MOUNT} ]; then
+    echo "'latestbuilds' directory is not mounted"
+    exit 3
+fi
+
+REL_MOUNT=/releases
+if [ ! -e ${REL_MOUNT} ]; then
+    echo "'releases' directory is not mounted"
+    exit 3
+fi
+
+# Compute S3 component dirname
+case "$PRODUCT" in
+    sync_gateway)
+        REL_DIRNAME=sync_gateway
+        S3_REL_DIRNAME=couchbase-sync-gateway
+        ;;
+    *ios*)
+        REL_DIRNAME=couchbase-lite-ios
+        S3_REL_DIRNAME=couchbase-lite/ios
+        ;;
+    *)
+        echo "Unsupported Product!"
+        usage
+        ;;
+esac
+
+# Compute destination directories
+S3CONFIG=${HOME}/.ssh/live.s3cfg
+S3_DIR=s3://packages.couchbase.com/releases/${S3_REL_DIRNAME}/${VERSION}
+SRC_DIR=${LB_MOUNT}/${PRODUCT}/${RELEASE}/${BLD_NUM}
+RELEASE_DIR=${REL_MOUNT}/mobile/${S3_REL_DIRNAME}/${VERSION}
+
+if [ ! -e ${BUILD_DIR} ]; then
+    echo "Given build doesn't exist: ${BUILD_DIR}"
+    exit 4
+fi
+
+upload()
+{
+    src=./
+    target=${1}
+    echo src: $src
+    echo dest: $target
+
+    # Verify build exists
+    if [ ! -e $src ]; then
+        echo "Given build doesn't exist: ${src}"
+        exit 5
+    fi
+    echo "Uploading to $target/ ..."
+    echo
+    S3_OPTS=' --exclude=*-source.tar.gz  --exclude=*.json  --exclude=*.properties '
+    s3cmd -c $S3CONFIG sync -P  ${S3_OPTS}  $src $target/
+
+    # Archive internal releaess
+    echo "Archiving ${SRC_DIR} to ${RELEASE_DIR} ..."
+    echo
+    mkdir -p ${RELEASE_DIR}
+    rsync -au $src ${RELEASE_DIR}
+}
+
+OPWD=`pwd`
+finish() {
+    cd $OPWD
+    exit
+}
+trap finish EXIT
+
+get_s3_upload_link()
+{
+    s3cmd ls -c $S3CONFIG ${S3_DIR}/  | cut -c 30- | sed -e 's/s3:/https:/'
+}
+
+cd ${SRC_DIR}
+upload ${S3_DIR}
+get_s3_upload_link
+
