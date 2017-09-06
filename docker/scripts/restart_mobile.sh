4bfed8a12abb2e36c5b04d63c38c2dcf00d7a07c
Kim Trang <kim.trang@couchbase.com>
2017-09-06 14:01:50 -0700

    CBD-2073  Update restart docker script for mobile-lite-android
    Change-Id: I167546b8a1aff92421f9e3216b989e3f9f20a23d
    Reviewed-on: http://review.couchbase.org/83151
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- docker/scripts/restart_mobile.sh
+++ docker/scripts/restart_mobile.sh
@@ -9,6 +9,6 @@ echo @@@@@@@@@@@@@@@@@@@@@@
 ../buildslaves/centos-70/sgw_jenkins/restart.sh
 ../buildslaves/ubuntu-14.04/sgw_jenkins/restart.sh
 ../buildslaves/ubuntu-14.04/android/restart.sh
-./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:latest zz-mobile-lightweight 2423 mobile.jenkins.couchbase.com
-./restart_jenkinsdocker.py ceejatec/centos-72-litecore-build:20170710 mobile-litecore-linux 6501 mobile.jenkins.couchbase.com
-
+./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:latest      zz-mobile-lightweight 2423 mobile.jenkins.couchbase.com
+./restart_jenkinsdocker.py ceejatec/centos-72-litecore-build:20170710       mobile-litecore-linux 6501 mobile.jenkins.couchbase.com
+./restart_jenkinsdocker.py ceejatec/ubuntu1404-mobile-lite-android:20170906 mobile-lite-android 6502 mobile.jenkins.couchbase.com
