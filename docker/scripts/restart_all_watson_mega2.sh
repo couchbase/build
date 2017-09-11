eb82794bff23e6234696a381ae19022331b5302f
Chris Hillery <ceej@couchbase.com>
2017-09-11 03:28:10 -0700

    MB-25438: Debian 9.1 Docker update
    Change-Id: I4bd2e9f4b83c82e520dd916bc9cba325fd6f7fa7
    Reviewed-on: http://review.couchbase.org/83260
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_all_watson_mega2.sh
+++ docker/scripts/restart_all_watson_mega2.sh
@@ -8,7 +8,7 @@ cd `dirname $0`
 # cbdeps builds, notably jemalloc needing valgrind headers
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-cv:20170522 spock-ubuntu16.04 5238 server.jenkins.couchbase.com
 # Spock Debian 9.1 builder
-./restart_jenkinsdocker.py ceejatec/debian-9-couchbase-build:20170731 spock-debian9 5230 server.jenkins.couchbase.com
+./restart_jenkinsdocker.py ceejatec/debian-9-couchbase-build:20170911 spock-debian9 5230 server.jenkins.couchbase.com
 
 # Primary zz-server-lightweight running on mega2 (same port as backup on mega3)
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:20170725 zz-server-lightweight 5322 server.jenkins.couchbase.com
