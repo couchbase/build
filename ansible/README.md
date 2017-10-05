d2a4fdd28bc3eb0c1e2927dc69b396f83acf00e7
Chris Hillery <ceej@couchbase.com>
2017-10-05 00:44:16 -0700

    Moving 'ansible' to build-infra repo
    Change-Id: I8d23fe4c76d3a6f7ab0d8eb2e8f9c4976b07d1fc
    Reviewed-on: http://review.couchbase.org/84049
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- ansible/README.md
+++
@@ -1,2 +0,0 @@
-This contains Ansible playbooks for creating various build slave images.
-
--- ansible/windows/prep-for-ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-host=$1
-passwordfile=./password.txt
-
-sshpass -f $passwordfile scp -o StrictHostKeyChecking=no ConfigureForAnsible.ps1 Administrator@$host:
-sshpass -f $passwordfile ssh -o StrictHostKeyChecking=no Administrator@$host powershell.exe -File ConfigureForAnsible.ps1 -SkipNetworkProfileCheck -ForceNewSSLCert
