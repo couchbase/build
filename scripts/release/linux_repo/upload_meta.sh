dca1eb5ea098e127e20a010b76990ceabdca3925
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-10-24 23:30:04 -0700

    Updated linux_repo scripts to handle 5.0.0 and fixed s3cmd sync commands
    Change-Id: Ic77badfee46c3534362eb4479d62bf258ac00cfd
    Reviewed-on: http://review.couchbase.org/84780
    Reviewed-by: Chris Hillery
    Tested-by: Kenneth Lareau

--- scripts/release/linux_repo/upload_meta.sh
+++ scripts/release/linux_repo/upload_meta.sh
@@ -45,10 +45,10 @@ then
 else
     if [[ $1 == "--update" ]]
     then
-      # s3cmd sync -P --no-delete-removed --no-check-md5 --progress --verbose  ${LOCAL_REPO_ROOT}/keys         #  ${S3ROOT}/keys/
-        s3cmd sync -P --no-delete-removed                --progress --verbose  ${LOCAL_REPO_ROOT}/keys            ${S3ROOT}/keys/
-        s3cmd sync -P --no-delete-removed                --progress --verbose  ${LOCAL_REPO_ROOT}/sources.list.d  ${S3ROOT}/sources.list.d/
-        s3cmd sync -P --no-delete-removed                --progress --verbose  ${LOCAL_REPO_ROOT}/yum.repos.d     ${S3ROOT}/yum.repos.d/
+      # s3cmd sync -P --no-delete-removed --no-check-md5 --progress --verbose  ${LOCAL_REPO_ROOT}/keys/         #  ${S3ROOT}/keys/
+        s3cmd sync -P --no-delete-removed                --progress --verbose  ${LOCAL_REPO_ROOT}/keys/            ${S3ROOT}/keys/
+        s3cmd sync -P --no-delete-removed                --progress --verbose  ${LOCAL_REPO_ROOT}/sources.list.d/  ${S3ROOT}/sources.list.d/
+        s3cmd sync -P --no-delete-removed                --progress --verbose  ${LOCAL_REPO_ROOT}/yum.repos.d/     ${S3ROOT}/yum.repos.d/
     else
         usage
         exit 9
