15429b38a23c4241d98a93df8d153f58352f2159
Chris Hillery <ceej@couchbase.com>
2017-11-03 01:45:18 -0700

    CBD-2149: Add SSH keys to CV slaves for easy access
    Also removed slave-setup directory (moved and updated to
    build-infra/ansible/linux/docker) and modified README instructions
    to use new couchbasebuild/ansible-playbook image.
    
    Change-Id: Ieeb99510b232ef00ed0225b40eefb9de6bb9ebba
    Reviewed-on: http://review.couchbase.org/85049
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/cv-ansible/restart_cv_dockerslaves.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir ~/.ssh
-echo 'StrictHostKeyChecking no' > ~/.ssh/config
-cd /mnt
-ansible-playbook -v -i inventory $* restart_cv_dockerslaves.yml
-
--- docker/scripts/cv-ansible/slave-setup/slave-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir ~/.ssh
-echo 'StrictHostKeyChecking no' > ~/.ssh/config
-ansible-playbook -v -i /mnt/inventory /mnt/slave-setup/slave-setup.yml
-
