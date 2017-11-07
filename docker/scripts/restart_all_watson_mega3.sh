142c8c87401c2675e528685aea24dafae1e76435
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-11-06 18:12:43 -0800

    MB-26616: dh-systemd package needed for build system for Debian 8
    Change-Id: If97acff8ffd2e89c42506d67c6b5fad531664dc1
    Reviewed-on: http://review.couchbase.org/85163
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- docker/scripts/restart_all_watson_mega3.sh
+++ docker/scripts/restart_all_watson_mega3.sh
@@ -8,7 +8,7 @@ cd `dirname $0`
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:20151223 watson-ubuntu12.04 5223 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/debian-7-couchbase-build:20170522 watson-debian7 5224 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20170522 watson-centos7-01 5227 server.jenkins.couchbase.com
-./restart_jenkinsdocker.py ceejatec/debian-8-couchbase-build:20170522 watson-debian8 5229 server.jenkins.couchbase.com
+./restart_jenkinsdocker.py ceejatec/debian-8-couchbase-build:20171106 watson-debian8 5229 server.jenkins.couchbase.com
 
 # Temporary cbdeps slave based on Ubuntu 12.04 CV image
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 watson-ubuntu12.04-cv 5233 server.jenkins.couchbase.com
