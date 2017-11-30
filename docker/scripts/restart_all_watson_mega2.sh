f8a63d0e9367344dcc858c43199eefd6d9aea24f
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-11-30 13:07:11 -0800

    CBD-2168: Update Debian 9.1 build slave to GCC 7.2.0
    Change-Id: Ie0dd19ead0a6bae21612ec72999b3d68b85788f1
    Reviewed-on: http://review.couchbase.org/86157
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_all_watson_mega2.sh
+++ docker/scripts/restart_all_watson_mega2.sh
@@ -9,6 +9,8 @@ cd `dirname $0`
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-cv:20170522 spock-ubuntu16.04 5238 server.jenkins.couchbase.com
 # Spock Debian 9.1 builder
 ./restart_jenkinsdocker.py ceejatec/debian-9-couchbase-build:20170911 spock-debian9 5230 server.jenkins.couchbase.com
+# Vulcan Debian 9.1 builder
+./restart_jenkinsdocker.py ceejatec/debian-9-couchbase-build:20171130 vulcan-debian9 5231 server.jenkins.couchbase.com
 # Vulcan Ubuntu 16.04 slave - probably should use CV image as well,
 # but don't want to rebuild that yet
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:20171122 vulcan-ubuntu16.04 5239 server.jenkins.couchbase.com
