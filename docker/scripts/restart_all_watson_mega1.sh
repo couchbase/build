eed788049c1fd0e525c55153fb99f6f5baa47e20
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-12-06 18:07:40 -0800

    CBD-2169: Update SuSE 11 build slave to GCC 7.2.0
    Change-Id: I54eb7563bf83d7d75bd55c9f2954b43e6d26ca4f
    Reviewed-on: http://review.couchbase.org/86500
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- docker/scripts/restart_all_watson_mega1.sh
+++ docker/scripts/restart_all_watson_mega1.sh
@@ -4,6 +4,8 @@ cd `dirname $0`
 
 # New hostness Watson docker containers (currently hosted on mega)
 ./restart_jenkinsdocker.py ceejatec/suse-11-couchbase-build:20170522 watson-suse11 5228 server.jenkins.couchbase.com
+# Vulcan docker container for SuSE 11
+./restart_jenkinsdocker.py ceejatec/suse-11-couchbase-build:20171206 vulcan-suse11 5229 server.jenkins.couchbase.com
 
 wait
 echo "All done!"
