5f1ef3862e85fb0373d29026e3d9651afb363228
Chris Hillery <ceej@couchbase.com>
2017-12-12 16:03:27 -0800

    Add 'jenkinsapi' to cv-zz-lightweight
    This slave was running quite an old image, so updating to Ubuntu 16. Also,
    it was running the CV-specific one, which isn't desirable since that
    version adds a bunch of test-only tools and this slave shouldn't be used
    for actual test jobs.
    
    Change-Id: I1a127036b3203bc833e5f6d347d64267b0b95994
    Reviewed-on: http://review.couchbase.org/86751
    Reviewed-by: Kim Trang <kim.trang@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_cv_builders.sh
+++ docker/scripts/restart_cv_builders.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 # Currently these slaves are all hosted on mega2
 
 # cv.jenkins slaves
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 cv-zz-lightweight 3224 cv.jenkins.couchbase.com &
+./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:20171212 cv-zz-lightweight 3224 cv.jenkins.couchbase.com &
 
 # Launch additional CV slaves using Ansible; see cv-ansible/README
 #docker run --rm -i -v /home/couchbase/jenkinsdocker-ssh:/home/couchbase/jenkinsdocker-ssh -v `pwd`/cv-ansible:/mnt williamyeh/ansible:ubuntu16.04 /mnt/restart_cv_dockerslaves.sh &
