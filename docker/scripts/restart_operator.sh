b1dc869aae2e4beaf3f6dbd7f9c54ce93ae3fe36
Chris Hillery <ceej@couchbase.com>
2018-01-22 19:20:56 -0800

    MB-26806: Update operator-build slave to latest image
    Change-Id: Ie4052e0e41a821b315ac469768567e0f131a79b9
    Reviewed-on: http://review.couchbase.org/88217
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_operator.sh
+++ docker/scripts/restart_operator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This runs on mega3
-./restart_jenkinsdocker.py --mount-docker couchbasebuild/ubuntu-1604-operator-build:20180119 operator-build 2997 server.jenkins.couchbase.com
+./restart_jenkinsdocker.py --mount-docker couchbasebuild/ubuntu-1604-operator-build:20180122 operator-build 2997 server.jenkins.couchbase.com
 
