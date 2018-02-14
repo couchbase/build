7337ece98633e06109acca5f0fb8e9a044f96e3d
Chris Hillery <ceej@couchbase.com>
2018-02-14 02:31:28 -0800

    Removing unused Docker slaves
    Change-Id: Ic4c8a7ff4eac334e26936e865ef1f780b0d4eb39
    Reviewed-on: http://review.couchbase.org/89399
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_all_watson_mega1.sh
+++ docker/scripts/restart_all_watson_mega1.sh
@@ -2,10 +2,27 @@
 
 cd `dirname $0`
 
-# New hostness Watson docker containers (currently hosted on mega)
+# Currently these slaves are all hosted on mega1
+
+# New hostness Watson docker containers
 ./restart_jenkinsdocker.py ceejatec/suse-11-couchbase-build:20170522 watson-suse11 5228 server.jenkins.couchbase.com
 # Vulcan docker container for SuSE 11
 ./restart_jenkinsdocker.py ceejatec/suse-11-couchbase-build:20171206 vulcan-suse11 5229 server.jenkins.couchbase.com
+
+# Old-school Sherlock docker containers
+./restart_jenkinsdocker.py ceejatec/centos-65-couchbase-build:20150618 sherlocker-centos6 2222 &
+sleep 5
+./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20150930 sherlocker-centos7 2227 &
+sleep 5
+./restart_jenkinsdocker.py ceejatec/debian-7-couchbase-build:20150927 sherlocker-debian7 2224 &
+sleep 5
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:latest zz-lightweight 2223 &
+sleep 5
+./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:20150927 sherlocker-ubuntu12.04-01 2225 &
+sleep 5
+./restart_jenkinsdocker.py ceejatec/ubuntu-1404-couchbase-build:20150927 sherlocker-ubuntu14.04 2226 &
+sleep 5
+./restart_jenkinsdocker.py ceejatec/suse-11-couchbase-build:20150927 sherlocker-suse11 2228 &
 
 wait
 echo "All done!"
