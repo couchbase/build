dfc9f639b11c8cc1367644323d2a9a4db35b88dc
Kim Trang <kim.trang@couchbase.com>
2018-03-12 16:33:15 -0700

    Updated to the latest couchbasebuild/ansible-slave:20180312 docker image
    Change-Id: I93ced7f5854bc0bb8c41afa6e2e78825bec11d89
    Reviewed-on: http://review.couchbase.org/90824
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- docker/scripts/restart_ansible.sh
+++ docker/scripts/restart_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./restart_jenkinsdocker.py couchbasebuild/ansible-slave:20171110 ansible-slave-server 2999 server.jenkins.couchbase.com
-./restart_jenkinsdocker.py couchbasebuild/ansible-slave:20171110 ansible-slave-mobile 2998 mobile.jenkins.couchbase.com
+./restart_jenkinsdocker.py couchbasebuild/ansible-slave:20180312 ansible-slave-server 2999 server.jenkins.couchbase.com
+./restart_jenkinsdocker.py couchbasebuild/ansible-slave:20180312 ansible-slave-mobile 2998 mobile.jenkins.couchbase.com
 
