b711282cf7fc0995cde7df9ceb71c8a79296bbf5
Chris Hillery <ceej@couchbase.com>
2018-04-09 23:38:43 -0700

    Clean NAS ever more thoroughly
    Change-Id: Ie937c74457c49ae497b4029b93914673a39ff51d
    Reviewed-on: http://review.couchbase.org/92476
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- scripts/release/clean_old_nas.sh
+++ scripts/release/clean_old_nas.sh
@@ -9,9 +9,9 @@ remove_glob() {
   find . -name ${glob} -atime +${days} -exec sh -c 'echo > "{}"; rm "{}"' \;
 }
 
-# @@@@@@@@@
-# Clean up Couchbase Server
-# @@@@@@@@@
+echo @@@@@@@@@
+echo Clean up Couchbase Server
+echo @@@@@@@@@
 cd ${LATESTBUILDS}/couchbase-server
 
 # All Windows ".bits" files older than 1 day (only needed for intra-build
@@ -28,19 +28,32 @@ remove_glob "*-PDB.zip" 30
 
 # All Ubuntu 12/14, Debian 8, Centos 6, Suse, Mac, "oel", and Windows builds
 # older than 30/60 days
-remove_glob "*macos*.zip" 30
-remove_glob "*windows*exe" 30
-remove_glob "*windows*msi" 60
-remove_glob "*ubuntu12.04*.deb" 30
-remove_glob "*ubuntu14.04*.deb" 30
-remove_glob "*ubuntu16.04*.deb" 30
-remove_glob "*debian7*.deb" 60
-remove_glob "*debian8*.deb" 30
-remove_glob "*centos6*.rpm" 30
-remove_glob "*suse11*.rpm" 30
-remove_glob "*suse12*.rpm" 30
-remove_glob "*oel*.rpm" 30
+remove_glob "*macos*.zip*" 30
+remove_glob "*windows*exe*" 30
+remove_glob "*windows*msi*" 60
+remove_glob "*ubuntu12.04*.deb*" 30
+remove_glob "*ubuntu14.04*.deb*" 30
+remove_glob "*ubuntu16.04*.deb*" 30
+remove_glob "*debian7*.deb*" 60
+remove_glob "*debian8*.deb*" 30
+remove_glob "*debian9*.deb*" 30
+remove_glob "*centos6*.rpm*" 30
+remove_glob "*suse11*.rpm*" 30
+remove_glob "*suse12*.rpm*" 30
+remove_glob "*oel*.rpm*" 30
 
-# All Source tarballs older than 60 days
-remove_glob "*source.tar.gz" 60
+echo @@@@@@@@@
+echo Clean up cbq
+echo @@@@@@@@@
+cd ${LATESTBUILDS}/cbq
+remove_glob cbq-linux 30
+remove_glob cbq-macos 30
+remove_glob cbq-windows.exe 30
 
+echo @@@@@@@@@
+echo Clean up ALL products
+echo @@@@@@@@@
+cd ${LATESTBUILDS}
+
+# All Source tarballs older than 7 days
+remove_glob "*source.tar.gz" 7
