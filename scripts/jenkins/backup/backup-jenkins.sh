5ac0519e650b693c402e06bb25dd9232ae50c461
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-04-24 13:38:10 -0700

    CBD-2419: Clean up script to do Jenkins backups.
    Some basic fixes and clean up for the Jenkins backup script, which will
    be run from each Jenkins server as a job to handle it's own backups on
    a nightly basis.  Some modern bashisms were used during the clean up
    (primarily to avoid needing to shell out to /bin/test).
    
    Change-Id: I79074936a44edc4bf33fff4cf23bec585c93025f
    Reviewed-on: http://review.couchbase.org/93129
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- scripts/jenkins/backup/backup-jenkins.sh
+++ scripts/jenkins/backup/backup-jenkins.sh
@@ -8,81 +8,75 @@ function show_help {
     echo "   -b :  Full backup directory, if needed. This is for full backup only."
     echo "         Full backups are not ftp-ed to NAS. If this option is not given"
     echo "         full backups are not made. (Optional)"
-    echo "   -i :  Instance name (eg, factory, cv_jenkins, mobile_jenkins) (Required)"
+    echo "   -i :  Instance name (eg, server_jenkins, cv_jenkins, mobile_jenkins) (Required)"
 }
+
+bold=$(tput bold)
+normal=$(tput sgr0)
 
 while getopts :d:j:b:i:h ARG; do
     case $ARG in
         d) JENKINS_DATA="$OPTARG"
            ;;
-
         j) JENKINS_JOBS="$OPTARG"
            ;;
-
         b) FULL_BACKUP_DIR="$OPTARG"
            ;;
-
         i) INSTANCE_NAME="$OPTARG"
            ;;
-
         h) show_help
            ;;
-
-        \?) #unrecognized option - show help
-            echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
+        \?) # Unrecognized option; show help
+            echo -e \\n"Option -${bold}${OPTARG}${normal} not allowed."
             show_help
     esac
 done
 
-if [ "x$JENKINS_DATA" = "x" ]; then
-    echo "Data direcotry (-d) is required"
+if [[ -z "$JENKINS_DATA" ]]; then
+    echo "Data directory (-d) is required"
     exit 1
 fi
 
-if [ "x$INSTANCE_NAME" = "x" ]; then
+if [[ -z "$INSTANCE_NAME" ]]; then
     echo "Instance name (-i) is required"
     exit 1
 fi
 
-if [ "x$JENKINS_DATA" != "x" -a ! -d $JENKINS_DATA ]; then
-    echo "Data direcotry - $JENKINS_DATA - doesn't exist"
+if [[ -n "$JENKINS_DATA" && ! -d "$JENKINS_DATA" ]]; then
+    echo "Data directory ${JENKINS_DATA} doesn't exist"
     exit 1
 fi
 
-if [ "x$JENKINS_JOBS" != "x" -a ! -d "$JENKINS_JOBS" ]; then
-    echo "Jobs direcotry - $JENKINS_JOBS - doesn't exist"
+if [[ -n "$JENKINS_JOBS" && ! -d "$JENKINS_JOBS" ]]; then
+    echo "Jobs directory ${JENKINS_JOBS} doesn't exist"
     exit 1
 fi
 
-if [ "x$FULL_BACKUP_DIR" != "x" -a ! -d "$FULL_BACKUP_DIR" ]; then
-    echo "Full backup directory - $FULL_BACKUP_DIR - doesn't exist"
+if [[ -n "$FULL_BACKUP_DIR" && ! -d "$FULL_BACKUP_DIR" ]]; then
+    echo "Full backup directory ${FULL_BACKUP_DIR} doesn't exist"
     exit 1
 fi
 
-rm -f jenkins_backup*tar.gz
+# Remove any old temporary backup tarball dumps
+/bin/rm -f jenkins_backup*tar.gz
 
-DAYOFWEEK=`date +%a`
+DAYOFWEEK=$(/bin/date +%a)
 DUMP=jenkins_backup.${DAYOFWEEK}.tar.gz
 
-ADDL_FILES=""
-if [ "$INSTANCE_NAME" = "factory" ]; then
-    ADDL_FILES="/usr/share/jenkins/jenkins.war"
-fi
+echo "Starting minimal backup at $(/bin/date)"
+nice -n 19 tar --exclude-from jenkins_backup_exclusions -zcf ${DUMP} ${JENKINS_DATA} ${JENKINS_JOBS}
+echo "Minimal backup finished at $(/bin/date)"
 
-echo Starting minimal backup at `date`
-nice -n 19 tar --exclude-from jenkins_backup_exclusions -zcf $DUMP $JENKINS_DATA $JENKINS_JOBS $ADDL_FILES
-echo Minimal backup finished at `date`
-
-echo ftp to nas started at `date`
+echo "ftp to NAS started at $(/bin/date)"
 ftp 172.23.120.24 << END_CMDS
-cd Jenkins_backup/$INSTANCE_NAME
+cd Jenkins_backup/${INSTANCE_NAME}
 bin
-put $DUMP
+put ${DUMP}
 END_CMDS
-echo ftp to nas ended at `date`
+echo "ftp to NAS ended at $(/bin/date)"
 
-if [ "$DAYOFWEEK" = "Sun" -a -n "$FULL_BACKUP_DIR" ]; then
-   echo Starting full backup at `date`
-   nice -n 19 rsync -au $JENKINS_DATA $JENKINS_JOBS $FULL_BACKUP_DIR
-   echo Full backup finished at `date`
+if [[ "$DAYOFWEEK" == "Sun" && -n "$FULL_BACKUP_DIR" ]]; then
+   echo "Starting full backup at $(/bin/date)"
+   nice -n 19 rsync -au ${JENKINS_DATA} ${JENKINS_JOBS} ${FULL_BACKUP_DIR}
+   echo "Full backup finished at $(/bin/date)"
 fi
