30da1df523dd84a4205b4486614023c1ec1d1f78
Kim Trang <kim.trang@couchbase.com>
2018-05-02 13:01:58 -0700

    CBD-2462 Upgraded to use bin/sdkmanager (formerly bin/android).  The current Jenkins’s emulator plugin does not support android command due to upgrade of android “tools” and “platforms”
    Change-Id: I242ea60121074b8710b5c3b455645aa58a93d41f
    Reviewed-on: http://review.couchbase.org/93626
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- docker/buildslaves/ubuntu-14.04/android/restart.sh
+++ docker/buildslaves/ubuntu-14.04/android/restart.sh
@@ -9,7 +9,7 @@ docker run --name="mobile-light" -v /home/couchbase/jenkinsdocker-ssh:/ssh \
 docker rm -f mobile-android
 docker run --name="mobile-android" -v /home/couchbase/jenkinsdocker-ssh:/ssh \
         --restart=unless-stopped \
-        -p 2422:22 -d ceejatec/ubuntu1404-mobile-android-docker:20160712
+        -p 2422:22 -d ceejatec/ubuntu1404-mobile-android-docker:20180502
 
 docker rm -f mobile-java
 docker run --name="mobile-java" -v /home/couchbase/jenkinsdocker-ssh:/ssh \
