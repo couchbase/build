7236f0827b306edc878c7ad54c4dc062600bfc8e
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-05-15 19:22:02 -0700

    Update host IP and image info for SuSE 12 buildslaves
    Change-Id: I8ae4b0cfc4f9fc8c6f66e8b82136d7ed431bd7d1
    Reviewed-on: http://review.couchbase.org/94267
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- docker/scripts/restart_suse.sh
+++ docker/scripts/restart_suse.sh
@@ -2,9 +2,9 @@
 
 cd `dirname $0`
 
-# Spock SUSE container (currently hosted on 172.23.113.172)
+# Spock SUSE container (currently hosted on 172.23.96.238)
 ./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20170418 spock-suse12 3125 server.jenkins.couchbase.com &
-./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20171205 vulcan-suse12 3126 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20180515 vulcan-suse12 3126 server.jenkins.couchbase.com &
 
 wait
 echo "All done!"
