8cc80201318355a00fb8a982fa053c070b849778
Kim Trang <kim.trang@couchbase.com>
2018-06-05 10:34:59 -0700

    CBD-2514 diable auto-update from latest android tools
    Change-Id: I3e3b3375138320aee57e8c9f4a1d9d26d72076ba
    Reviewed-on: http://review.couchbase.org/95202
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- docker/scripts/restart_mobile.sh
+++ docker/scripts/restart_mobile.sh
@@ -11,5 +11,5 @@ echo @@@@@@@@@@@@@@@@@@@@@@
 ../buildslaves/ubuntu-14.04/android/restart.sh
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:latest      zz-mobile-lightweight 2423 mobile.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/centos-72-litecore-build:20170710       mobile-litecore-linux 6501 mobile.jenkins.couchbase.com
-./restart_jenkinsdocker.py --no-workspace ceejatec/ubuntu1604-mobile-lite-android:20180529 mobile-lite-android    6502 mobile.jenkins.couchbase.com
-./restart_jenkinsdocker.py --no-workspace ceejatec/ubuntu1604-mobile-lite-android:20180529 mobile-lite-android-02 6503 mobile.jenkins.couchbase.com
+./restart_jenkinsdocker.py --no-workspace ceejatec/ubuntu1604-mobile-lite-android:20180604 mobile-lite-android    6502 mobile.jenkins.couchbase.com
+./restart_jenkinsdocker.py --no-workspace ceejatec/ubuntu1604-mobile-lite-android:20180604 mobile-lite-android-02 6503 mobile.jenkins.couchbase.com
