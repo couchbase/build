834183e340a9462206f9e95c2673a0b3d8035241
Chris Hillery <ceej@couchbase.com>
2018-11-05 16:24:16 -0800

    Remove obsolete Ubuntu 12.04 slaves
    Change-Id: Ifedf1d0104f759559a8cf4ade9710dd3cdeeff9e
    Reviewed-on: http://review.couchbase.org/101411
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_all_watson_mega3.sh
+++ docker/scripts/restart_all_watson_mega3.sh
@@ -7,7 +7,6 @@ cd `dirname $0`
 # Vulcan CentOS 6 builder
 ./restart_jenkinsdocker.py couchbasebuild/server-centos6-build:20180713 vulcan-centos6 5225 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py couchbasebuild/server-ubuntu16-build:20180905 zz-server-lightweight-backup 5322 server.jenkins.couchbase.com
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-build:20151223 watson-ubuntu12.04 5223 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/debian-7-couchbase-build:20170522 watson-debian7 5224 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20170522 watson-centos7-01 5227 server.jenkins.couchbase.com
 # Vulcan CentOS 7 builder
@@ -15,9 +14,6 @@ cd `dirname $0`
 ./restart_jenkinsdocker.py ceejatec/debian-8-couchbase-build:20171106 watson-debian8 5229 server.jenkins.couchbase.com
 # Vulcan Debian 8.2 builder
 ./restart_jenkinsdocker.py couchbasebuild/server-debian8-build:20181017 vulcan-debian8 5230 server.jenkins.couchbase.com
-
-# Temporary cbdeps slave based on Ubuntu 12.04 CV image
-./restart_jenkinsdocker.py ceejatec/ubuntu-1204-couchbase-cv:20160304 watson-ubuntu12.04-cv 5233 server.jenkins.couchbase.com
 
 wait
 echo "All done!"
