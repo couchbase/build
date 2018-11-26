864f6b81c1526ce2029dd3b2d8ad2bd760c9551b
Chris Hillery <ceej@couchbase.com>
2018-11-26 00:19:10 -0800

    MB-31499: Update Mad-Hatter slaves to CMake 3.12+
    Also remove stale cv-jenkins and suse-12 Dockerfiles (in build-infra now)
    
    Change-Id: Ifdc412a874dc6c6116a7ab7f1c0e98d7d6cf6633
    Reviewed-on: http://review.couchbase.org/102082
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_suse.sh
+++ docker/scripts/restart_suse.sh
@@ -2,9 +2,9 @@
 
 cd `dirname $0`
 
-# Spock SUSE container (currently hosted on 172.23.96.152)
+# SUSE 12 containers (currently hosted on 172.23.96.152)
 ./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20170418 spock-suse12 3125 server.jenkins.couchbase.com &
-./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20180815 vulcan-suse12 3126 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20181126 vulcan-suse12 3126 server.jenkins.couchbase.com &
 
 wait
 echo "All done!"
--- docker/scripts/restart_suse_2.sh
+++ docker/scripts/restart_suse_2.sh
@@ -2,9 +2,9 @@
 
 cd `dirname $0`
 
-# Vulcan+ SUSE container (currently hosted on 172.23.96.156)
+# SUSE 12 containers (currently hosted on 172.23.96.156)
 ./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20170418 spock-suse12-02 3127 server.jenkins.couchbase.com &
-./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20180815 vulcan-suse12-02 3128 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20181126 vulcan-suse12-02 3128 server.jenkins.couchbase.com &
 
 wait
 echo "All done!"
