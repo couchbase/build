95be02e91e7229dd1d2e02f2a31db171350c3e7d
Chris Hillery <ceej@couchbase.com>
2018-12-18 18:07:33 -0800

    MB-31451: Update Centos 6 Alice slave to CMake 3.13
    Change-Id: I30d20f5995c54c4ee4b01afce3f04a56914ddff6
    Reviewed-on: http://review.couchbase.org/103008
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/scripts/restart_all_watson_mega3.sh
+++ docker/scripts/restart_all_watson_mega3.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 # Watson docker containers (currently hosted on mega3)
 ./restart_jenkinsdocker.py ceejatec/centos-65-couchbase-build:20170522 watson-centos6-01 5222 server.jenkins.couchbase.com
 # Vulcan CentOS 6 builder
-./restart_jenkinsdocker.py couchbasebuild/server-centos6-build:20180713 vulcan-centos6 5225 server.jenkins.couchbase.com
+./restart_jenkinsdocker.py couchbasebuild/server-centos6-build:20181218 vulcan-centos6 5225 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py couchbasebuild/server-ubuntu16-build:20181125 zz-server-lightweight-backup 5322 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/debian-7-couchbase-build:20170522 watson-debian7 5224 server.jenkins.couchbase.com
 ./restart_jenkinsdocker.py ceejatec/centos-70-couchbase-build:20170522 watson-centos7-01 5227 server.jenkins.couchbase.com
