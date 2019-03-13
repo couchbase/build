695851ee2f08bc58a3ea287f1dc570eb1f2d6ecd
Kim Trang <kim.trang@couchbase.com>
2019-03-13 15:10:25 -0700

    CBD-2902 Upgrade cmake-3.13.0
    Change-Id: Ie4d1fbf4f8e510bc6736d5c49737052a755415be
    Reviewed-on: http://review.couchbase.org/106147
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- docker/scripts/restart_mobile.sh
+++ docker/scripts/restart_mobile.sh
@@ -10,7 +10,7 @@ echo @@@@@@@@@@@@@@@@@@@@@@
 ../buildslaves/ubuntu-14.04/sgw_jenkins/restart.sh # port 2321
 ../buildslaves/ubuntu-14.04/android/restart.sh  # port 2300
 ./restart_jenkinsdocker.py ceejatec/ubuntu-1604-couchbase-build:latest      zz-mobile-lightweight 2423 mobile.jenkins.couchbase.com
-./restart_jenkinsdocker.py ceejatec/centos-72-litecore-build:20170710       mobile-litecore-linux 6501 mobile.jenkins.couchbase.com
+./restart_jenkinsdocker.py couchbasebuild/centos-72-litecore-build:20190313 mobile-litecore-linux 6501 mobile.jenkins.couchbase.com
 ./restart_jenkinsdocker.py --no-workspace ceejatec/ubuntu1604-mobile-lite-android:20180604 mobile-lite-android    6502 mobile.jenkins.couchbase.com
 ./restart_jenkinsdocker.py --no-workspace ceejatec/ubuntu1604-mobile-lite-android:20180604 mobile-lite-android-02 6503 mobile.jenkins.couchbase.com
 ./restart_jenkinsdocker.py --no-workspace couchbasebuild/ubuntu1604-sgw-build:20181204 mobile-sgw-ubuntu16.04 2323 mobile.jenkins.couchbase.com
