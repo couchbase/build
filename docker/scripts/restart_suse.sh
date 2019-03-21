dc7532c5b093f9dc4b28e6e8fa278ea987be0ef5
Kenneth Lareau <kenneth.lareau@couchbase.com>
2019-03-20 17:28:02 -0700

    Update SuSE 12 buildslaves (needed for v8 7.1)
    Change-Id: Ie5a2d14fe4836a75de1873a883d78cb69b9ef296
    Reviewed-on: http://review.couchbase.org/106543
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- docker/scripts/restart_suse.sh
+++ docker/scripts/restart_suse.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 # SUSE 12 containers (currently hosted on 172.23.96.152)
 ./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20170418 spock-suse12 3125 server.jenkins.couchbase.com &
-./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20181228 vulcan-suse12 3126 server.jenkins.couchbase.com &
+./restart_jenkinsdocker.py localonly/suse-12-couchbase-build:20190320 vulcan-suse12 3126 server.jenkins.couchbase.com &
 
 wait
 echo "All done!"
