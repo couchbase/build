612f3e7fc082211bb30083fca129dc346d94e9df
Ming Ho <ming.ho@couchbase.com>
2020-07-23 18:37:33 -0700

    CBD-3529, generate basic table of sonar scan result add project version so that result only contains current version's measures
    -Ming Ho
    
    Change-Id: I4d0edd1affc3f5a8c2e727d8e5afcf8adbe7ecd7
    Reviewed-on: http://review.couchbase.org/c/build/+/133177
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- scripts/jenkins/sonar/sonar_scan.py
+++ scripts/jenkins/sonar/sonar_scan.py
@@ -11,13 +11,14 @@ scan_target can be all, or single project/group from manifest
 
 assume sonar-scanner is on the PATH
 """
-
+import pandas
 from lxml import etree
 import subprocess
 import os, sys, re
 import argparse
 import logging
 import json
+import requests
 
 # logging
 logger = logging.getLogger()
@@ -27,7 +28,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel('INFO')
 
-def sonar_properties(project_name):
+def sonar_properties(project_name,version):
     project_dir=os.path.abspath(project_name)
     #load custom properties if it exists
     props_file=os.getcwd()+'/build/scripts/jenkins/sonar/'+project_name+'.json'
@@ -44,6 +45,7 @@ def sonar_properties(project_name):
         os.mkdir(project_dir+'/dummy')
     f = open(project_name+'/sonar-project.properties', 'w')
     f.write('sonar.projectKey='+project_name)
+    f.write('sonar.projectVersion='+version)
     f.write('\nsonar.nodejs.executable='+args.node_exec)
     f.write('\nsonar.java.binaries='+project_dir+'/dummy')
     f.write('\nsonar.host.url='+args.sonar_host_url)
@@ -73,7 +75,7 @@ def manifest_checkout():
     cmd='git clone ssh://git@github.com/couchbase/manifest.git'
     subprocess.check_output(cmd, shell=True);
 
-def repo_checkout(project,repo_base,default_rev):
+def sonar_scan(project,repo_base,default_rev,version):
     project_name=project.get('name')
     cmd='git clone '+repo_base+project_name+'.git'
     if not os.path.exists(project_name):
@@ -85,18 +87,43 @@ def repo_checkout(project,repo_base,default_rev):
             logger.info('checkout revision: %s', revision)
             cmd='cd '+project_name+';git checkout '+revision
             subprocess.check_output(cmd, shell=True);
-        sonar_properties(project_name)
-        sonar_scan(project_name)
+        sonar_properties(project_name,version)
+        scan_project(project_name)
     else:
       logger.info('%s exist.  Possibily duplicate project listed in manifest.  Skipping', project_name)
 
-def sonar_scan(project_name):
+def scan_project(project_name):
     project_dir=os.path.abspath(project_name)
     cmd='sonar-scanner -Dsonar.projectBaseDir='+project_dir
     logger.info('%s', cmd)
     subprocess.check_output(cmd, shell=True);
 
-def process_manifest(xml,target):
+def get_scan_result(project_list):
+
+    f = open('scan-result.csv', 'w')
+    f.write('project,new_vulnerabilities,new_code_smells,new_bugs')
+    # quality_gate_api_endpoint
+    URL = 'http://cleancode.service.couchbase.com/api/measures/component'
+    for proj in set(project_list):
+        #project name is actually component in the api request
+        PARAMS = {
+            'component': proj,
+            'metricKeys': 'new_vulnerabilities,new_code_smells,new_bugs'
+        }
+        try:
+            res = requests.get(url = URL, params = PARAMS)
+            project_measures=res.json()
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        f.write('\n'+project_measures['component']['name']\
+            +','+project_measures['component']['measures'][0]['period']['value']\
+            +','+project_measures['component']['measures'][1]['period']['value']\
+            +','+project_measures['component']['measures'][2]['period']['value'])
+    f.close()
+    df = pandas.read_csv("scan-result.csv")
+    df.to_html('scan-result.htm')
+
+def process_manifest(xml,target,version):
     tree = etree.parse(xml)
     if target == 'all':
         projects=tree.xpath('//project[not(@name="packaging") and not(@name="build")]')
@@ -110,6 +137,7 @@ def process_manifest(xml,target):
     default_revision = defaults[0].get('revision') if defaults else "master"
 
     remoteDict=dict()
+    project_list=list()
     for item in tree.xpath('//remote'):
         #add urls containing couchbase & couchbaselabs to dict.
         if re.search('github.com/(couchbase|couchbaselabs)/?$',item.get('fetch')):
@@ -119,9 +147,13 @@ def process_manifest(xml,target):
         remote = project.get('remote', default=default_remote)
         #only scan projects under couchbase and couchbaselabs
         if (remote in remoteDict):
-            repo_checkout(project,remoteDict.get(remote), default_revision)
+            project_list.append(project.get('name'))
+            sonar_scan(project,remoteDict.get(remote), default_revision, version)
         else:
             logger.info('%s is not a repo under couchbase, couchbase-priv, or couchbaselabs.  skipping...', project.get('name'))
+
+    #get scan results
+    get_scan_result(project_list)
 
 def main(args):
 
@@ -130,7 +162,7 @@ def main(args):
     manifest_checkout()
     manifest_file='manifest/couchbase-server/'+args.branch+'.xml'
     if os.path.exists(manifest_file):
-        process_manifest(manifest_file, args.scan_target)
+        process_manifest(manifest_file, args.scan_target, args.version)
     else:
         sys.exit(manifest_file+' does not exist')
 
@@ -142,6 +174,7 @@ if __name__ == "__main__":
     parser.add_argument('--node_exec', help='PATH to node executable\n', required=True)
     parser.add_argument('--scan_target', help='target project or group or all\n', required=True)
     parser.add_argument('--branch', help='target branch, i.e. cheshire-cat\n', required=True)
+    parser.add_argument('--version', help='version, i.e. cheshire-cat is 7.0.0\n', required=True)
     parser.add_argument('--exclusions', help='files to be excluded from analysis', default=None)
 
     args = parser.parse_args()
