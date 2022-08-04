295549906b89d18171fd04015113e026838cb009
Jim Borden <jim.borden@couchbase.com>
2022-08-04 15:41:16 -0400

    Remove centos6 from litecore deps manifest
    Change-Id: I877a6a8d17f24b0e65f19175772a4420e5beac19
    Reviewed-on: https://review.couchbase.org/c/build/+/178498
    Tested-by: Jim Borden <jim.borden@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- scripts/jenkins/lite-core/manifest.cmake
+++ scripts/jenkins/lite-core/manifest.cmake
@@ -4,5 +4,4 @@
 #
 # DECLARE_DEP (name VERSION version-revision PLATFORMS platform1 platform2)
 #
-DECLARE_DEP (icu4c VERSION 54.1-cb10 PLATFORMS centos6 centos7)
-DECLARE_DEP (zlib VERSION 1.2.11-cb3 PLATFORMS centos6)
+DECLARE_DEP (icu4c VERSION 54.1-cb10 PLATFORMS centos7)
