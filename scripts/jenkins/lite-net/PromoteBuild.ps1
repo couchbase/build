744749d981dcd7e1ed967de7675c0f38f319c88a
Jim Borden <jim.borden@couchbase.com>
2023-03-16 09:17:21 +0900

    Add in support for CBL .NET 3.1 and above
    This means both adding a new package (Couchbase.Lite.[Enterprise.]Support.WinUI) and downloading and updating snupkg files along with nupkg
    
    Change-Id: Ib0f6d6fac407898ce394949e0fb2dbf2cc84f525
    Reviewed-on: https://review.couchbase.org/c/build/+/188243
    Tested-by: Jim Borden <jim.borden@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- scripts/jenkins/lite-net/PromoteBuild.ps1
+++ scripts/jenkins/lite-net/PromoteBuild.ps1
@@ -50,28 +50,37 @@ try {
     }
 
     $Version1 = $InVersion.StartsWith("1")
+    $Version31Up = -not $Version1 -and -not $InVersion.StartsWith("2") -and -not $InVersion.StartsWith("3.0")
+    $buildlessVersion = $InVersion.Split("-")[0]
+    $numericBuildNumber = $InVersion.Split("-")[1].TrimStart('b', '0')
     if($Version1) {
-        $buildlessVersion = $InVersion.Split("-")[0]
-        $numericBuildNumber = $InVersion.Split("-")[1].TrimStart('b', '0')
         $package_names = "Couchbase.Lite","Couchbase.Lite.Listener","Couchbase.Lite.Listener.Bonjour","Couchbase.Lite.Storage.CustomSQLite","Couchbase.Lite.Storage.SQLCipher","Couchbase.Lite.Storage.ForestDB","Couchbase.Lite.Storage.SystemSQLite"
         foreach($package in $package_names) {
             Write-Host "Downloading http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-net/$buildlessVersion/$numericBuildNumber/$package.$InVersion.nupkg"
             Invoke-WebRequest "http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-net/$buildlessVersion/$numericBuildNumber/$package.$InVersion.nupkg" -OutFile "${package}.${InVersion}.nupkg"
         }
+    } elseif ($Version31Up) {
+        $package_names = "Couchbase.Lite","Couchbase.Lite.Enterprise","Couchbase.Lite.Support.Android","Couchbase.Lite.Support.iOS","Couchbase.Lite.Support.NetDesktop","Couchbase.Lite.Support.UWP","Couchbase.Lite.Support.WinUI","Couchbase.Lite.Enterprise.Support.Android","Couchbase.Lite.Enterprise.Support.iOS","Couchbase.Lite.Enterprise.Support.NetDesktop","Couchbase.Lite.Enterprise.Support.UWP","Couchbase.Lite.Enterprise.Support.WinUI"
+        foreach($package in $package_names) {
+            Write-Host "Downloading https://proget.sc.couchbase.com/nuget/CI/package/$package/$InVersion..."
+            Invoke-WebRequest https://proget.sc.couchbase.com/nuget/CI/package/$package/$InVersion -OutFile "${package}.${InVersion}.nupkg"
+            Invoke-WebRequest http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-net/${buildlessVersion}/${numericBuildNumber}/${package}.${InVersion}.snupkg -OutFile "${package}.${InVersion}.snupkg"
+        }
     } else {
         $package_names = "Couchbase.Lite","Couchbase.Lite.Enterprise","Couchbase.Lite.Support.Android","Couchbase.Lite.Support.iOS","Couchbase.Lite.Support.NetDesktop","Couchbase.Lite.Support.UWP","Couchbase.Lite.Enterprise.Support.Android","Couchbase.Lite.Enterprise.Support.iOS","Couchbase.Lite.Enterprise.Support.NetDesktop","Couchbase.Lite.Enterprise.Support.UWP"
         foreach($package in $package_names) {
-            Write-Host "Downloading http://172.23.113.15/nuget/Internal/package/$package/$InVersion..."
-            Invoke-WebRequest http://172.23.113.15/nuget/Internal/package/$package/$InVersion -OutFile "${package}.${InVersion}.nupkg"
+            Write-Host "Downloading https://proget.sc.couchbase.com/nuget/Internal/package/$package/$InVersion..."
+            Invoke-WebRequest https://proget.sc.couchbase.com/nuget/Internal/package/$package/$InVersion -OutFile "${package}.${InVersion}.nupkg"
         }
     }
 
-    foreach($file in (Get-ChildItem $pwd -Filter *.nupkg)) {
+    foreach($file in (Get-ChildItem $pwd -Filter *.*nupkg)) {
+        $packageExtension = [System.IO.Path]::GetExtension($file.Name).TrimStart('.')
         $packageComponents = [System.IO.Path]::GetFileNameWithoutExtension($file.Name).Split('.') | Take-While { -Not [System.Char]::IsDigit($args[0][0]) }
         $package = [System.String]::Join(".", $packageComponents)
         Remove-Item -Recurse -Force $package -ErrorAction Ignore
         New-Item -ItemType Directory $package
-        [System.IO.Compression.ZipFile]::ExtractToDirectory((Join-Path (Get-Location) "${package}.${InVersion}.nupkg"), (Join-Path (Get-Location) $package))
+        [System.IO.Compression.ZipFile]::ExtractToDirectory((Join-Path (Get-Location) "${package}.${InVersion}.$packageExtension"), (Join-Path (Get-Location) $package))
         Push-Location $package
         $stringContent = (Get-Content -Path "${package}.nuspec").Replace($InVersion, $OutVersion)
         $nuspec = [xml]$stringContent
@@ -83,10 +92,10 @@ try {
         $nuspec.Save([System.IO.Path]::Combine($pwd, "${package}.nuspec"))
         Pop-Location
 
-        Remove-Item -Path "$package.$OutVersion.nupkg" -ErrorAction Ignore -Force
-        & 7z a -tzip "$package.$OutVersion.nupkg" ".\$package\*"
+        Remove-Item -Path "$package.$OutVersion.$packageExtension" -ErrorAction Ignore -Force
+        & 7z a -tzip "$package.$OutVersion.$packageExtension" ".\$package\*"
         Remove-Item -Recurse -Force -Path $package
-        Remove-Item -Force -Path "${package}.${InVersion}.nupkg"
+        Remove-Item -Force -Path "${package}.${InVersion}.$packageExtension"
     }
 } finally {
     Pop-Location
