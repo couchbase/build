ad88fdf2e9a8f036b0872d80e9c9a24fd90fd239
Ming Ho <ming.ho@couchbase.com>
2024-12-17 15:40:24 -0800

    CBD-6160 packaging scripts for couchbase-lite-edgeserver.
    -Ming
    
    Change-Id: I728fac0624b6ade1d8ad02d4e11a64a6d8e6786a
    Reviewed-on: https://review.couchbase.org/c/build/+/221068
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

---
+++ scripts/jenkins/lite-edgeserver/lite-edgeserver-linux-package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+# This script is used by couchbase-lite-edgeserver-linux on Mobile Jenkins
+# PRODUCT, VERSION, and BLD_NUM are set by the job.
+
+# Prepare staging area for packaging
+if [[ -d ${PRODUCT} ]]; then
+    rm -rf ${PRODUCT}
+fi
+cp -rp ${WORKSPACE}/edge-server/install $PRODUCT
+
+# Get build agent's docker id
+# sidecar needs to know where to mount the volume.
+NODE_NAME=$(uname -n)
+
+# build rpm and deb
+ruby package-rpm.rb couchbase-lite-edgeserver ${PRODUCT} ${VERSION}-${BLD_NUM} x86_64
+docker run --rm --pull=always --volumes-from ${NODE_NAME} --workdir `pwd` --user 1000:1000 \
+    couchbasebuild/server-deb-sidecar:latest ruby package-deb.rb ${PRODUCT} \
+    ${PRODUCT} ${VERSION}-${BLD_NUM} amd64
+
+# copy deb and rpm to make it easier for Jenkins to publish
+cp build/deb/*.deb ${WORKSPACE}
+cp build/rpm/${PRODUCT}_${VERSION}-${BLD_NUM}/rpmbuild/RPMS/*/*.rpm ${WORKSPACE}
---
+++ scripts/jenkins/lite-edgeserver/package-deb.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+####    /${PKGR} ${SOURCE} ${PRODUCT} ${PRODUCT_VERSION} ${ARCH}
+
+require 'rubygems'
+require 'fileutils'
+require 'rake'
+
+PRODUCT_BASE    = "couchbase"
+
+STARTDIR  = Dir.getwd()
+
+SOURCE          = ARGV[0] || "#{STARTDIR}/couchbase-lite-edgeserver"
+PRODUCT         = ARGV[1] || "#{PRODUCT_BASE}-lite-edgeserver"
+PRODUCT_VERSION = ARGV[2] || "1.0.0-1234"
+ARCH            = ARGV[3] || `uname -m`.chomp
+
+VERSION         = PRODUCT_VERSION.split('-')[0]    # e.g., 1.0.0
+BLDNUM          = PRODUCT_VERSION.split('-')[1]    # e.g., 1234
+PKGNAME         ="#{PRODUCT}_#{VERSION}"
+PREFIX          ="/opt/#{PRODUCT}"
+
+sh %{echo "Packaging #{PRODUCT} for #{ARCH}"}
+STAGE_DIR = "#{STARTDIR}/build/deb/#{PKGNAME}-#{BLDNUM}"
+FileUtils.rm_rf   "#{STAGE_DIR}"
+FileUtils.mkdir_p "#{STAGE_DIR}"
+FileUtils.mkdir_p "#{STAGE_DIR}/debian"
+FileUtils.mkdir_p "#{STAGE_DIR}/opt"
+
+[["#{STARTDIR}/deb_templates", "#{STAGE_DIR}/debian"]].each do |src_dst|
+    Dir.chdir(src_dst[0]) do
+        Dir.glob("*.tmpl").each do |x|
+            target = "#{src_dst[1]}/#{x.gsub('.tmpl', '')}"
+            sh %{sed -e s,@@PRODUCT_VERSION@@,#{PRODUCT_VERSION},g           #{x} |
+                 sed -e s,@@PREFIX@@,#{PREFIX},g                          |
+                 sed -e s,@@PRODUCT@@,#{PRODUCT},g                        |
+                 sed -e s,@@PRODUCT_BASE@@,#{PRODUCT_BASE},g > #{target}}
+            sh %{chmod a+x #{target}}
+        end
+    end
+end
+
+sh %{cp -R "#{SOURCE}" "#{STAGE_DIR}/opt/"}
+
+Dir.chdir STAGE_DIR do
+  sh %{dpkg-buildpackage -b -uc}
+end
